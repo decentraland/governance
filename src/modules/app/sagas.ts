04156c30cc51d6971c4451d69ad735fa164e3121
Fede <i@2fd.me>
2020-09-11 13:01:21 -0300

    chore: force app name and version

--- src/modules/app/sagas.ts
+++ src/modules/app/sagas.ts
@@ -3,6 +3,7 @@ import { loadAppsSuccess, loadAppsFailure, LOAD_APPS_REQUEST, loadAppsRequest } 
 import { getOrganization } from 'modules/organization/selectors'
 import { Organization, App } from '@aragon/connect'
 import { LOAD_ORGANIZATION_SUCCESS } from 'modules/organization/actions'
+import { getAppName } from './utils'
 
 export function* appSaga() {
   yield takeLatest(LOAD_ORGANIZATION_SUCCESS, reloadLoadApps)
@@ -19,6 +20,8 @@ function* loadApps() {
     const apps: App[] = yield call(() => organization.apps())
     const record = {} as Record<string, App>
     for (const app of apps) {
+      (app as any).name = app.name || getAppName(app.address)?.toLowerCase() || undefined;
+      (app as any).version = app.version || '0.0.0'
       record[app.address] = app
     }
 
