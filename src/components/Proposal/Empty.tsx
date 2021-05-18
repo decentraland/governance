f8b82fe842edbe098e0032c4441db78795249acf
Fede <i@2fd.me>
2021-05-18 16:16:57 -0300

    fix: empty state

--- src/components/Proposal/Empty.tsx
+++ src/components/Proposal/Empty.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import './Empty.css'
+import Watermelon from '../Icon/Watermelon'
 
 export type EmptyProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> & {
   description?: React.ReactNode
@@ -9,7 +10,8 @@ export type EmptyProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> 
 
 export default React.memo(function Empty({ description, ...props}: EmptyProps) {
   return <div {...props} className={TokenList.join(['Empty', props.className])}>
-    {typeof description === 'string' && <Paragraph small semiBold secondary>{description}</Paragraph>}
+    <Watermelon />
+    {typeof description === 'string' && <Paragraph small secondary>{description}</Paragraph>}
     {typeof description !== 'string' && description}
   </div>
 })
