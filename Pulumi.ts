56ee279e288936803968f5c2f4f6e62c9e807f19
Fede <i@2fd.me>
2021-05-18 14:45:19 -0300

    chore: re deploy

--- Pulumi.ts
+++ Pulumi.ts
@@ -1,16 +1,16 @@
 // import { resolve } from "path";
-// import { buildGatsby } from "decentraland-gatsby-deploy/dist/recepies/buildGatsby";
-// const { gatsby } = require("./package.json");
+import { buildGatsby } from "decentraland-gatsby-deploy/dist/recepies/buildGatsby";
+const { gatsby } = require("./package.json");
 
 export = async function main() {
-  // return buildGatsby({
-  //   name: 'governance',
-  //   // contentSource: resolve(__dirname, '../public'),
-  //   usePublicTLD: process.env['USE_PUBLIC_TLD'] === 'true',
-  //   serviceSource: '.',
-  //   servicePaths: [
-  //     '/api/*',
-  //     '/metrics'
-  //   ]
-  // })
+  return buildGatsby({
+    name: 'governance',
+    // contentSource: resolve(__dirname, '../public'),
+    usePublicTLD: process.env['USE_PUBLIC_TLD'] === 'true',
+    serviceSource: '.',
+    servicePaths: [
+      '/api/*',
+      '/metrics'
+    ]
+  })
 }
