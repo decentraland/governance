2aed22c2a5e481642fbfa98bc9d297b99a415575
Fede <i@2fd.me>
2021-05-19 15:46:06 -0300

    fix: catalyt template

--- src/entities/Proposal/templates/catalyst.ts
+++ src/entities/Proposal/templates/catalyst.ts
@@ -4,7 +4,7 @@ import { formatMarkdown, template } from "./utils";
 export const title = (proposal: NewProposalCatalyst) => `Add catalyst node with domain ${proposal.domain} to the catalyst network`
 
 export const description = (proposal: NewProposalCatalyst) => template`
-Should the catalyst node with the domain ${proposal.domain} and owner ${proposal.owner} be added to Decentraland’s Catalyst Network?
+Should the catalyst node with the domain ${proposal.domain} and owner ${proposal.owner} be added to Decentraland's Catalyst Network?
 
 ## Description
 
