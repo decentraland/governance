e45282f44996530775d970e43fda6889e542dce2
Fede <i@2fd.me>
2021-05-24 10:44:08 -0300

    chore: down current page

--- .ci/index.ts
+++ .ci/index.ts
@@ -1,15 +1,15 @@
-import { env, envTLD } from 'dcl-ops-lib/domain'
-import { buildStatic } from 'dcl-ops-lib/buildStatic'
+// import { env, envTLD } from 'dcl-ops-lib/domain'
+// import { buildStatic } from 'dcl-ops-lib/buildStatic'
 
 async function main() {
-  const site = buildStatic({
-    domain: `governance.decentraland.${env === 'prd' ? 'org' : envTLD}`,
-    defaultPath: 'index.html'
-  })
+  // const site = buildStatic({
+  //   domain: `governance.decentraland.${env === 'prd' ? 'org' : envTLD}`,
+  //   defaultPath: 'index.html'
+  // })
 
-  return {
-    cloudfrontDistribution: site.cloudfrontDistribution,
-    bucketName: site.contentBucket
-  }
+  // return {
+  //   cloudfrontDistribution: site.cloudfrontDistribution,
+  //   bucketName: site.contentBucket
+  // }
 }
 export = main
