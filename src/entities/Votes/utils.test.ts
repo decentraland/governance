ade3481537f3d25ae867bac8bc0075976a9bd24e
Fede <i@2fd.me>
2021-06-02 21:12:44 -0300

    test: add test for calculateResult  and calculateResultWinner

--- src/entities/Votes/utils.test.ts
+++ src/entities/Votes/utils.test.ts
@@ -1,19 +1,520 @@
 
-import { calculateResult } from './utils'
+import { calculateResult, calculateResultWinner } from './utils'
 
 const choices = ['yes', 'no']
 
 const votes = {
-  '0x8cff6832174091dae86f0244e3fd92d4ced2fe07': { choice: 1, vp: 56275 },
-  '0x9982b469910c2ee2ea566dcfcc250cdd34056397': { choice: 1, vp: 400 },
-  '0xd2d950cea649feef4d6111c18adbd9a37b3a9f63': { choice: 2, vp: 4269 },
-  '0xd210dc1dd26751503cbf1b8c9154224707820da8': { choice: 1, vp: 1282 },
-  '0xe2b6024873d218b2e83b462d3658d8d7c3f55a18': { choice: 1, vp: 121889 },
-  '0xec6e6c0841a2ba474e92bf42baf76bfe80e8657c': { choice: 1, vp: 12000 },
+  "0xe58d9940a395d303e691dbe0676710d9c140101a": {
+    "choice": 1,
+    "vp": 4182,
+    "timestamp": 0,
+  },
+  "0xe2cfeab2beba9a7f09fd981f13e84df1e9978a5e": {
+    "choice": 1,
+    "vp": 1000,
+    "timestamp": 0,
+  },
+  "0xd2d950cea649feef4d6111c18adbd9a37b3a9f63": {
+    "choice": 2,
+    "vp": 4269,
+    "timestamp": 0,
+  },
+  "0x1f149885bb140f07ff88de2ac4d521bc27964c1b": {
+    "choice": 1,
+    "vp": 110000,
+    "timestamp": 0,
+  },
+  "0xaf0e126d11161fd002200e980103b78470e660e4": {
+    "choice": 2,
+    "vp": 8274,
+    "timestamp": 0,
+  },
+  "0xad015f00d380da858c76d736ff0f266608db0d98": {
+    "choice": 2,
+    "vp": 80,
+    "timestamp": 0,
+  },
+  "0xad33b38e8990328af86adf9382b6372d0d2fd617": {
+    "choice": 1,
+    "vp": 69,
+    "timestamp": 0,
+  },
+  "0x0a7e606eb21c80c28a005daeb5f3d99a40d6b154": {
+    "choice": 2,
+    "vp": 50,
+    "timestamp": 0,
+  },
+  "0xab45f68689ecf6b7927774529384b3c559153ce7": {
+    "choice": 2,
+    "vp": 2002,
+    "timestamp": 0,
+  },
+  "0x3cb9e515e2452937b501734aa2cc8ea673c28f27": {
+    "choice": 2,
+    "vp": 12269,
+    "timestamp": 0,
+  },
+  "0xaabe0ecfaf9e028d63cf7ea7e772cf52d662691a": {
+    "choice": 2,
+    "vp": 8656,
+    "timestamp": 0,
+  },
+  "0xd689478d44a438798ee0dc07657cce2135c0aef7": {
+    "choice": 2,
+    "vp": 17034,
+    "timestamp": 0,
+  },
+  "0xe062d5abbd68f1a91037fa2e918a4a24ad70bc76": {
+    "choice": 2,
+    "vp": 100,
+    "timestamp": 0,
+  },
+  "0x02dcce9e0aa57881dd15d09ad904c6ff93f0c059": {
+    "choice": 2,
+    "vp": 2006,
+    "timestamp": 0,
+  },
+  "0x5e382071464a6f9ea29708a045983dc265b0d86d": {
+    "choice": 2,
+    "vp": 7313,
+    "timestamp": 0,
+  },
+  "0x862f109696d7121438642a78b3caa38f476db08b": {
+    "choice": 2,
+    "vp": 16000,
+    "timestamp": 0,
+  },
+  "0xbeee816c3ab89976b22faba9a17e12488e30bcaa": {
+    "choice": 2,
+    "vp": 159,
+    "timestamp": 0,
+  },
+  "0x9caf2f699d62ef9602f5fc95d7f646d3c8659875": {
+    "choice": 2,
+    "vp": 73259,
+    "timestamp": 0,
+  },
+  "0x9b04da63e17bfe3b966884439fe06318dbe03863": {
+    "choice": 2,
+    "vp": 32576,
+    "timestamp": 0,
+  },
+  "0x598f8af1565003ae7456dac280a18ee826df7a2c": {
+    "choice": 2,
+    "vp": 14682,
+    "timestamp": 0,
+  },
+  "0x76fb13f00cdbdd5eac8e2664cf14be791af87cb0": {
+    "choice": 2,
+    "vp": 4001,
+    "timestamp": 0,
+  },
+  "0xa01424b7540adbb792375dcf97b733a5d68ad347": {
+    "choice": 2,
+    "vp": 59421,
+    "timestamp": 0,
+  },
+  "0xeb277c1eee99139d7ccb9b9bf4e9002ab7ab4ebf": {
+    "choice": 2,
+    "vp": 6000,
+    "timestamp": 0,
+  },
+  "0xd210dc1dd26751503cbf1b8c9154224707820da8": {
+    "choice": 2,
+    "vp": 1282,
+    "timestamp": 0,
+  },
+  "0xffac7fd045303112fdb28e9dace8e1334ad324c0": {
+    "choice": 1,
+    "vp": 524937,
+    "timestamp": 0,
+  },
+  "0xcf10cd8b5dc2323b1eb6de6164647756bad4de4d": {
+    "choice": 2,
+    "vp": 25951,
+    "timestamp": 0,
+  },
+  "0x338ad1918362025f34b3701dac1e9648e8e8268f": {
+    "choice": 2,
+    "vp": 356310,
+    "timestamp": 0,
+  },
+  "0xe161cc33f5b430be52aa69520d32cd3f39fa2be6": {
+    "choice": 1,
+    "vp": 2026000,
+    "timestamp": 0,
+  },
+  "0x1e93e534c5e26b01ed242410b43ae23dd0faa52b": {
+    "choice": 1,
+    "vp": 12028,
+    "timestamp": 0,
+  },
+  "0x4278d3e39f25d690b2d060c119ab2478fa09aff5": {
+    "choice": 2,
+    "vp": 6121,
+    "timestamp": 0,
+  },
+  "0x79bfbb59be9312110a8693bf49cc5bae8a4ef0f5": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0x039e574ceb2c58519586ad10e353f7cdd09df92a": {
+    "choice": 2,
+    "vp": 8717,
+    "timestamp": 0,
+  },
+  "0xa05bca6f6d9c0bc2c3a597ac3f800eaf502aca66": {
+    "choice": 2,
+    "vp": 7348,
+    "timestamp": 0,
+  },
+  "0x05c351382db8d770207f319d96ac1184c3717ede": {
+    "choice": 2,
+    "vp": 1645,
+    "timestamp": 0,
+  },
+  "0x8d1bf34397d31903320ac0a8fee81b3bb7531537": {
+    "choice": 1,
+    "vp": 22500,
+    "timestamp": 0,
+  },
+  "0x23be90335e79d3245615985d91f69814f98a0fab": {
+    "choice": 2,
+    "vp": 6000,
+    "timestamp": 0,
+  },
+  "0x1bbec7a15d0f0853b8f3ca0ad4d3573ce186d5ee": {
+    "choice": 2,
+    "vp": 2021,
+    "timestamp": 0,
+  },
+  "0x1177ba1e2fa6dbf1c9753c4e3405410173af1e83": {
+    "choice": 1,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0xe793376ad5357ccce8cd0f8c8be012464648f8f3": {
+    "choice": 2,
+    "vp": 10000,
+    "timestamp": 0,
+  },
+  "0x2d2f80ecb07f01532c044d1423bb930f9ba4f700": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0x8cff6832174091dae86f0244e3fd92d4ced2fe07": {
+    "choice": 2,
+    "vp": 56255,
+    "timestamp": 0,
+  },
+  "0x82b16206e6d54ec145835720e13c43ecdb57b814": {
+    "choice": 2,
+    "vp": 6000,
+    "timestamp": 0,
+  },
+  "0x8b51c1ba09ee33e7649cac62ccb6d0f410f5647a": {
+    "choice": 2,
+    "vp": 852531,
+    "timestamp": 0,
+  },
+  "0x103b3a31c6732c8615161757c64d2c5ed7daad38": {
+    "choice": 2,
+    "vp": 18000,
+    "timestamp": 0,
+  },
+  "0x9c6b3261282fc36c9b6efae89717f4c1f9883234": {
+    "choice": 2,
+    "vp": 176,
+    "timestamp": 0,
+  },
+  "0x31e99699bccde902afc7c4b6b23bb322b8459d22": {
+    "choice": 2,
+    "vp": 24000,
+    "timestamp": 0,
+  },
+  "0x90c878ce39208e58477ee6bf4ba1c8b24d9b361a": {
+    "choice": 1,
+    "vp": 74,
+    "timestamp": 0,
+  },
+  "0x726e3b010934ecffb184b5f6b19a1edccdcbbb83": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0x96d623001c5c6de3cd8b07346da6d0d97dd4c2b3": {
+    "choice": 2,
+    "vp": 0,
+    "timestamp": 0,
+  },
+  "0x9cf7a1bf516a5bb95458a43ab030242b2aed5ae2": {
+    "choice": 2,
+    "vp": 2017,
+    "timestamp": 0,
+  },
+  "0xcfa836ba6aed57164a5e6ddebc631f41019f0aa2": {
+    "choice": 2,
+    "vp": 3177,
+    "timestamp": 0,
+  },
+  "0x1b3010767f2f4c67a3f7e35bc3eab89069d2aaf6": {
+    "choice": 2,
+    "vp": 241,
+    "timestamp": 0,
+  },
+  "0xe13e240fc0e3756dddc9253640b7491375844e17": {
+    "choice": 2,
+    "vp": 9,
+    "timestamp": 0,
+  },
+  "0xd6c556e191b48170dd687c51d0e61847ed625a81": {
+    "choice": 2,
+    "vp": 2293,
+    "timestamp": 0,
+  },
+  "0x5e5d33cb2088b37248767bb6431dac4d9db78146": {
+    "choice": 2,
+    "vp": 2361,
+    "timestamp": 0,
+  },
+  "0x7fcad8632c69b6489455be08b690b41aa52b2146": {
+    "choice": 2,
+    "vp": 251,
+    "timestamp": 0,
+  },
+  "0x1f667f4d615c4931f2fd215d458f19c6a03c59dd": {
+    "choice": 2,
+    "vp": 5455,
+    "timestamp": 0,
+  },
+  "0x8d2c9c6c5029496fd8321762b20d2529ef027c26": {
+    "choice": 2,
+    "vp": 6324,
+    "timestamp": 0,
+  },
+  "0xc5b4e3ed8b30bf691bfda2d1604080a6781d0fc3": {
+    "choice": 1,
+    "vp": 16000,
+    "timestamp": 0,
+  },
+  "0x51e723b14b160b9b420cbf7ccdb889f36213f0f3": {
+    "choice": 1,
+    "vp": 42531,
+    "timestamp": 0,
+  },
+  "0x7616e594f1832d0dc7aad3923967d76fd98416e2": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0x0052d2e4af4548e6cf19e817b944cedb23ae09df": {
+    "choice": 2,
+    "vp": 6560,
+    "timestamp": 0,
+  },
+  "0x9aed8a57feee810bd727c57fe82a1f7a96808672": {
+    "choice": 2,
+    "vp": 0,
+    "timestamp": 0,
+  },
+  "0xcc1e0a566dbd10869c071c811aba436357858f05": {
+    "choice": 1,
+    "vp": 238,
+    "timestamp": 0,
+  },
+  "0x4cb01b6672bc750821af952ec5a2447fc90195b1": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0x967fb0c36e4f5288f30fb05f8b2a4d7b77eaca4b": {
+    "choice": 2,
+    "vp": 8000,
+    "timestamp": 0,
+  },
+  "0xa5e2bb97de3c62ca06f1a2293eb7e01c114e9a49": {
+    "choice": 2,
+    "vp": 12029,
+    "timestamp": 0,
+  },
+  "0x7aaaae8cd46ab71e1e5084d55d9c6b3ea4d970a0": {
+    "choice": 2,
+    "vp": 8019,
+    "timestamp": 0,
+  },
+  "0xa73e532f93c0cdc35bd79abb0d0a22713154bc98": {
+    "choice": 2,
+    "vp": 76017,
+    "timestamp": 0,
+  },
+  "0x8a32d38857d8421c7461f9dd2140d90c096e06c0": {
+    "choice": 2,
+    "vp": 82000,
+    "timestamp": 0,
+  },
+  "0xd6e62a97a55537cd04847bb73e22208bd20106aa": {
+    "choice": 2,
+    "vp": 2,
+    "timestamp": 0,
+  },
+  "0x23d8646672cdca0b3261adc46aa2d5a44b0a56eb": {
+    "choice": 2,
+    "vp": 8136,
+    "timestamp": 0,
+  },
+  "0x41e3faab16624921d2b43f2c82acfac0e6240a15": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0xb59da3a5fa2fab1e8aae5cb352146b3b02f53578": {
+    "choice": 2,
+    "vp": 2003,
+    "timestamp": 0,
+  },
+  "0x4ac6144e29bdad84e7671027da80422bb00787ce": {
+    "choice": 2,
+    "vp": 6006,
+    "timestamp": 0,
+  },
+  "0xf7e158bd2b6e79ef2f2ab72ac6cb2fea239c2a9b": {
+    "choice": 2,
+    "vp": 4001,
+    "timestamp": 0,
+  },
+  "0x6650f2cf168f73fb884310df5b11721f7db3b73a": {
+    "choice": 2,
+    "vp": 0,
+    "timestamp": 0,
+  },
+  "0x8b47f00455c2e40a549535798796dea0ebb94158": {
+    "choice": 2,
+    "vp": 30000,
+    "timestamp": 0,
+  },
+  "0x509cdebba232e7b7f603db5c93d1e4f1fed9c6f8": {
+    "choice": 2,
+    "vp": 1050,
+    "timestamp": 0,
+  },
+  "0x0a717b74a7c9402d2be25c1955d14631be10f55a": {
+    "choice": 2,
+    "vp": 4795,
+    "timestamp": 0,
+  },
+  "0x69d494eade06850b5074b502faa3666ec19f0787": {
+    "choice": 1,
+    "vp": 376000,
+    "timestamp": 0,
+  },
+  "0x4959f54f7b30224047ac3cae6bf147bbaee8b61c": {
+    "choice": 1,
+    "vp": 945000,
+    "timestamp": 0,
+  },
+  "0x2c8f6c836d4c1f778958b5c26234dac5afd6e11f": {
+    "choice": 2,
+    "vp": 868,
+    "timestamp": 0,
+  },
+  "0x1ca5e86a1a7a88645dfe163071367a317c9033e4": {
+    "choice": 2,
+    "vp": 4589,
+    "timestamp": 0,
+  },
+  "0x4d29b7f953ba471fb650fc5842127b05e35949b5": {
+    "choice": 2,
+    "vp": 80978,
+    "timestamp": 0,
+  },
+  "0xd4a08cf067c83d1b2cc1d26831569b7850804be7": {
+    "choice": 2,
+    "vp": 28000,
+    "timestamp": 0,
+  },
+  "0x361b9fbf20ed8de4b62cd5b0ccdf36face38bdc4": {
+    "choice": 2,
+    "vp": 37,
+    "timestamp": 0,
+  },
+  "0x29d5cea7d511810f3ff754886b898fce16a6d8fd": {
+    "choice": 2,
+    "vp": 2000,
+    "timestamp": 0,
+  },
+  "0x30b1f4bd5476906f38385b891f2c09973196b742": {
+    "choice": 2,
+    "vp": 16005,
+    "timestamp": 0,
+  },
+  "0x7a61a0ed364e599ae4748d1ebe74bf236dd27b09": {
+    "choice": 1,
+    "vp": 137897,
+    "timestamp": 0,
+  },
+  "0x2dad57078bd67b4da2e226051e1e20da11abc1e4": {
+    "choice": 1,
+    "vp": 100000,
+    "timestamp": 0,
+  },
+  "0x6caa802c6895ecd31601518c7f6b9ef6f9349238": {
+    "choice": 1,
+    "vp": 1179,
+    "timestamp": 0,
+  },
+  "0x83c9440dc34da00c47a0d4dc2b598d7bdb1b53f7": {
+    "choice": 1,
+    "vp": 171,
+    "timestamp": 0,
+  },
+  "0x3a017854138c5f9b85b0457b832151b28213b6e6": {
+    "choice": 2,
+    "vp": 0,
+    "timestamp": 0,
+  },
+  "0xb291e11e4195f1a0a6f6b8d4f0691bd2e89d8fe4": {
+    "choice": 1,
+    "vp": 560,
+    "timestamp": 0,
+  },
+  "0x3fb38cee8d0ba7dcf59403a8c397626dc9c7a13b": {
+    "choice": 1,
+    "vp": 1,
+    "timestamp": 0,
+  }
 }
 
 describe('entities/Votes/utils', () => {
   test('calculateResult', () => {
-    console.log(calculateResult(choices, votes))
+    expect(calculateResult(choices, votes))
+      .toEqual([
+        {
+          choice: 'yes',
+          power: 4322367,
+          votes: 21,
+          color: 'approve',
+          progress: 68
+        },
+        {
+          choice: 'no',
+          power: 2065731,
+          votes: 75,
+          color: 'reject',
+          progress: 32
+        }
+      ])
+  })
+
+  test('calculateResultWinner', () => {
+    expect(calculateResultWinner(choices, votes))
+      .toEqual({
+        choice: 'yes',
+        power: 4322367,
+        votes: 21,
+        color: 'approve',
+        progress: 68
+      })
   })
 })
