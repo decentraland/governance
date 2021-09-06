45217c9269e4416388cf479b6d545f591923ae4a
frami <i@2fd.me>
2021-09-06 17:11:02 +0200

    fix: use React.memo

--- src/components/Status/StatusLabel.tsx
+++ src/components/Status/StatusLabel.tsx
@@ -10,11 +10,11 @@ export type StatusLabelProps = React.HTMLAttributes<HTMLDivElement> & {
   status: ProposalStatus
 }
 
-export default function StatusLabel({ status, ...props }: StatusLabelProps) {
+export default React.memo(function StatusLabel({ status, ...props }: StatusLabelProps) {
   status = isProposalStatus(status) ? status : ProposalStatus.Pending
   return <div {...props} className={TokenList.join(['StatusLabel', `StatusLabel--${status}`, props.className])}>
     {status === ProposalStatus.Passed && <img src={check} width="20" height="20" />}
     {status === ProposalStatus.Enacted && <img src={checkInvert} width="20" height="20" />}
     <span>{status}</span>
   </div>
-}
+})
