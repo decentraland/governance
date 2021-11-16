a15b4afd01711e9c7f01360845e6e25fe4f0ec6f
oliviel <olivielultimate@gmail.com>
2021-11-16 13:35:14 -0400

    Update voteslist header to use en.json.modal

--- src/components/Modal/VotesList.tsx
+++ src/components/Modal/VotesList.tsx
@@ -25,7 +25,7 @@ export function VotesList({onClickAccept, votes, ...props }: VotesListModalProps
 
   return <Modal {...props} size="tiny" className={TokenList.join(['ProposalModal', 'VotesList' ,props.className])} closeIcon={<Close />}>
   <Modal.Content className="ProposalModal__Title">
-    <Header>{Object.entries(votes || {}).length} {Object.entries(votes || {}).length === 1 ? 'Vote' : 'Votes'}</Header>
+    <Header>{Object.entries(votes || {}).length} {Object.entries(votes || {}).length === 1 ? l('modal.votes_list.vote') : l('modal.votes_list.votes')}</Header>
   </Modal.Content>
   <div className="VotesList_Container_Header">
     <Grid columns='equal'>
