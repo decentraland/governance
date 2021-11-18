23239308ce12ce29ae07e5fe8d1ee8663092d9ca
jparnaudo <juampiarnaudo@gmail.com>
2021-11-18 11:08:38 -0300

    Initial layout for proposal comments section

--- src/components/Proposal/ProposalComments.tsx
+++ src/components/Proposal/ProposalComments.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { Link } from 'gatsby-plugin-intl'
 import { Header } from 'decentraland-ui/dist/components/Header/Header'
 import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
-import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import { ProposalAttributes } from '../../entities/Proposal/types'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import locations from '../../modules/locations'
-import { Vote } from '../../entities/Votes/types'
 import './ProposalComments.css'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import Avatar from 'decentraland-gatsby/dist/components/User/Avatar'
@@ -18,26 +17,28 @@ import Watermelon from '../Icon/Watermelon'
 
 export type ProposalResultSectionProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> & {
   proposal?: ProposalAttributes | null,
-  votes?: Record<string, Vote> | null,
-  votingPower?: number
   loading?: boolean,
-  disabled?: boolean,
-  changingVote?: boolean,
-  onChangeVote?: (e: React.MouseEvent<any>, changing: boolean) => void,
-  onVote?: (e: React.MouseEvent<any>, choice: string, choiceIndex: number) => void
 }
 
 export default React.memo(function ProposalComments({ proposal, loading, ...props }: ProposalResultSectionProps) {
-  const l = useFormatMessage()
+  const comments = true
   const [ profile ] = useAsyncMemo(
     async () => proposal?.user ? profiles.load(proposal.user) : null,
     [ proposal?.user ],
     { callWithTruthyDeps: true }
   )
+  const isProfile = !!profile && !profile.isDefaultProfile
 
-  return <div>
+  if (!profile) {
+    return null
+  }
+
+  return <div className="ProposalComments">
     <hr />
-    <Header>Comments</Header>
+    <div className="ProposalComments__Header">
+      <Header>Comments</Header> {comments &&
+    <Button basic as={Link} href={locations.balance()}>Join the discussion</Button>}
+    </div>
     <div {...props} className={TokenList.join([
       'ProposalComments',
       loading && 'ProposalComments--loading',
@@ -45,18 +46,25 @@ export default React.memo(function ProposalComments({ proposal, loading, ...prop
     ])}>
       <div className="ProposalComments__Content">
         <Loader active={loading} />
-        <div className="ProposalComments__NoComments">
+        {!comments && <div className="ProposalComments__NoComments">
           <Watermelon />
           <Paragraph small secondary>No comments yet, you have the chance to be the first one!</Paragraph>
           <Link to={locations.balance()}>Join the discussion</Link>
-        </div>
-        <div className="ProposalComments__Comments">
-            {profile && profile.name && <Avatar size="mini" address={profile.ethAddress} style={{ marginRight: '.5rem' }} />}
-            {profile && profile.name}
-            {(!profile || !profile.name) && !!proposal?.user &&  <Blockie scale={3} seed={proposal?.user || ''}>
-              <Address value={proposal?.user || ''} />
-            </Blockie>}
-        </div>
+        </div>}
+        {comments && <div className="ProposalComments__Comment">
+          <div className="ProposalComments__ProfileImage">
+            {isProfile && <Avatar size="medium" address={profile!.ethAddress} />}
+            {!isProfile &&  <Blockie scale={5} seed={proposal?.user || ''} />}
+          </div>
+          <div>
+            <div className="ProposalComments__Author">
+              {isProfile && profile!.name || <Address value={profile!.ethAddress || ''} strong />}
+              {!isProfile &&  <Address value={proposal?.user || ''} strong />}
+              <span><Paragraph secondary >3 days ago</Paragraph></span>
+            </div>
+            <Paragraph small>This is a pretty nice comment nice comment nice comment nice comment</Paragraph>
+          </div>
+        </div>}
       </div>
     </div>
   </div>
