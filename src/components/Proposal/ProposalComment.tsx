77c30a547147ac8846fbfad5a4c708568450d917
jparnaudo <juampiarnaudo@gmail.com>
2021-11-25 10:45:03 -0300

    Add forum avatar and html sanitization to proposal comment components

--- src/components/Proposal/ProposalComment.tsx
+++ src/components/Proposal/ProposalComment.tsx
@@ -1,45 +1,32 @@
 import React from 'react'
+import DOMPurify from "dompurify";
 import './ProposalComment.css'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import Avatar from 'decentraland-gatsby/dist/components/User/Avatar'
-import { Blockie } from 'decentraland-ui/dist/components/Blockie/Blockie'
-import useAsyncMemo from 'decentraland-gatsby/dist/hooks/useAsyncMemo'
-import profiles from 'decentraland-gatsby/dist/utils/loader/profile'
 import Time from 'decentraland-gatsby/dist/utils/date/Time'
 
 export type ProposalCommentProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> & {
   user?: string | null,
+  avatar_url: string,
   created_at: string,
   cooked: string|undefined,
 }
 
-export default function ProposalComment({ user, created_at, cooked}: ProposalCommentProps) {
-  const [ profile ] = useAsyncMemo(
-    async () => user ? profiles.load(user) : null, [ user ], { callWithTruthyDeps: true }
-  )
-  const isProfile = !!profile && !profile.isDefaultProfile
-
+export default function ProposalComment({ user, avatar_url, created_at, cooked}: ProposalCommentProps) {
   const createMarkup = (html:any) => {
-    return  {
-      __html: html
-    }
-  }
-
-  if (!profile) {
-    return null
+    return  {__html: DOMPurify.sanitize(html, {USE_PROFILES: {html: true}, FORBID_TAGS: ['style'], FORBID_ATTR: ['style']})}
   }
 
   return <div className="ProposalComment">
           <div className="ProposalComment__ProfileImage">
-            {isProfile && <Avatar size="medium" address={profile!.ethAddress} />}
-            {!isProfile &&  <Blockie scale={5} seed={user || ''} />}
+            <Avatar size="medium" src={avatar_url} />
           </div>
           <div>
             <div className="ProposalComment__Author">
               <Paragraph bold>{user}</Paragraph>
               <span><Paragraph secondary >{Time.from(created_at).fromNow()}</Paragraph></span>
             </div>
-            <Paragraph small dangerouslySetInnerHTML={createMarkup(cooked)} />
+            <div dangerouslySetInnerHTML={createMarkup(cooked)} />
           </div>
         </div>
 }
--- src/components/Proposal/ProposalComments.tsx
+++ src/components/Proposal/ProposalComments.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Link } from 'gatsby-plugin-intl'
 import { Header } from 'decentraland-ui/dist/components/Header/Header'
 import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import { ProposalAttributes } from '../../entities/Proposal/types'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
-import locations from '../../modules/locations'
 import './ProposalComments.css'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import Watermelon from '../Icon/Watermelon'
@@ -13,6 +11,7 @@ import ProposalComment from './ProposalComment'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import useAsyncMemo from 'decentraland-gatsby/dist/hooks/useAsyncMemo'
 import { Governance } from '../../api/Governance'
+import { forumUrl } from '../../entities/Proposal/utils'
 
 export type ProposalResultSectionProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> & {
   proposal?: ProposalAttributes | null,
@@ -21,13 +20,16 @@ export type ProposalResultSectionProps = Omit<React.HTMLAttributes<HTMLDivElemen
 
 export default React.memo(function ProposalComments({ proposal, loading, ...props }: ProposalResultSectionProps) {
   const l = useFormatMessage()
-  const [comments] = useAsyncMemo(async () => Governance.get().getProposalComments(proposal!.id), [proposal])
+  const [comments] = useAsyncMemo(async () => proposal ? Governance.get().getProposalComments(proposal!.id) : null, [proposal])
+  let renderComments = comments && comments.length > 0
 
   return <div className="ProposalComments">
     <hr />
     <div className="ProposalComments__Header">
-      <Header>Comments</Header> {comments &&
-    <Button basic as={Link} href={locations.balance()}>{l('page.proposal_comments.join_discussion_label')}</Button>}
+      <Header>Comments</Header>
+      {renderComments &&
+        <Button basic loading={loading} disabled={!proposal} href={proposal && forumUrl(proposal) || ''}>{l('page.proposal_comments.join_discussion_label')}</Button>
+      }
     </div>
     <div {...props} className={TokenList.join([
       'ProposalComments',
@@ -36,16 +38,16 @@ export default React.memo(function ProposalComments({ proposal, loading, ...prop
     ])}>
       <div className="ProposalComments__Content">
         <Loader active={loading} />
-        {!comments && <div className="ProposalComments__NoComments">
+        {!renderComments && <div className="ProposalComments__NoComments">
           <Watermelon />
           <Paragraph small secondary>{l('page.proposal_comments.no_comments_text')}</Paragraph>
-          <Link to={locations.balance()}>{l('page.proposal_comments.join_discussion_label')}</Link>
+          <Button basic loading={loading} disabled={!proposal} href={proposal && forumUrl(proposal) || ''}>{l('page.proposal_comments.join_discussion_label')}</Button>
         </div>}
-        {comments && comments.length > 0 &&
-          comments.map(comment => <ProposalComment
-            key={comment.created_at}
-            // avatar={comment.avatar_template}
-            user={comment!.username}
+        {renderComments &&
+          comments!.map((comment, index) => <ProposalComment
+            key={'comment_' + index}
+            avatar_url={comment.avatar_url}
+            user={comment.username}
             created_at={comment.created_at}
             cooked={comment.cooked}
           />)
