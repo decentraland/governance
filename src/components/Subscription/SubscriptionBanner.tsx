8ad00b3c499459b8b0349a226298a03e142ec61f
jparnaudo <juampiarnaudo@gmail.com>
2021-11-26 11:57:55 -0300

    Add email outline icon

--- src/components/Subscription/SubscriptionBanner.tsx
+++ src/components/Subscription/SubscriptionBanner.tsx
@@ -4,13 +4,13 @@ import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 
-const icon = require('../../images/icons/catalyst.svg')
+const icon = require('../../images/icons/email-outline.svg')
 
 export default function SubscriptionBanner() {
   const l = useFormatMessage()
   return <a className="SubscriptionBanner">
     <div className="SubscriptionBanner__Icon">
-      <img src={icon} width="48" height="48" alt="email icon"/>
+      <img src={icon} width="48" height="48" alt="email-outline"/>
     </div>
     <div className="SubscriptionBanner__Description">
       <Paragraph small semiBold>{l(`page.subscription_banner.title`)}</Paragraph>
