0312313e5244773c9855d6a5ce8b177c3555a7b3
jparnaudo <juampiarnaudo@gmail.com>
2021-11-26 11:21:43 -0300

    Subscription banner WIP

---
+++ src/components/Subscription/SubscriptionBanner.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './SubscriptionBanner.css'
+import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
+
+const icon = require('../../images/icons/catalyst.svg')
+
+export default function SubscriptionBanner() {
+  const l = useFormatMessage()
+  return <a className="SubscriptionBanner">
+    <div className="SubscriptionBanner__Icon">
+      <img src={icon} width="48" height="48" alt="email icon"/>
+    </div>
+    <div className="SubscriptionBanner__Description">
+      <Paragraph small semiBold>{l(`page.subscription_banner.title`)}</Paragraph>
+      <Paragraph tiny>{l(`page.subscription_banner.description`)}</Paragraph>
+    </div>
+    <Button className="SubscriptionBanner__Button" primary size="small">
+      {l(`page.subscription_banner.subscribe_button_label`)}
+    </Button>
+  </a>
+}
