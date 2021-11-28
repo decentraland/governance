a9ac3bcf0320b83030f369188f7a745ddfd36f30
jparnaudo <juampiarnaudo@gmail.com>
2021-11-28 14:36:53 -0300

    SubscriptionBanner with modal WIP

--- src/components/Modal/NewsletterSubscriptionModal.tsx
+++ src/components/Modal/NewsletterSubscriptionModal.tsx
@@ -3,12 +3,14 @@ import { Modal, ModalProps} from 'decentraland-ui/dist/components/Modal/Modal'
 import { Header } from 'decentraland-ui/dist/components/Header/Header'
 import { Close } from 'decentraland-ui/dist/components/Close/Close'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Field } from "decentraland-ui/dist/components/Field/Field"
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 
 import './ProposalModal.css'
 import './NewsletterSubscriptionModal.css'
+import Label from 'decentraland-gatsby/dist/components/Form/Label'
 
 export type NewsletterSubscriptionModalProps = Omit<ModalProps, 'children'> & {
   loading?: boolean
@@ -17,8 +19,9 @@ export type NewsletterSubscriptionModalProps = Omit<ModalProps, 'children'> & {
 
 export function NewsletterSubscriptionModal({ onClickAccept, loading, ...props }: NewsletterSubscriptionModalProps) {
   const l = useFormatMessage()
-  const [isValid, setIsValid] = useState(false);
+  const [isValid, setIsValid] = useState(true);
   const [message, setMessage] = useState('');
+  const [ email, setEmail ] = useState('')
 
   const emailRegex = /\S+@\S+\.\S+/;
 
@@ -26,29 +29,31 @@ export function NewsletterSubscriptionModal({ onClickAccept, loading, ...props }
     const email = event.target.value;
     if (emailRegex.test(email)) {
       setIsValid(true);
-      setMessage('Your email looks good!');
+      setMessage('');
+      setEmail(email)
     } else {
       setIsValid(false);
-      setMessage('Please enter a valid email');
+      setMessage(l('modal.newsletter_subscription.email_error_message') || '');
+      setEmail(email)
     }
   };
 
-  return <Modal {...props} size="tiny" className={TokenList.join(['ProposalModal', props.className])} closeIcon={<Close />}>
-    <Modal.Content className="ProposalModal__Title">
+  return <Modal {...props} size="tiny" className={TokenList.join(['ProposalModal', "NewsletterSubscriptionModal"])} closeIcon={<Close />}>
+    <Modal.Content className="ProposalModal__Title NewsletterSubscriptionModal__Title">
       <Header>{l('modal.newsletter_subscription.title')}</Header>
-      <Paragraph small>{l('modal.newsletter_subscription.description')}</Paragraph>
+      <Paragraph small className="NewsletterSubscriptionModal__Description">{l('modal.newsletter_subscription.description')}</Paragraph>
       <Paragraph small>{l('modal.newsletter_subscription.description_sub')}</Paragraph>
     </Modal.Content>
     <Modal.Content className="ProposalModal__Form">
-      <input
+      <Label>{l('modal.newsletter_subscription.email_label')}</Label>
+      <Field
         type="email"
-        placeholder="Enter your email"
-        className="NewsletterSubscriptionModal__email-input"
+        value={email}
+        placeholder={l('modal.newsletter_subscription.email_placeholder')}
         onChange={validateEmail}
+        message={message}
+        error={!isValid}
       />
-      <div className={`NewsletterSubscriptionModal__message ${isValid ? 'success' : 'error'}`}>
-        {message}
-      </div>
     </Modal.Content>
     <Modal.Content className="ProposalModal__Actions">
       <Button primary onClick={onClickAccept} loading={loading}>{l('modal.newsletter_subscription.accept')}</Button>
