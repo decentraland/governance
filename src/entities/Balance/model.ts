25c4f29af508aa80523d45c4f3c006718652c77d
jparnaudo <juampiarnaudo@gmail.com>
2021-12-28 22:32:04 -0300

    Add methods to find the latest balances

--- src/entities/Balance/model.ts
+++ src/entities/Balance/model.ts
@@ -7,17 +7,38 @@ export default class BalanceModel extends Model<BalanceAttributes> {
   static withTimestamps = false
   static primaryKey = 'id'
 
-  static async findLatest(wallet_id:string, token_id:string):Promise<BalanceAttributes | undefined> {
+  static async findLatest(wallet_id: string, token_id: string): Promise<BalanceAttributes | undefined> {
     const query = SQL`
-      SELECT b.*
-      FROM ${table(BalanceModel)} b
-      WHERE
-        "wallet_id" = ${wallet_id}
-        AND "token_id" = ${token_id}
-      ORDER BY "created_at" DESC
-      LIMIT 1
+        SELECT b.*
+        FROM ${table(BalanceModel)} b
+        WHERE "wallet_id" = ${wallet_id}
+          AND "token_id" = ${token_id}
+        ORDER BY "created_at" DESC
+        LIMIT 1
     `
     let result = await this.query<BalanceAttributes>(query)
     return result.pop()
+  }
+
+  static async findLatestBalances(): Promise<BalanceAttributes[]> {
+    const query = SQL`
+        SELECT b.*
+        FROM public.balances as b
+                 INNER JOIN
+             (SELECT b.wallet_id,
+                     b.token_id,
+                     max(b.created_at) as max_time
+              FROM public.balances as b
+                       INNER JOIN public.wallets as w
+                                  ON w.id = b.wallet_id
+                       INNER JOIN public.tokens as t
+                                  ON t.id = b.token_id
+              WHERE t.network = w.network
+              GROUP BY b.wallet_id, b.token_id) as latestBalances
+             ON b.wallet_id = latestBalances.wallet_id
+                 AND b.token_id = latestBalances.token_id
+                 AND b.created_at = latestBalances.max_time;
+    `
+    return await this.query<BalanceAttributes>(query)
   }
 }
