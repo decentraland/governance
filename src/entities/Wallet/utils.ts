3168a408cc6a0b5b49a5e12c97d9b01c4a7dcf16
jparnaudo <juampiarnaudo@gmail.com>
2022-01-03 14:27:48 -0300

    - Remove block explorer addresses from env vars

--- src/entities/Wallet/utils.ts
+++ src/entities/Wallet/utils.ts
@@ -3,6 +3,9 @@ import { BlockExplorerLink, WalletAttributes } from './types'
 import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { TokenBalance } from '../Balance/types'
 
+const ETHERSCAN_BASE_URL='https://etherscan.io/'
+const POLYGON_BASE_URL='https://polygonscan.com/'
+
 export function formattedTokenBalance(tokenBalance: TokenBalance) {
   return parseInt(tokenBalance.amount) / 10 ** tokenBalance.decimals
 }
@@ -11,9 +14,9 @@ export function blockExplorerLink(wallet:WalletAttributes):BlockExplorerLink {
   const addressUrl = 'address/' + wallet.address
   switch (wallet.network){
     case ChainId.ETHEREUM_MAINNET:
-      return {name: 'Etherscan', link: process.env.ETHERSCAN_BASE_URL + addressUrl }
+      return {name: 'Etherscan', link: ETHERSCAN_BASE_URL + addressUrl }
     case ChainId.MATIC_MAINNET:
-      return {name: 'Polygonscan', link: process.env.POLYGON_BASE_URL + addressUrl }
+      return {name: 'Polygonscan', link: POLYGON_BASE_URL + addressUrl }
     default:
       logger.error('Unable to get block explorer link', {wallet: wallet})
       return {name: '', link: '/' }
