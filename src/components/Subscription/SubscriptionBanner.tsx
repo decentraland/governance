9815ad7225b6808fd602e56eee19cddfcccd79f6
lemu <juampiarnaudo@gmail.com>
2022-02-10 12:11:21 -0300

    166 - Add close to newsletter modal (#172)
    * Add close button to Subscription Banner
    
    * Improve styling for mobile
    
    * Add z-index to close button
    
    * Add useCallback to handleClose fn

--- src/components/Subscription/SubscriptionBanner.tsx
+++ src/components/Subscription/SubscriptionBanner.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import './SubscriptionBanner.css'
+import React, { useEffect, useState, useCallback } from 'react'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Close } from 'decentraland-ui/dist/components/Close/Close'
 import useAuthContext from 'decentraland-gatsby/dist/context/Auth/useAuthContext'
 import {
   NewsletterSubscriptionModal,
   NEWSLETTER_SUBSCRIPTION_KEY,
   ANONYMOUS_USR_SUBSCRIPTION
 } from '../Modal/NewsletterSubscriptionModal'
+import './SubscriptionBanner.css'
 
 const icon = require('../../images/icons/email-outline.svg')
+const HIDE_NEWSLETTER_SUBSCRIPTION_KEY = 'org.decentraland.governance.newsletter_subscription.hide'
 
 enum ShowSubscriptionBanner {
   YES,
@@ -30,7 +32,10 @@ export default function SubscriptionBanner({active}: SubscriptionBannerProps) {
 
   useEffect(() => {
     const subscriptions:string[] = JSON.parse(localStorage.getItem(NEWSLETTER_SUBSCRIPTION_KEY) || '[]');
-    const showSubsBanner = !account && !subscriptions.includes(ANONYMOUS_USR_SUBSCRIPTION) || account && !subscriptions.includes(account)
+    const showSubsBanner =
+      localStorage.getItem(HIDE_NEWSLETTER_SUBSCRIPTION_KEY) != 'true' &&
+      (!account && !subscriptions.includes(ANONYMOUS_USR_SUBSCRIPTION) ||
+      account && !subscriptions.includes(account))
     setShowSubscriptionBanner(showSubsBanner ? ShowSubscriptionBanner.YES : ShowSubscriptionBanner.NO)
   }, [account])
 
@@ -44,18 +49,30 @@ export default function SubscriptionBanner({active}: SubscriptionBannerProps) {
     setSubscribed(false)
   }
 
+  const handleClose = useCallback((e: React.MouseEvent<any>) => {
+    e.preventDefault()
+    e.stopPropagation()
+    localStorage.setItem(HIDE_NEWSLETTER_SUBSCRIPTION_KEY, 'true')
+    setShowSubscriptionBanner(ShowSubscriptionBanner.NO)
+  }, [])
+
   return <div>
     {showSubscriptionBanner == ShowSubscriptionBanner.YES && active && <a className="SubscriptionBanner">
       <div className="SubscriptionBanner__Icon">
         <img src={icon} width="48" height="48" alt="email-outline" />
       </div>
-      <div className="SubscriptionBanner__Description">
-        <Paragraph small semiBold>{l(`page.subscription_banner.title`)}</Paragraph>
-        <Paragraph tiny>{l(`page.subscription_banner.description`)}</Paragraph>
+      <div className="SubscriptionBanner__Content">
+        <div className="SubscriptionBanner__Description">
+          <Paragraph small semiBold>{l(`page.subscription_banner.title`)}</Paragraph>
+          <Paragraph tiny>{l(`page.subscription_banner.description`)}</Paragraph>
+        </div>
+        <div className="SubscriptionBanner__ButtonContainer">
+          <Button className="SubscriptionBanner__Button" primary size="small" onClick={() => setConfirmSubscription(true)}>
+            {l(`page.subscription_banner.subscribe_button_label`)}
+          </Button>
+        </div>
       </div>
-      <Button className="SubscriptionBanner__Button" primary size="small" onClick={() => setConfirmSubscription(true)}>
-        {l(`page.subscription_banner.subscribe_button_label`)}
-      </Button>
+      <Close small onClick={handleClose} />
     </a>}
     <NewsletterSubscriptionModal
       open={confirmSubscription}
