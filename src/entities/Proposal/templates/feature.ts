a0f681f21f285bafb9d109376bb5cb5e6ca8e6c8
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-02-13 12:39:08 -0300

    Update feature template text

--- src/entities/Proposal/templates/feature.ts
+++ src/entities/Proposal/templates/feature.ts
@@ -4,13 +4,7 @@ import { formatMarkdown, template } from "./utils";
 export const title = (proposal: NewProposalFeature) => proposal.title.split('\n')[0]
 
 export const description = (proposal: NewProposalFeature) => template`
-Should the following feature request be added?
-
-## Product
-
-${proposal.product}
-
-## Explanation
+## Further Explanation
 
 ${formatMarkdown(proposal.explanation)}
 
