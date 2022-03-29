2eba62179655a76a1e84c4a8bd5eadd13c3a1ee5
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-03-29 11:21:48 -0300

    Update Divider component

--- src/components/Section/Divider.tsx
+++ src/components/Section/Divider.tsx
@@ -1,6 +1,11 @@
-import React from "react"
-import "./Divider.css"
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
+import React from 'react'
+import './Divider.css'
 
-export default function Divider() {
-  return <hr className="Divider" />
+export default function Divider({ className, size }: { className?: string; size?: 'small' }) {
+  return (
+    <div className={TokenList.join([size && `Divider__Container--${size}`, className])}>
+      <hr className="Divider" />
+    </div>
+  )
 }
