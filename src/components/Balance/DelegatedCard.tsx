dd77dfa8aa936da65144bc86b41735ffc1561005
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-04-06 11:32:49 -0300

    Add new styles to delegated card in balance

---
+++ src/components/Balance/DelegatedCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Link from 'decentraland-gatsby/dist/components/Text/Link'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Card } from 'decentraland-ui/dist/components/Card/Card'
+import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+import { Header } from 'decentraland-ui/dist/components/Header/Header'
+import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
+import ActionableLayout from '../Layout/ActionableLayout'
+import Scale from '../Icon/Scale'
+import { snapshotUrl } from '../../entities/Proposal/utils'
+import { DelegationResult } from '../../hooks/useDelegation'
+import DelegatedCardProfile from './DelegatedCardProfile'
+import './DelegatedCard.css'
+
+const SNAPSHOT_SPACE = process.env.GATSBY_SNAPSHOT_SPACE || '' // TODO: Move to snapshot utils file
+const EDIT_DELEGATION_URL = snapshotUrl(`#/delegate/${SNAPSHOT_SPACE}`) // TODO: Move to snapshot utils file
+
+interface Props {
+  delegation: DelegationResult
+  account: string | null
+  accountBalance: string | null
+}
+
+const DelegatedCard = ({ delegation, account, accountBalance }: Props) => {
+  const l = useFormatMessage()
+
+  return (
+    <ActionableLayout
+      rightAction={
+        <Button basic as={Link} href={EDIT_DELEGATION_URL} className="screenOnly">
+          {l(`page.balance.delegations_from_action`)}
+          <Icon name="chevron right" />
+        </Button>
+      }
+    >
+      <Card>
+        <Card.Content className="DelegatedCard">
+          {delegation.delegatedTo.length === 0 && (
+            <div className="DelegatedCard__Empty">
+              <Scale />
+              <Header>{l('page.balance.delegations_from_empty_title')}</Header>
+              <Markdown
+                className="DelegatedCard__EmptyDescription"
+                source={
+                  l(
+                    account === accountBalance
+                      ? `page.balance.delegations_from_you_empty_description`
+                      : `page.balance.delegations_from_address_empty`
+                  ) || ''
+                }
+              />
+              <Button basic>{l('page.balance.delegations_from_delegate_vp')}</Button>
+            </div>
+          )}
+          {delegation.delegatedTo.length > 0 && (
+            <>
+              <Header>{l(`page.balance.delegations_from_title`)}</Header>
+              {delegation.delegatedTo.map((delegation) => {
+                return (
+                  <DelegatedCardProfile
+                    key={[delegation.delegate, delegation.delegator].join('::')}
+                    address={delegation.delegate}
+                  />
+                )
+              })}
+            </>
+          )}
+        </Card.Content>
+      </Card>
+    </ActionableLayout>
+  )
+}
+
+export default DelegatedCard
---
+++ src/components/Balance/DelegatedCardProfile.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Blockie } from 'decentraland-ui/dist/components/Blockie/Blockie'
+import { Address } from 'decentraland-ui/dist/components/Address/Address'
+import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import profiles, { Profile } from 'decentraland-gatsby/dist/utils/loader/profile'
+import useAsyncMemo from 'decentraland-gatsby/dist/hooks/useAsyncMemo'
+import Avatar from 'decentraland-gatsby/dist/components/User/Avatar'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+import { Link } from 'gatsby-plugin-intl'
+import locations from '../../modules/locations'
+import './DelegatedCardProfile.css'
+
+export type DelegatedCardProfileProps = {
+  address: string
+}
+
+export default function DelegatedCardProfile({ address }: DelegatedCardProfileProps) {
+  const l = useFormatMessage()
+  const [profile, profileState] = useAsyncMemo<Profile>(() => profiles.load(address || ''), [address])
+
+  if (!address) {
+    return null
+  }
+
+  const isProfile = !!profile && !profile.isDefaultProfile
+  const href = locations.balance({ address })
+
+  return (
+    <div className="DelegatedCardProfile">
+      {!isProfile && (
+        <Blockie seed={address!}>
+          <Address value={address!} strong />
+        </Blockie>
+      )}
+      {isProfile && (
+        <div className="DelegatedCardProfile__ProfileContainer">
+          <Avatar className="DelegatedCardProfile__ProfileAvatar" address={profile!.ethAddress} size="big" />
+          <span className="DelegatedCardProfile__ProfileName">{profile!.name}</span>
+          <Link to={href}>{l('page.balance.delegations_from_view_profile')}</Link>
+        </div>
+      )}
+      <Loader size="small" active={profileState.loading} />
+    </div>
+  )
+}
