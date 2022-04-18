fb31db0dd8fc4f0593ab69dda76b4c796f80d310
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-04-18 11:31:20 -0300

    Add picked by and voting power to delegation from card

--- src/components/Delegation/DelegatedFromUserCard.tsx
+++ src/components/Delegation/DelegatedFromUserCard.tsx
@@ -8,7 +8,8 @@ import { Header } from 'decentraland-ui/dist/components/Header/Header'
 import ActionableLayout from '../Layout/ActionableLayout'
 import Scale from '../Icon/Scale'
 import { snapshotUrl } from '../../entities/Proposal/utils'
-import { DelegationResult } from '../../hooks/useDelegation'
+import useDelegation, { DelegationResult } from '../../hooks/useDelegation'
+import useDelegatedVotingPower from '../../hooks/useDelegatedVotingPower'
 import DelegatedCardProfile from './DelegatedCardProfile'
 import Empty from '../Common/Empty'
 import './DelegatedFromUserCard.css'
@@ -24,13 +25,20 @@ interface DelegatedFromUserCardProps {
 const DelegatedFromUserCard = ({ isLoggedUserProfile, delegation }: DelegatedFromUserCardProps) => {
   const t = useFormatMessage()
 
+  const address = delegation?.delegatedTo?.length > 0 ? delegation?.delegatedTo[0].delegate : null
+  const [delegateDelegations] = useDelegation(address)
+  const { delegatedVotingPower } = useDelegatedVotingPower(delegateDelegations.delegatedFrom)
+
   return (
-    <ActionableLayout className="DelegatedFromUserCard"
+    <ActionableLayout
+      className="DelegatedFromUserCard"
       rightAction={
-        isLoggedUserProfile && <Button basic as={Link} href={EDIT_DELEGATION_URL} >
-          {t(`page.balance.delegations_from_action`)}
-          <Icon name="chevron right" />
-        </Button>
+        isLoggedUserProfile && (
+          <Button basic as={Link} href={EDIT_DELEGATION_URL}>
+            {t(`page.balance.delegations_from_action`)}
+            <Icon name="chevron right" />
+          </Button>
+        )
       }
     >
       <Card>
@@ -52,17 +60,20 @@ const DelegatedFromUserCard = ({ isLoggedUserProfile, delegation }: DelegatedFro
           )}
           {delegation.delegatedTo.length > 0 && (
             <>
-              <Header>{
-                t(
-                  isLoggedUserProfile ?
-                    'page.balance.delegations_from_user_title' :
-                    'page.balance.delegations_from_address_title'
-                )}</Header>
+              <Header>
+                {t(
+                  isLoggedUserProfile
+                    ? 'page.balance.delegations_from_user_title'
+                    : 'page.balance.delegations_from_address_title'
+                )}
+              </Header>
               {delegation.delegatedTo.map((delegation) => {
                 return (
                   <DelegatedCardProfile
                     key={[delegation.delegate, delegation.delegator].join('::')}
                     address={delegation.delegate}
+                    pickedBy={delegateDelegations?.delegatedFrom.length - 1}
+                    votingPower={delegatedVotingPower}
                   />
                 )
               })}
