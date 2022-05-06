c3532d03e83a8d54d5c555429b047f4a66a821e8
Andy Espagnolo <andyespagnolo@gmail.com>
2022-05-06 11:36:22 -0300

    Add loader to delegation detail modal + style fixes (#286)
    * Fix vp distribution popup error
    
    * Decrease margin in detail header
    
    * Add loader to candidates detail modal
    
    * Clear timeout on hook cleanup
    
    * Move text to intl file

--- src/components/Modal/VotingPowerDelegationDetail/VotingPowerDelegationDetail.tsx
+++ src/components/Modal/VotingPowerDelegationDetail/VotingPowerDelegationDetail.tsx
@@ -9,6 +9,7 @@ import useManaBalance from 'decentraland-gatsby/dist/hooks/useManaBalance'
 import Time from 'decentraland-gatsby/dist/utils/date/Time'
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
 import { Modal } from 'decentraland-ui/dist/components/Modal/Modal'
 import { Stats } from 'decentraland-ui/dist/components/Stats/Stats'
 import { Popup } from 'decentraland-ui/dist/components/Popup/Popup'
@@ -41,30 +42,36 @@ type VotingPowerDelegationDetailProps = {
   onBackClick: () => void
 }
 
+let timeout: ReturnType<typeof setTimeout>
+
 function VotingPowerDelegationDetail({ userVotes, candidate, onBackClick }: VotingPowerDelegationDetailProps) {
   const t = useFormatMessage()
   const { address } = candidate
-  const { votingPower } = useVotingPowerBalance(address)
-  const [delegation] = useDelegation(address)
-  const { delegatedVotingPower } = useDelegatedVotingPower(delegation.delegatedFrom)
-  const [mainnetMana] = useManaBalance(address, ChainId.ETHEREUM_MAINNET)
-  const [maticMana] = useManaBalance(address, ChainId.MATIC_MAINNET)
+  const { votingPower, isLoadingVotingPower } = useVotingPowerBalance(address)
+  const [delegation, delegationState] = useDelegation(address)
+  const { delegatedVotingPower, isLoadingScores } = useDelegatedVotingPower(delegation.delegatedFrom)
+  const [mainnetMana, mainnetManaState] = useManaBalance(address, ChainId.ETHEREUM_MAINNET)
+  const [maticMana, maticManaState] = useManaBalance(address, ChainId.MATIC_MAINNET)
   const wManaContract = useWManaContract()
-  const [wMana] = useBalanceOf(wManaContract, address, 'ether')
-  const [land] = useLandBalance(address, ChainId.ETHEREUM_MAINNET)
-  const [ens] = useEnsBalance(address, ChainId.ETHEREUM_MAINNET)
-  const [candidateVotes] = useAsyncMemo(async () => Governance.get().getAddressVotes(address), [])
+  const [wMana, wManaState] = useBalanceOf(wManaContract, address, 'ether')
+  const [land, landState] = useLandBalance(address, ChainId.ETHEREUM_MAINNET)
+  const [ens, ensState] = useEnsBalance(address, ChainId.ETHEREUM_MAINNET)
+  const [candidateVotes, candidateVotesState] = useAsyncMemo(async () => Governance.get().getAddressVotes(address), [])
   const [isExpanded, setIsExpanded] = useState(false)
   const [matchingVotes, setMatchingVotes] = useState<MatchResult | null>(null)
   const [showFadeout, setShowFadeout] = useState(true)
 
   useEffect(() => {
     if (!isExpanded) {
-      setTimeout(() => {
+      timeout = setTimeout(() => {
         setShowFadeout(true)
       }, 500)
     } else {
       setShowFadeout(false)
+    }
+
+    return () => {
+      clearTimeout(timeout)
     }
   }, [isExpanded])
 
@@ -76,6 +83,17 @@ function VotingPowerDelegationDetail({ userVotes, candidate, onBackClick }: Voti
 
   const mana = mainnetMana + maticMana + (wMana || 0)
   const ownVotingPower = votingPower - delegatedVotingPower
+
+  const isLoading =
+    delegationState.loading ||
+    isLoadingVotingPower ||
+    isLoadingScores ||
+    mainnetManaState.loading ||
+    maticManaState.loading ||
+    wManaState.loading ||
+    landState.loading ||
+    ensState.loading ||
+    candidateVotesState.loading
 
   return (
     <>
@@ -120,68 +138,81 @@ function VotingPowerDelegationDetail({ userVotes, candidate, onBackClick }: Voti
             {t(`modal.vp_delegation.details.${!isExpanded ? 'show_more' : 'show_less'}`)}
           </Button>
         </div>
-        <Grid columns={3}>
-          <Grid.Row>
-            <Grid.Column>
-              <Stats title={t('modal.vp_delegation.details.stats_own_voting_power')}>
-                <VotingPower value={ownVotingPower} size="large" />
-              </Stats>
-            </Grid.Column>
-            <Grid.Column>
-              <Stats title={t('modal.vp_delegation.details.stats_delegated_voting_power')}>
-                <VotingPower value={delegatedVotingPower} size="large" />
-              </Stats>
-            </Grid.Column>
-            <Grid.Column>
-              <Stats title={t('modal.vp_delegation.details.stats_total_voting_power')}>
-                <VotingPower value={votingPower} size="large" />
-              </Stats>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <Stats title={t('modal.vp_delegation.details.stats_mana')}>
-                <VotingPower value={Math.floor(mana)} size="medium" />
-              </Stats>
-            </Grid.Column>
-            <Grid.Column>
-              <Stats title={t('modal.vp_delegation.details.stats_land')}>
-                <VotingPower value={land! * LAND_MULTIPLIER} size="medium" />
-              </Stats>
-            </Grid.Column>
-            <Grid.Column>
-              <Stats title={t('modal.vp_delegation.details.stats_name')}>
-                <VotingPower value={ens * NAME_MULTIPLIER} size="medium" />
-              </Stats>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns="1">
-            <Grid.Column>
-              <VotingPowerDistribution
-                mana={mana}
-                name={ens * NAME_MULTIPLIER}
-                land={land * LAND_MULTIPLIER}
-                delegated={delegatedVotingPower}
-              />
-            </Grid.Column>
-          </Grid.Row>
-          {candidateVotes && (
+        {isLoading && (
+          <Grid columns={1}>
             <Grid.Row>
-              {candidateVotes.length > 0 && (
+              <div className="VotingPowerDelegationDetail__Loading">
+                <Loader size="big" />
+                <span className="VotingPowerDelegationDetail__LoaderText">
+                  {t('modal.vp_delegation.details.stats_loading')}
+                </span>
+              </div>
+            </Grid.Row>
+          </Grid>
+        )}
+        {!isLoading && (
+          <>
+            <Grid columns={3}>
+              <Grid.Row>
                 <Grid.Column>
-                  <Stats title={t('modal.vp_delegation.details.stats_active_since')}>
-                    <div className="VotingPowerDelegationDetail__StatsValue">
-                      {Time.unix(candidateVotes[candidateVotes.length - 1].created).format('MMMM, YYYY')}
-                    </div>
+                  <Stats title={t('modal.vp_delegation.details.stats_own_voting_power')}>
+                    <VotingPower value={ownVotingPower} size="large" />
                   </Stats>
                 </Grid.Column>
-              )}
-              <Grid.Column>
-                <Stats title={t('modal.vp_delegation.details.stats_voted_on')}>
-                  <div className="VotingPowerDelegationDetail__StatsValue">{candidateVotes.length}</div>
-                </Stats>
-              </Grid.Column>
-              {matchingVotes && (
+                <Grid.Column>
+                  <Stats title={t('modal.vp_delegation.details.stats_delegated_voting_power')}>
+                    <VotingPower value={delegatedVotingPower} size="large" />
+                  </Stats>
+                </Grid.Column>
+                <Grid.Column>
+                  <Stats title={t('modal.vp_delegation.details.stats_total_voting_power')}>
+                    <VotingPower value={votingPower} size="large" />
+                  </Stats>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Stats title={t('modal.vp_delegation.details.stats_mana')}>
+                    <VotingPower value={Math.floor(mana)} size="medium" />
+                  </Stats>
+                </Grid.Column>
+                <Grid.Column>
+                  <Stats title={t('modal.vp_delegation.details.stats_land')}>
+                    <VotingPower value={land! * LAND_MULTIPLIER} size="medium" />
+                  </Stats>
+                </Grid.Column>
+                <Grid.Column>
+                  <Stats title={t('modal.vp_delegation.details.stats_name')}>
+                    <VotingPower value={ens * NAME_MULTIPLIER} size="medium" />
+                  </Stats>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="1">
+                <Grid.Column>
+                  <VotingPowerDistribution
+                    mana={mana}
+                    name={ens * NAME_MULTIPLIER}
+                    land={land * LAND_MULTIPLIER}
+                    delegated={delegatedVotingPower}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+              {candidateVotes && (
+                <Grid.Row>
+                  {candidateVotes.length > 0 && (
+                    <Grid.Column>
+                      <Stats title={t('modal.vp_delegation.details.stats_active_since')}>
+                        <div className="VotingPowerDelegationDetail__StatsValue">
+                          {Time.unix(candidateVotes[candidateVotes.length - 1].created).format('MMMM, YYYY')}
+                        </div>
+                      </Stats>
+                    </Grid.Column>
+                  )}
+                  <Grid.Column>
+                    <Stats title={t('modal.vp_delegation.details.stats_voted_on')}>
+                      <div className="VotingPowerDelegationDetail__StatsValue">{candidateVotes.length}</div>
+                    </Stats>
+                  </Grid.Column>{matchingVotes && (
                 <Grid.Column>
                   <Stats title={t('modal.vp_delegation.details.stats_match')}>
                     <Popup
@@ -203,21 +234,23 @@ function VotingPowerDelegationDetail({ userVotes, candidate, onBackClick }: Voti
                   </Stats>
                 </Grid.Column>
               )}
-            </Grid.Row>
-          )}
-        </Grid>
-        {candidateVotes && candidateVotes.length > 0 && (
-          <div className="VotingPowerDelegationDetail__Initiatives">
-            <span className="VotingPowerDelegationDetail__InitiativesTitle">
-              {t('modal.vp_delegation.details.stats_initiatives_title')}
-            </span>
-            <div className="VotingPowerDelegationDetail__InitiativesList">
-              {candidateVotes.map((item) => {
-                const match = matchingVotes?.matches.find((p) => p.proposal_id === item.proposal.id)
-                return <VotedInitiative key={item.id} vote={item} voteMatch={match?.sameVote} />
-              })}
-            </div>
-          </div>
+                </Grid.Row>
+              )}
+            </Grid>
+            {candidateVotes && candidateVotes.length > 0 && (
+              <div className="VotingPowerDelegationDetail__Initiatives">
+                <span className="VotingPowerDelegationDetail__InitiativesTitle">
+                  {t('modal.vp_delegation.details.stats_initiatives_title')}
+                </span>
+                <div className="VotingPowerDelegationDetail__InitiativesList">
+                  {candidateVotes.map((item) => {
+                    const match = matchingVotes?.matches.find((p) => p.proposal_id === item.proposal.id)
+                return <VotedInitiative key={item.id} vote={item} voteMatch={match?.sameVote}/>
+                  })}
+                </div>
+              </div>
+            )}
+          </>
         )}
       </Modal.Content>
     </>
