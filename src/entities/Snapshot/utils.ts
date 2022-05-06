95f0d3eaefa96c7d6940a366d1f8333ca155e7b2
jparnaudo <juampiarnaudo@gmail.com>
2022-05-06 13:33:18 -0300

    Match fn and component refactor

--- src/entities/Snapshot/utils.ts
+++ src/entities/Snapshot/utils.ts
@@ -1,39 +1,36 @@
-import { Wallet } from '@ethersproject/wallet';
-import { intersection } from 'lodash';
-import { SnapshotVote } from '../../api/Snapshot';
+import { Wallet } from '@ethersproject/wallet'
+
+import { SnapshotVote } from '../../api/Snapshot'
 
 export interface MatchResult {
-  percentage: number,
-  matches: { proposal_id: string, sameVote: boolean }[]
+  percentage: number
+  matches: { proposal_id: string; sameVote: boolean }[]
 }
 
 export async function signMessage(wallet: Wallet, msg: string) {
-  return wallet.signMessage(Buffer.from(msg, 'utf8'));
+  return wallet.signMessage(Buffer.from(msg, 'utf8'))
 }
 
 export function calculateMatch(votes1: SnapshotVote[], votes2: SnapshotVote[]): MatchResult {
-
   const match: MatchResult = { percentage: 0, matches: [] }
-  const commonProposalIds = intersection(votes1.map(vote => vote.proposal.id), votes2.map(vote => vote.proposal.id))
 
-  if (commonProposalIds.length !== 0) {
-
-    let matchCounter = 0
-    for (const proposalId of commonProposalIds) {
-      const vote1 = votes1.find(v => v.proposal.id === proposalId)!
-      const vote2 = votes2.find(v => v.proposal.id === proposalId)!
-
+  let matchCounter = 0
+  let proposalsInCommon = 0
+  for (const vote1 of votes1) {
+    const proposalId = vote1.proposal.id
+    const vote2 = votes2.find((v) => v.proposal.id === proposalId)
+    if (vote2) {
+      proposalsInCommon++
       if (vote1.choice === vote2.choice) {
         matchCounter++
         match.matches.push({ proposal_id: proposalId, sameVote: true })
-      }
-      else {
+      } else {
         match.matches.push({ proposal_id: proposalId, sameVote: false })
       }
     }
-
-    match.percentage = Math.round(matchCounter / commonProposalIds.length * 100)
   }
-
+  if (proposalsInCommon > 0) {
+    match.percentage = Math.round((matchCounter / proposalsInCommon) * 100)
+  }
   return match
 }
