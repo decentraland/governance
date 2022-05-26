f140b00ea93432370e891b1ef58073ab03c80bd6
Nicolás Comerci <ncomerci@itba.edu.ar>
2022-05-26 09:22:14 -0300

    comment fixes

--- src/components/Section/VestingSection.tsx
+++ src/components/Section/VestingSection.tsx
@@ -4,6 +4,7 @@ import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 
+import './DetailsSection.css'
 import './VestingSection.css'
 
 interface Props {
@@ -12,22 +13,24 @@ interface Props {
 
 function VestingSection({ vestingAddress }: Props) {
   const vestingUrl = process.env.GATSBY_VESTING_DASHBOARD_URL
+  const t = useFormatMessage()
 
   if (!vestingUrl) {
     console.error('Vesting Dashboard URL not found')
     return <></>
   }
 
-  const t = useFormatMessage()
   const url = vestingUrl.replace('%23', '#').concat(vestingAddress)
 
   return (
     <div className="VestingSection DetailsSection">
-      <div className="VestingLabel">{t('page.proposal_detail.grant.vesting_label')}</div>
-      <Markdown>{t('page.proposal_detail.grant.vesting_description')}</Markdown>
-      <Button href={url} target="_blank" rel="noopener noreferrer" primary size="small">
-        {t('page.proposal_detail.grant.vesting_button')}
-      </Button>
+      <div className="DetailsSection__Content">
+        <div className="VestingLabel">{t('page.proposal_detail.grant.vesting_label')}</div>
+        <Markdown>{t('page.proposal_detail.grant.vesting_description')}</Markdown>
+        <Button href={url} target="_blank" rel="noopener noreferrer" primary size="small">
+          {t('page.proposal_detail.grant.vesting_button')}
+        </Button>
+      </div>
     </div>
   )
 }
