49db8ba96dd985c2e128e066aedf56db56d2f0a1
lemu <juampiarnaudo@gmail.com>
2022-05-26 15:19:34 -0300

    Username fixes (#331)
    * Username improvements & fixes
    Co-authored-by: Nicolás Comerci <ncomerci@itba.edu.ar>

--- src/components/Delegation/DelegatedCardProfile.tsx
+++ src/components/Delegation/DelegatedCardProfile.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
-import Avatar from 'decentraland-gatsby/dist/components/User/Avatar'
+import { Size } from 'decentraland-gatsby/dist/components/Props/types'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Link } from 'decentraland-gatsby/dist/plugins/intl'
-import { Address } from 'decentraland-ui/dist/components/Address/Address'
-import { Blockie } from 'decentraland-ui/dist/components/Blockie/Blockie'
-import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
 
-import useProfile from '../../hooks/useProfile'
 import locations from '../../modules/locations'
 import VotingPower from '../Token/VotingPower'
+import Username from '../User/Username'
 
 import './DelegatedCardProfile.css'
 
@@ -21,41 +18,33 @@ export type DelegatedCardProfileProps = {
 
 export default function DelegatedCardProfile({ address, pickedBy, votingPower }: DelegatedCardProfileProps) {
   const t = useFormatMessage()
-  const { profile, profileState, hasDclProfile } = useProfile(address)
-
   if (!address) {
     return null
   }
 
   return (
     <div className="DelegatedCardProfile">
-      {!hasDclProfile && (
-        <Blockie seed={address!}>
-          <Address value={address!} strong />
-        </Blockie>
-      )}
-      {hasDclProfile && (
-        <div className="DelegatedCardProfile__Container">
-          <Avatar className="DelegatedCardProfile__Avatar" address={profile!.ethAddress} size="big" />
-          <span className="DelegatedCardProfile__Name">{profile!.name}</span>
-          {(!!votingPower || !!pickedBy) && (
-            <div className="DelegatedCardProfile__DescriptionContainer">
-              {!!votingPower && (
-                <span className="DelegatedCardProfile__Description">
-                  {t('page.balance.delegations_from_voting_power')} <VotingPower secondary value={votingPower} />
-                </span>
-              )}
-              {!!pickedBy && (
-                <span className="DelegatedCardProfile__Description">
-                  {t('page.balance.delegations_from_picked_by', { count: pickedBy })}
-                </span>
-              )}
-            </div>
-          )}
-          <Link href={locations.balance({ address })}>{t('page.balance.delegations_from_view_profile')}</Link>
-        </div>
-      )}
-      <Loader size="small" active={profileState.loading} />
+      <div className="DelegatedCardProfile__Container">
+        <Username className="DelegatedCardProfile__Avatar" address={address} size={Size.Big} iconOnly />
+        <span className="DelegatedCardProfile__Name">
+          <Username address={address} addressOnly />
+        </span>
+        {(!!votingPower || !!pickedBy) && (
+          <div className="DelegatedCardProfile__DescriptionContainer">
+            {!!votingPower && (
+              <span className="DelegatedCardProfile__Description">
+                {t('page.balance.delegations_from_voting_power')} <VotingPower secondary value={votingPower} />
+              </span>
+            )}
+            {!!pickedBy && (
+              <span className="DelegatedCardProfile__Description">
+                {t('page.balance.delegations_from_picked_by', { count: pickedBy })}
+              </span>
+            )}
+          </div>
+        )}
+        <Link href={locations.balance({ address })}>{t('page.balance.delegations_from_view_profile')}</Link>
+      </div>
     </div>
   )
 }
