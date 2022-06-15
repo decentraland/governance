6d8ed5e29409c9e80525d9c18447945298269825
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-06-15 16:28:41 -0300

    check if server is running on heroku

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import metrics from 'decentraland-gatsby/dist/entities/Prometheus/routes'
 import RequestError from 'decentraland-gatsby/dist/entities/Route/error'
 import handle from 'decentraland-gatsby/dist/entities/Route/handle'
 import { withBody, withCors, withDDosProtection, withLogs } from 'decentraland-gatsby/dist/entities/Route/middleware'
-import { status } from 'decentraland-gatsby/dist/entities/Route/routes'
+import { filesystem, status } from 'decentraland-gatsby/dist/entities/Route/routes'
 import { initializeServices } from 'decentraland-gatsby/dist/entities/Server/handler'
 import { serverInitializer } from 'decentraland-gatsby/dist/entities/Server/utils'
 import express from 'express'
@@ -58,10 +58,15 @@ app.get(
 
 app.use(sitemap)
 app.use('/', social)
-app.use(express.static('public'))
-app.use(function (_req, res) {
-  res.status(404).sendFile('404/index.html', { root: path.join(__dirname, '../public') })
-})
+
+if (process.env.HEROKU === 'true') {
+  app.use(express.static('public'))
+  app.use(function (_req, res) {
+    res.status(404).sendFile('404/index.html', { root: path.join(__dirname, '../public') })
+  })
+} else {
+  app.use(filesystem('public', '404.html'))
+}
 
 void initializeServices([
   process.env.DATABASE !== 'false' && databaseInitializer(),
