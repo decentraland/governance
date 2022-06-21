4f7d633aae3e935c8c5c8c4837597e6b5f1c8a1d
1emu <github@1emu.sh>
2022-06-21 12:28:08 -0300

    Error message component and internationalization

--- src/components/Error/ErrorMessage.tsx
+++ src/components/Error/ErrorMessage.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 import Link from 'decentraland-gatsby/dist/components/Text/Link'
+import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
+import useClipboardCopy from 'decentraland-gatsby/dist/hooks/useClipboardCopy'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+import Time from 'decentraland-gatsby/dist/utils/date/Time'
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 
 import ErrorNotice from '../Icon/ErrorNotice'
@@ -15,29 +19,40 @@ interface Props {
 
 export default function ErrorMessage({ label, errorMessage }: Props) {
   const t = useFormatMessage()
+  const [copied, state] = useClipboardCopy(Time.Second)
+  const [open, setOpen] = useState(false)
+
+  const toggleHandler = () => {
+    setOpen(!open)
+  }
+
+  const handleCopy = useCallback(() => {
+    state.copy(errorMessage)
+  }, [errorMessage, state])
 
   return (
     <div className="ErrorMessage__Container">
       <div className="ErrorMessage__Header">
         <ErrorNotice />
         <span>{label}</span>
-        <Button className="ErrorMessage__Show" basic as={Link} href={'/'}>
-          {'Hide error'}
+        <Button className="ErrorMessage__Show" basic as={Link} onClick={toggleHandler}>
+          {open ? t('error.message.hide') : t('error.message.show')}
         </Button>
       </div>
-      <div className="ErrorMessage__Message">
-        <pre>{errorMessage}</pre>
-        <div className="ErrorMessage__Copy">
-          <span>{'Copy to Clipboard'}</span>
+      <div className={TokenList.join(['ErrorMessage__Content', open && 'ErrorMessage__Content--open'])}>
+        <div className="ErrorMessage__Message">
+          <pre>{errorMessage}</pre>
+          <Button
+            className={TokenList.join(['Button', 'ErrorMessage__Copy'])}
+            primary
+            size="small"
+            onClick={handleCopy}
+          >
+            <span>{copied ? t('error.message.copied') : t('error.message.copy')}</span>
+          </Button>
         </div>
+        <Markdown className="ErrorMessage__CallForAction">{t('error.message.call_for_action')}</Markdown>
       </div>
-      <span className="ErrorMessage__CallForAction">
-        We rely on an external service that can fail. However, feel free to post this error message on
-        <Link className="ErrorMessage__DiscordLink" href={'/'}>
-          {'Discord'}
-        </Link>
-        .
-      </span>
     </div>
   )
 }
