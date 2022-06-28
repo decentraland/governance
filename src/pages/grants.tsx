36808926115351ac1c6bd9bc1c8f541f5c791f6b
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-06-28 17:54:05 -0300

    feat: filter by category logic

--- src/pages/grants.tsx
+++ src/pages/grants.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
 import Head from 'decentraland-gatsby/dist/components/Head/Head'
 import MaintenancePage from 'decentraland-gatsby/dist/components/Layout/MaintenancePage'
@@ -7,7 +7,7 @@ import useResponsive from 'decentraland-gatsby/dist/hooks/useResponsive'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import { Container } from 'decentraland-ui/dist/components/Container/Container'
 import { Table } from 'decentraland-ui/dist/components/Table/Table'
-import { isEmpty } from 'lodash'
+import { filter, isEmpty } from 'lodash'
 import Responsive from 'semantic-ui-react/dist/commonjs/addons/Responsive'
 
 import Banner, { BannerType } from '../components/Grants/Banner'
@@ -18,22 +18,16 @@ import BurgerMenuContent from '../components/Layout/BurgerMenu/BurgerMenuContent
 import BurgerMenuPushableLayout from '../components/Layout/BurgerMenu/BurgerMenuPushableLayout'
 import LoadingView from '../components/Layout/LoadingView'
 import Navigation, { NavigationTab } from '../components/Layout/Navigation'
-import { GrantAttributes, GrantWithUpdateAttributes } from '../entities/Proposal/types'
+import { GrantAttributes, GrantWithUpdateAttributes, ProposalGrantCategory } from '../entities/Proposal/types'
 import useGrants from '../hooks/useGrants'
 import { isUnderMaintenance } from '../modules/maintenance'
 
 import './grants.css'
 
+const PROPOSAL_GRANT_CATEGORY_ALL = 'All'
+
 const CURRENT_GRANTS_PER_PAGE = 8
 const PAST_GRANTS_PER_PAGE = 10
-
-enum GrantsFilter {
-  All = 'All',
-  Community = 'Community',
-  Platform = 'Platform',
-  Gaming = 'Gaming',
-  Content = 'Content',
-}
 
 export default function GrantsPage() {
   const t = useFormatMessage()
@@ -43,21 +37,49 @@ export default function GrantsPage() {
   const { grants, isLoadingGrants } = useGrants()
   const [filteredCurrentGrants, setFilteredCurrentGrants] = useState<GrantWithUpdateAttributes[]>([])
   const [filteredPastGrants, setFilteredPastGrants] = useState<GrantAttributes[]>([])
-  const [selectedCategory, setSelectedCategory] = useState<GrantsFilter>(GrantsFilter.All)
+  const [selectedCategory, setSelectedCategory] = useState<ProposalGrantCategory | typeof PROPOSAL_GRANT_CATEGORY_ALL>(
+    PROPOSAL_GRANT_CATEGORY_ALL
+  )
+
+  const currentGrantsFilteredByCategory = useMemo(
+    () => ({
+      [PROPOSAL_GRANT_CATEGORY_ALL]: grants?.current,
+      [ProposalGrantCategory.Community]: filter(
+        grants.current,
+        (item) => item.configuration.category === ProposalGrantCategory.Community
+      ),
+      [ProposalGrantCategory.Gaming]: filter(
+        grants.current,
+        (item) => item.configuration.category === ProposalGrantCategory.Gaming
+      ),
+      [ProposalGrantCategory.PlatformContributor]: filter(
+        grants.current,
+        (item) => item.configuration.category === ProposalGrantCategory.PlatformContributor
+      ),
+      [ProposalGrantCategory.ContentCreator]: filter(
+        grants.current,
+        (item) => item.configuration.category === ProposalGrantCategory.ContentCreator
+      ),
+    }),
+    [grants]
+  )
 
   const handleLoadMoreCurrentGrantsClick = useCallback(() => {
     if (grants) {
-      const newCurrentGrants = grants.current.slice(0, filteredCurrentGrants.length + CURRENT_GRANTS_PER_PAGE)
+      const newCurrentGrants = currentGrantsFilteredByCategory[selectedCategory].slice(
+        0,
+        filteredCurrentGrants.length + CURRENT_GRANTS_PER_PAGE
+      )
       setFilteredCurrentGrants(newCurrentGrants)
     }
-  }, [grants, filteredCurrentGrants.length])
+  }, [grants, currentGrantsFilteredByCategory, selectedCategory, filteredCurrentGrants?.length])
 
   const handleLoadMorePastGrantsClick = useCallback(() => {
     if (grants) {
       const newPastGrants = grants.past.slice(0, filteredPastGrants.length + PAST_GRANTS_PER_PAGE)
       setFilteredPastGrants(newPastGrants)
     }
-  }, [grants, filteredPastGrants.length])
+  }, [grants, filteredPastGrants?.length])
 
   useEffect(() => {
     if (!isEmpty(grants)) {
@@ -65,6 +87,16 @@ export default function GrantsPage() {
       setFilteredPastGrants(grants.past.slice(0, PAST_GRANTS_PER_PAGE))
     }
   }, [grants])
+
+  useEffect(() => {
+    if (grants && selectedCategory) {
+      const newGrants =
+        selectedCategory === PROPOSAL_GRANT_CATEGORY_ALL
+          ? grants?.current?.slice(0, CURRENT_GRANTS_PER_PAGE)
+          : filter(grants?.current, (item) => item.configuration.category === selectedCategory)
+      setFilteredCurrentGrants(newGrants)
+    }
+  }, [grants, selectedCategory])
 
   if (isUnderMaintenance()) {
     return (
@@ -106,7 +138,8 @@ export default function GrantsPage() {
     ]
   }
 
-  const showLoadMoreCurrentGrantsButton = filteredCurrentGrants.length !== grants?.current?.length
+  const showLoadMoreCurrentGrantsButton =
+    filteredCurrentGrants?.length !== currentGrantsFilteredByCategory[selectedCategory]?.length
   const showLoadMorePastGrantsButton = filteredPastGrants.length !== grants?.past?.length
 
   return (
@@ -135,43 +168,43 @@ export default function GrantsPage() {
                     <h2 className="GrantsPage__CurrentGrantsTitle">{t('page.grants.currently_funded')}</h2>
                     <div className="GrantsPage__CurrentGrantsFilters">
                       <FilterButton
-                        selected={selectedCategory === GrantsFilter.All}
-                        onClick={() => setSelectedCategory(GrantsFilter.All)}
-                        count={28}
+                        selected={selectedCategory === PROPOSAL_GRANT_CATEGORY_ALL}
+                        onClick={() => setSelectedCategory(PROPOSAL_GRANT_CATEGORY_ALL)}
+                        count={currentGrantsFilteredByCategory[PROPOSAL_GRANT_CATEGORY_ALL]?.length}
                       >
                         {t('page.grants.category_filters.all')}
                       </FilterButton>
                       <FilterButton
-                        selected={selectedCategory === GrantsFilter.Community}
-                        onClick={() => setSelectedCategory(GrantsFilter.Community)}
-                        count={8}
+                        selected={selectedCategory === ProposalGrantCategory.Community}
+                        onClick={() => setSelectedCategory(ProposalGrantCategory.Community)}
+                        count={currentGrantsFilteredByCategory[ProposalGrantCategory.Community]?.length}
                       >
                         {t('page.grants.category_filters.community')}
                       </FilterButton>
                       <FilterButton
-                        selected={selectedCategory === GrantsFilter.Gaming}
-                        onClick={() => setSelectedCategory(GrantsFilter.Gaming)}
-                        count={3}
+                        selected={selectedCategory === ProposalGrantCategory.Gaming}
+                        onClick={() => setSelectedCategory(ProposalGrantCategory.Gaming)}
+                        count={currentGrantsFilteredByCategory[ProposalGrantCategory.Gaming]?.length}
                       >
                         {t('page.grants.category_filters.gaming')}
                       </FilterButton>
                       <FilterButton
-                        selected={selectedCategory === GrantsFilter.Content}
-                        onClick={() => setSelectedCategory(GrantsFilter.Content)}
-                        count={7}
+                        selected={selectedCategory === ProposalGrantCategory.ContentCreator}
+                        onClick={() => setSelectedCategory(ProposalGrantCategory.ContentCreator)}
+                        count={currentGrantsFilteredByCategory[ProposalGrantCategory.ContentCreator]?.length}
                       >
                         {t('page.grants.category_filters.content')}
                       </FilterButton>
                       <FilterButton
-                        selected={selectedCategory === GrantsFilter.Platform}
-                        onClick={() => setSelectedCategory(GrantsFilter.Platform)}
-                        count={2}
+                        selected={selectedCategory === ProposalGrantCategory.PlatformContributor}
+                        onClick={() => setSelectedCategory(ProposalGrantCategory.PlatformContributor)}
+                        count={currentGrantsFilteredByCategory[ProposalGrantCategory.PlatformContributor]?.length}
                       >
                         {t('page.grants.category_filters.platform')}
                       </FilterButton>
                     </div>
                     <Container className="GrantsPage__CurrentGrantsContainer">
-                      {filteredCurrentGrants.map((grant) => (
+                      {filteredCurrentGrants?.map((grant) => (
                         <GrantCard key={`CurrentGrantCard_${grant.id}`} grant={grant} />
                       ))}
                     </Container>
