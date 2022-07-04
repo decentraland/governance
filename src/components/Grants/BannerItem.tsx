960b21daaf311af13c6d9547952fe60cdb06a867
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-07-04 10:44:57 -0300

    feat: update grants banner styles

--- src/components/Grants/BannerItem.tsx
+++ src/components/Grants/BannerItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import ChevronRight from '../Icon/ChevronRight'
-
 import './BannerItem.css'
 
 interface Props {
@@ -16,7 +14,6 @@ const BannerItem = ({ title, description }: Props) => {
         <h3 className="GrantsBannerItem__Title">{title}</h3>
         <p className="GrantsBannerItem__Description">{description}</p>
       </div>
-      <ChevronRight />
     </div>
   )
 }
