ca8252a9ce1cd4fd9b3803e0a09d5c0a90038f48
Andrés Espagnolo <andyespagnolo@gmail.com>
2022-08-01 13:30:24 -0300

    feat: add transparency metric and move texts to i18n file

--- src/components/Home/MetricsCards.tsx
+++ src/components/Home/MetricsCards.tsx
@@ -1,9 +1,13 @@
-import React, { useRef } from 'react'
+import React, { useMemo, useRef } from 'react'
 import Flickity from 'react-flickity-component'
 
+import useAsyncMemo from 'decentraland-gatsby/dist/hooks/useAsyncMemo'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import useResponsive from 'decentraland-gatsby/dist/hooks/useResponsive'
 import { Container } from 'decentraland-ui/dist/components/Container/Container'
 import Responsive from 'semantic-ui-react/dist/commonjs/addons/Responsive'
+
+import { DclData } from '../../api/DclData'
 
 import MetricsCard from './MetricsCard'
 import './MetricsCards.css'
@@ -19,18 +23,44 @@ const flickityOptions = {
   friction: 0.15,
 }
 
-const MOCK_DATA = [
-  { category: 'Proposals', title: '48 active proposals', description: '5 ending soon' },
-  { category: 'Participation', title: '165 votes this week', description: '542 votes last 30 days' },
-  { category: 'Treasury', title: '$45,467,895.28', description: 'Consolidated in USD' },
-]
-
 const MetricsCards = () => {
   const responsive = useResponsive()
   const isMobile = responsive({ maxWidth: Responsive.onlyMobile.maxWidth })
   const flickity = useRef<Flickity>()
+  const t = useFormatMessage()
+  const [data] = useAsyncMemo(async () => DclData.get().getData())
+  const treasuryAmount = useMemo(
+    () =>
+      data?.balances
+        .reduce((acc, obj) => {
+          return acc + Number(obj.amount) * obj.rate
+        }, 0)
+        .toFixed(2),
+    [data?.balances]
+  )
 
-  const content = MOCK_DATA.map((item) => (
+  const metricsData = useMemo(
+    () => [
+      {
+        category: t('page.home.metrics.proposals'),
+        title: t('page.home.metrics.active_proposals', { value: 48 }),
+        description: t('page.home.metrics.ending_soon', { value: 5 }),
+      },
+      {
+        category: t('page.home.metrics.participation'),
+        title: t('page.home.metrics.votes_this_week', { value: 100 }),
+        description: t('page.home.metrics.votes_last_month', { value: 352 }),
+      },
+      {
+        category: t('page.home.metrics.treasury'),
+        title: `$${t('general.number', { value: treasuryAmount })}`,
+        description: t('page.home.metrics.consolidated'),
+      },
+    ],
+    [t, treasuryAmount]
+  )
+
+  const content = metricsData.map((item) => (
     <MetricsCard key={item.category} category={item.category} title={item.title} description={item.description} />
   ))
 
