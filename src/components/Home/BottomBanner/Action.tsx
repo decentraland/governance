25c7f7c74b1541c1f6bdf7d9b7df87a97af7dd47
lemu <github@lemu.sh>
2022-08-15 13:30:35 -0400

    fix: bottom banner display for all sizes

--- src/components/Home/BottomBanner/Action.tsx
+++ src/components/Home/BottomBanner/Action.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid/Grid'
-
 import './Action.css'
 
 export interface ActionProps {
@@ -14,17 +12,11 @@ export interface ActionProps {
 function Action({ icon, title, description, url }: ActionProps) {
   return (
     <a href={url} target="_blank" rel="noreferrer" className="Action">
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <div className="Action__Icon">{icon}</div>
-          </Grid.Column>
-          <Grid.Column width={13}>
-            <div className="Action__Title">{title}</div>
-            <div className="Action__Description">{description}</div>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <div className="Action__Icon">{icon}</div>
+      <div>
+        <div className="Action__Title">{title}</div>
+        <div className="Action__Description">{description}</div>
+      </div>
     </a>
   )
 }
