b02c7ee54c0366b6e227a0b2be03f190443ed686
lemu <github@lemu.sh>
2022-09-05 18:46:05 -0300

    feat: add profile tab. Rename wrapping (?) to balance

---
+++ src/pages/profile.tsx
@@ -0,0 +1,57 @@
+import React, { useMemo } from 'react'
+
+import { useLocation } from '@gatsbyjs/reach-router'
+import Head from 'decentraland-gatsby/dist/components/Head/Head'
+import MaintenancePage from 'decentraland-gatsby/dist/components/Layout/MaintenancePage'
+import useAuthContext from 'decentraland-gatsby/dist/context/Auth/useAuthContext'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+import { Container } from 'decentraland-ui/dist/components/Container/Container'
+import isEthereumAddress from 'validator/lib/isEthereumAddress'
+
+import LoadingView from '../components/Layout/LoadingView'
+import Navigation, { NavigationTab } from '../components/Layout/Navigation'
+import LogIn from '../components/User/LogIn'
+import { isUnderMaintenance } from '../modules/maintenance'
+
+export default function ProfilePage() {
+  const t = useFormatMessage()
+  const location = useLocation()
+  const params = useMemo(() => new URLSearchParams(location.search), [location.search])
+  const [userAddress, authState] = useAuthContext()
+  const address = isEthereumAddress(params.get('address') || '') ? params.get('address') : userAddress
+  const isLoggedUserProfile = userAddress === address
+
+  if (isUnderMaintenance()) {
+    return (
+      <>
+        <Head
+          title={t('page.balance.title') || ''}
+          description={t('page.balance.description') || ''}
+          image="https://decentraland.org/images/decentraland.png"
+        />
+        <Navigation activeTab={NavigationTab.Profile} />
+        <MaintenancePage />
+      </>
+    )
+  }
+
+  if (authState.loading) {
+    return <LoadingView />
+  }
+
+  if (!userAddress) {
+    return <LogIn title={t('page.profile.title') || ''} description={t('page.profile.description') || ''} />
+  }
+
+  return (
+    <div className="BalancePage">
+      <Head
+        title={t('page.profile.title') || ''}
+        description={t('page.profile.description') || ''}
+        image="https://decentraland.org/images/decentraland.png"
+      />
+      <Navigation activeTab={NavigationTab.Profile} />
+      <Container className="Profile__Container"></Container>
+    </div>
+  )
+}
