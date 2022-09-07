1230a10e253e4f2de02cf3313a141f0b1808fe8d
lemu <github@lemu.sh>
2022-09-07 15:56:43 -0300

    feat: profile box component

---
+++ src/components/Profile/ProfileBox.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
+
+import Helper from '../Helper/Helper'
+import Divider from '../Section/Divider'
+
+import './ProfileBox.css'
+
+interface Props {
+  title: string
+  info?: string
+  children: React.ReactNode
+}
+
+export function ProfileBox({ children, title, info }: Props) {
+  return (
+    <div className="ProfileBox__Container">
+      <div className={TokenList.join(['ProfileBox__Header', 'ProfileBox__Padded'])}>
+        <span>{title}</span>
+        {info && <Helper text={info} size="12" position="right center" />}
+      </div>
+      <Divider className="ProfileBox__Divider" />
+      <div className="ProfileBox__Padded">{children}</div>
+    </div>
+  )
+}
