3b2bc2ba7fff7a0de9f633102543dcaf6b198a2b
lemu <github@lemu.sh>
2022-09-15 12:59:30 -0300

    Minor snapshot integration improvements (#515)
    * feat: add retry for getting IPFS hash
    
    * chore: improve error message logging
    
    * chore: checksum addresses before getting scores
    
    * fix: get env vars at method calls instead of at client instantiation
    
    * Improve proposal creation logging
    
    * feat: update to snapshot's latest client
    
    * empty commit
    
    (cherry picked from commit 56209233c1cccbe180e02f3ebd0e889a4f585079)

--- src/services/SnapshotService.ts
+++ src/services/SnapshotService.ts
@@ -1,5 +1,6 @@
 import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { Avatar } from 'decentraland-gatsby/dist/utils/api/Catalyst'
+import retry from 'decentraland-gatsby/dist/utils/promise/retry'
 
 import { IPFS } from '../clients/IPFS'
 import { SnapshotApi, SnapshotReceipt } from '../clients/SnapshotApi'
@@ -63,7 +64,7 @@ export class SnapshotService {
 
   private static async getIpfsSnapshotContent(proposalCreationReceipt: SnapshotReceipt) {
     try {
-      return await IPFS.get().getHash(proposalCreationReceipt.ipfs)
+      return await retry(3, () => IPFS.get().getHash(proposalCreationReceipt.ipfs))
     } catch (err: any) {
       SnapshotService.dropSnapshotProposal(proposalCreationReceipt.id)
       throw new Error("Couldn't retrieve proposal from the IPFS: " + err.message, err)
