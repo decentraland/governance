4f6f33f7f9c0f2e0de4ca4f4a291cd666cd66f1b
Nicolás Comerci <45410089+ncomerci@users.noreply.github.com>
2022-09-20 16:06:50 +0200

    feat: edit enactment info (#526)

--- src/entities/Proposal/utils.ts
+++ src/entities/Proposal/utils.ts
@@ -124,6 +124,7 @@ export function isValidUpdateProposalStatus(current: ProposalStatus, next: Propo
     case ProposalStatus.Finished:
       return next === ProposalStatus.Rejected || next === ProposalStatus.Passed || next === ProposalStatus.Enacted
     case ProposalStatus.Passed:
+    case ProposalStatus.Enacted:
       return next === ProposalStatus.Enacted
     default:
       return false
