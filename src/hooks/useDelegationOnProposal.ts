43a76b4559648f063332b2c96356e7516a2e28ef
Andy Espagnolo <andyespagnolo@gmail.com>
2022-09-27 14:35:11 -0300

    fix: snapshot delegations query limit  (#530)
    * fix: remove delegated from limit and clean code
    
    * feat: query delegations in batches, unify function
    
    * fix: blockNumber condition and query argument
    
    * fix: picked by query
    
    * refactor: move picked by fetch to subgraph client
    
    * refactor: use inBatches in SnapshotSubgraph

--- src/hooks/useDelegationOnProposal.ts
+++ src/hooks/useDelegationOnProposal.ts
@@ -2,12 +2,12 @@ import useAsyncMemo from 'decentraland-gatsby/dist/hooks/useAsyncMemo'
 
 import { EMPTY_DELEGATION } from '../clients/SnapshotGraphql'
 import { ProposalAttributes } from '../entities/Proposal/types'
-import { getDelegationsOnProposal } from '../entities/Snapshot/utils'
+import { getDelegations } from '../entities/Snapshot/utils'
 
 export default function useDelegationOnProposal(proposal?: ProposalAttributes | null, address?: string | null) {
   return useAsyncMemo(
     async () => {
-      return await getDelegationsOnProposal(address, proposal?.snapshot_proposal.snapshot)
+      return await getDelegations(address, proposal?.snapshot_proposal.snapshot)
     },
     [address, proposal],
     { initialValue: EMPTY_DELEGATION, callWithTruthyDeps: true }
