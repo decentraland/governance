85fa7af83fca475a22aed86155b1831ee542b0da
lemu <github@lemu.sh>
2022-10-04 14:02:50 -0300

    feat: slideable vp distribution labels

--- src/components/Modal/VotingPowerDelegationDetail/VotingPowerDistributionLabels.tsx
+++ src/components/Modal/VotingPowerDelegationDetail/VotingPowerDistributionLabels.tsx
@@ -10,39 +10,37 @@ const VotingPowerDistributionLabels = () => {
   const t = useFormatMessage()
 
   return (
-    <MobileSlider>
-      <div className="VotingPowerDistribution__Labels">
-        <VotingPowerDistributionLabel
-          labelText={t('modal.vp_delegation.details.stats_bar_mana')}
-          tooltipText={t('modal.vp_delegation.details.stats_bar_mana_info')}
-          className={'VotingPowerDistribution__Mana'}
-        />
-        <VotingPowerDistributionLabel
-          labelText={t('modal.vp_delegation.details.stats_bar_name')}
-          tooltipText={t('modal.vp_delegation.details.stats_bar_name_info')}
-          className={'VotingPowerDistribution__Name'}
-        />
-        <VotingPowerDistributionLabel
-          labelText={t('modal.vp_delegation.details.stats_bar_linked_wearables')}
-          tooltipText={t('modal.vp_delegation.details.stats_bar_linked_wearables_info')}
-          className={'VotingPowerDistribution__LinkedWearables'}
-        />
-        <VotingPowerDistributionLabel
-          labelText={t('modal.vp_delegation.details.stats_bar_land')}
-          tooltipText={t('modal.vp_delegation.details.stats_bar_land_info')}
-          className={'VotingPowerDistribution__Land'}
-        />
-        <VotingPowerDistributionLabel
-          labelText={t('modal.vp_delegation.details.stats_bar_estates')}
-          tooltipText={t('modal.vp_delegation.details.stats_bar_estates_info')}
-          className={'VotingPowerDistribution__Estate'}
-        />
-        <VotingPowerDistributionLabel
-          labelText={t('modal.vp_delegation.details.stats_bar_delegated')}
-          tooltipText={t('modal.vp_delegation.details.stats_bar_delegated_info')}
-          className={'VotingPowerDistribution__Delegated'}
-        />
-      </div>
+    <MobileSlider containerClassName="VotingPowerDistribution__Labels">
+      <VotingPowerDistributionLabel
+        labelText={t('modal.vp_delegation.details.stats_bar_mana')}
+        tooltipText={t('modal.vp_delegation.details.stats_bar_mana_info')}
+        className={'VotingPowerDistribution__Mana'}
+      />
+      <VotingPowerDistributionLabel
+        labelText={t('modal.vp_delegation.details.stats_bar_name')}
+        tooltipText={t('modal.vp_delegation.details.stats_bar_name_info')}
+        className={'VotingPowerDistribution__Name'}
+      />
+      <VotingPowerDistributionLabel
+        labelText={t('modal.vp_delegation.details.stats_bar_linked_wearables')}
+        tooltipText={t('modal.vp_delegation.details.stats_bar_linked_wearables_info')}
+        className={'VotingPowerDistribution__LinkedWearables'}
+      />
+      <VotingPowerDistributionLabel
+        labelText={t('modal.vp_delegation.details.stats_bar_land')}
+        tooltipText={t('modal.vp_delegation.details.stats_bar_land_info')}
+        className={'VotingPowerDistribution__Land'}
+      />
+      <VotingPowerDistributionLabel
+        labelText={t('modal.vp_delegation.details.stats_bar_estates')}
+        tooltipText={t('modal.vp_delegation.details.stats_bar_estates_info')}
+        className={'VotingPowerDistribution__Estate'}
+      />
+      <VotingPowerDistributionLabel
+        labelText={t('modal.vp_delegation.details.stats_bar_delegated')}
+        tooltipText={t('modal.vp_delegation.details.stats_bar_delegated_info')}
+        className={'VotingPowerDistribution__Delegated'}
+      />
     </MobileSlider>
   )
 }
