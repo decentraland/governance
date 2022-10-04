27be87cf3bde111f08a70bdfd9e60f3a7e367458
lemu <github@lemu.sh>
2022-10-04 11:19:50 -0300

    User voting stats (#548)
    * chore: user voting stats wip
    
    * chore: move displayable address to useProfile
    
    * chore: outcomeMatch
    
    * chore: useVotingStats
    
    * feat: add different conclusions depending on percentages
    
    * feat: add conclusion for when looking at your stats. Hide personal match when no user is logged in
    
    * fix: add right margin to username blockies/avatars
    
    * chore: refactors
    
    * chore: add more validations to outcomeMatch
    
    * chore: memoize functions

--- src/hooks/useNameOrAddress.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-
-import useProfile from './useProfile'
-
-export default function useNameOrAddress(address?: string | null) {
-  const { profile, hasDclProfile } = useProfile(address)
-  const profileHasName = hasDclProfile && profile!.name && profile!.name.length > 0
-  return useMemo(() => (profile && profileHasName ? profile.name : address), [profileHasName])
-}
