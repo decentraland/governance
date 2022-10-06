5907973d08fd3b287fbe5d6146b0983b7df34875
Andy Espagnolo <andyespagnolo@gmail.com>
2022-10-06 18:37:21 -0300

    chore: test variables

--- src/modules/env.ts
+++ src/modules/env.ts
@@ -2,8 +2,10 @@ import { config } from '../config'
 
 export const env = (variable: string, defaultValue?: string) => {
   if (process.env.HEROKU === 'true') {
+    console.log(`Heroku Process Env - ${variable}`, process.env[variable])
     return process.env[variable]
   }
 
+  console.log(`Config Process Env - ${variable}`, process.env[variable])
   return config.get(variable, defaultValue)
 }
