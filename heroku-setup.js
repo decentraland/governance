226700ae056f9f3db21123c9c7d23f95d910a03f
Andy Espagnolo <andyespagnolo@gmail.com>
2022-10-07 10:31:05 -0300

    chore: added try catch and some nice messages

--- heroku-setup.js
+++ heroku-setup.js
@@ -1,10 +1,15 @@
 function writeEnv() {
-  const fs = require('fs')
-  const data = fs.readFileSync('./src/config/env/heroku.json')
-  const json = JSON.parse(data)
-  let file = ''
-  Object.keys(json).forEach((item) => (file += `${item}=${json[item]}\n`))
-  fs.writeFileSync('./.env.production', file)
+  try {
+    const fs = require('fs')
+    const data = fs.readFileSync('./src/config/env/heroku.json')
+    const json = JSON.parse(data)
+    let file = ''
+    Object.keys(json).forEach((item) => (file += `${item}=${json[item]}\n`))
+    fs.writeFileSync('./.env.production', file)
+    console.log('Finished writing heroku.json to ./env.production. Have a nice day 🌞!')
+  } catch (error) {
+    console.log('Error writing heroku.json to ./env.production 😱', error)
+  }
 }
 
 writeEnv()
