6e4885ad3a0af21362298ae8c6eb00d4e8852662
frami <i@2fd.me>
2022-10-10 11:43:42 -0300

    chore: remove pulumi

--- Pulumi.ts
+++
@@ -1,8 +0,0 @@
-// import { resolve } from "path";
-// import {env} from "dcl-ops-lib/domain";
-// import { buildGatsby } from "decentraland-gatsby-deploy/dist/recepies/buildGatsby";
-// import { variable } from "decentraland-gatsby-deploy/dist/pulumi/env";
-
-export = async function main() {
-  return {}
-}
