4a4e3f5d22b8eab3df0c6cbd6fa464503c554e7d
lemu <github@lemu.sh>
2022-10-13 00:09:25 -0500

    feat: reaction picker WIP

--- src/components/Proposal/SentimentSurvey.tsx
+++ src/components/Proposal/SentimentSurvey.tsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Loader } from 'decentraland-ui'
 
 import { ProposalAttributes } from '../../entities/Proposal/types'
+import { SurveyTopicAttributes } from '../../entities/SurveyTopic/types'
 import useSurveyTopics from '../../hooks/useSurveyTopics'
+import { Feedback } from '../Modal/Votes/VotingModal'
 
 import './SentimentSurvey.css'
-import SentimentSurveyRow from './SentimentSurveyRow'
+import SentimentSurveyRow, { Reaction } from './SentimentSurveyRow'
 
 interface Props {
   proposal: ProposalAttributes
 }
 
+export type Topic = Pick<SurveyTopicAttributes, 'topic_id' | 'label'>
+
 const SentimentSurvey = ({ proposal }: Props) => {
   const t = useFormatMessage()
   const { surveyTopics, isLoadingSurveyTopics } = useSurveyTopics(proposal.id)
+  const [survey, setSurvey] = useState<Feedback>({})
+
+  const addToSurvey = (topic: Topic, reaction: Reaction) => {
+    setSurvey((prev) => {
+      prev[topic.topic_id] = reaction.type
+      return prev
+    })
+  }
+
+  const removeFromSurvey = (topic: Topic) => {
+    setSurvey((prev) => {
+      prev[topic.topic_id] = null
+      return prev
+    })
+  }
 
   return (
     <div className="SentimentSurvey">
@@ -27,7 +46,12 @@ const SentimentSurvey = ({ proposal }: Props) => {
             <span>{'Reaction'}</span>
           </div>
           {surveyTopics?.map((topic, index) => (
-            <SentimentSurveyRow key={`SentimentSurveyRow_${index}`} topic={topic} />
+            <SentimentSurveyRow
+              key={`SentimentSurveyRow_${index}`}
+              topic={topic}
+              onReactionPicked={addToSurvey}
+              onReactionUnpicked={removeFromSurvey}
+            />
           ))}
         </>
       )}
