2a73d151982c5650040a4d17142071e27c9ee5a5
Andy Espagnolo <andyespagnolo@gmail.com>
2022-10-21 10:10:47 -0300

    fix: forum auth and filesystem server changes (#578)
    * chore: set cache header and always use filesystem
    * chore: remove slash from base url in discourse client
    * chore: add env var for discourse user to backend access
    * chore: remove cache control header

--- src/entities/Discourse/utils.ts
+++ src/entities/Discourse/utils.ts
@@ -2,7 +2,7 @@ import { DiscoursePostInTopic, DiscourseTopic } from '../../clients/Discourse'
 import { env } from '../../modules/env'
 import { ProposalComment, ProposalCommentsInDiscourse } from '../Proposal/types'
 
-export const DISCOURSE_USER = env('GATSBY_DISCOURSE_USER') || ''
+export const DISCOURSE_USER = process.env.GATSBY_DISCOURSE_USER || env('GATSBY_DISCOURSE_USER') || ''
 export const BASE_AVATAR_URL = process.env.DISCOURSE_BASE_AVATAR_URL
 export const DISCOURSE_API = process.env.GATSBY_DISCOURSE_API || env('GATSBY_DISCOURSE_API') || ''
 const DEFAULT_AVATAR_SIZE = '45'
--- src/server.ts
+++ src/server.ts
@@ -70,25 +70,18 @@ app.get(
   })
 )
 
-if (process.env.HEROKU === 'true') {
-  app.use(express.static('public'))
-  app.use(function (_req, res) {
-    res.status(404).sendFile('404/index.html', { root: path.join(__dirname, '../public') })
+app.use(
+  filesystem('public', '404.html', {
+    defaultHeaders: {
+      'Content-Security-Policy': `base-uri 'self'; child-src https:; connect-src https: wss:; default-src 'none'; font-src https: data:; form-action 'self'; frame-ancestors 'none'; frame-src https:; img-src https: data:; manifest-src 'self'; media-src 'self'; object-src 'none'; prefetch-src https: data:; style-src 'unsafe-inline' https: data:; worker-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://decentraland.org https://*.decentraland.org https://cdn.segment.com https://cdn.rollbar.com https://ajax.cloudflare.com https://googleads.g.doubleclick.net https://ssl.google-analytics.com https://tagmanager.google.com https://www.google-analytics.com https://www.google-analytics.com https://www.google.com https://www.googleadservices.com https://www.googletagmanager.com`,
+      'X-Content-Type-Options': 'nosniff',
+      'X-Frame-Options': 'DENY',
+      'strict-transport-security': 'max-age=15552000; includeSubDomains; preload',
+      'referrer-policy': 'strict-origin-when-cross-origin',
+      'x-xss-protection': '1; mode=block',
+    },
   })
-} else {
-  app.use(
-    filesystem('public', '404.html', {
-      defaultHeaders: {
-        'Content-Security-Policy': `base-uri 'self'; child-src https:; connect-src https: wss:; default-src 'none'; font-src https: data:; form-action 'self'; frame-ancestors 'none'; frame-src https:; img-src https: data:; manifest-src 'self'; media-src 'self'; object-src 'none'; prefetch-src https: data:; style-src 'unsafe-inline' https: data:; worker-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://decentraland.org https://*.decentraland.org https://cdn.segment.com https://cdn.rollbar.com https://ajax.cloudflare.com https://googleads.g.doubleclick.net https://ssl.google-analytics.com https://tagmanager.google.com https://www.google-analytics.com https://www.google-analytics.com https://www.google.com https://www.googleadservices.com https://www.googletagmanager.com`,
-        'X-Content-Type-Options': 'nosniff',
-        'X-Frame-Options': 'DENY',
-        'strict-transport-security': 'max-age=15552000; includeSubDomains; preload',
-        'referrer-policy': 'strict-origin-when-cross-origin',
-        'x-xss-protection': '1; mode=block',
-      },
-    })
-  )
-}
+)
 
 void initializeServices([
   process.env.DATABASE !== 'false' && databaseInitializer(),
