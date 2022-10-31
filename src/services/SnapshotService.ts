d5542bf4ac9ba02299c4fb3935d73d9831439f1b
Andy Espagnolo <andyespagnolo@gmail.com>
2022-10-31 14:28:00 -0300

    feat: remove ipfs fetch on proposal creation (#586)
    * feat: remove ipfs fetch on proposal creation
    
    * empty commit!
    
    * feat: remove snapshot_signature column from proposal model
    
    * feat: remove ipfs client
    
    * chore: remove radash package

--- src/services/SnapshotService.ts
+++ src/services/SnapshotService.ts
@@ -1,9 +1,8 @@
 import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { Avatar } from 'decentraland-gatsby/dist/utils/api/Catalyst'
-import { retry } from 'radash'
 
-import { IPFS } from '../clients/IPFS'
 import { SnapshotApi, SnapshotReceipt } from '../clients/SnapshotApi'
+import { SnapshotGraphql } from '../clients/SnapshotGraphql'
 import * as templates from '../entities/Proposal/templates'
 import { proposalUrl, snapshotProposalUrl } from '../entities/Proposal/utils'
 import { SNAPSHOT_SPACE } from '../entities/Snapshot/constants'
@@ -30,9 +29,10 @@ export class SnapshotService {
       blockNumber
     )
 
+    const snapshotId = proposalCreationReceipt.id
     const snapshotUrl = snapshotProposalUrl({
       snapshot_space: SNAPSHOT_SPACE,
-      snapshot_id: proposalCreationReceipt.id,
+      snapshot_id: snapshotId,
     })
 
     logger.log('Snapshot proposal created', {
@@ -40,8 +40,8 @@ export class SnapshotService {
       snapshot_proposal: JSON.stringify(proposalCreationReceipt),
     })
 
-    const snapshotContent = await this.getIpfsSnapshotContent(proposalCreationReceipt)
-    return { snapshotId: proposalCreationReceipt.id, snapshotUrl: snapshotUrl, snapshotContent }
+    const snapshotContent = await this.getProposalContent(snapshotId)
+    return { snapshotId, snapshotUrl, snapshotContent }
   }
 
   private static async getProposalTitleAndBody(
@@ -62,12 +62,12 @@ export class SnapshotService {
     return { proposalTitle, proposalBody }
   }
 
-  private static async getIpfsSnapshotContent(proposalCreationReceipt: SnapshotReceipt) {
+  private static async getProposalContent(snapshotId: string) {
     try {
-      return await retry({ times: 3 }, () => IPFS.get().getHash(proposalCreationReceipt.ipfs))
+      return await SnapshotGraphql.get().getProposalContent(snapshotId)
     } catch (err: any) {
-      SnapshotService.dropSnapshotProposal(proposalCreationReceipt.id)
-      throw new Error("Couldn't retrieve proposal from the IPFS: " + err.message, err)
+      SnapshotService.dropSnapshotProposal(snapshotId)
+      throw new Error("Couldn't retrieve proposal content: " + err.message, err)
     }
   }
 
