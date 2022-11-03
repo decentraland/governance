064712082b08b7afd5d7572715a87bf868a25b41
Nicolás Comerci <45410089+ncomerci@users.noreply.github.com>
2022-11-03 14:29:55 +0100

    empty state (#594)

--- src/components/Profile/VotedProposalsBox.tsx
+++ src/components/Profile/VotedProposalsBox.tsx
@@ -4,8 +4,10 @@ import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Container } from 'decentraland-ui/dist/components/Container/Container'
 
 import useVotedProposals from '../../hooks/useVotedProposals'
+import Empty from '../Common/Empty'
 import FullWidthButton from '../Common/FullWidthButton'
 import SkeletonBars from '../Common/SkeletonBars'
+import Watermelon from '../Icon/Watermelon'
 import ProfileProposalItem from '../Proposal/ProfileProposalItem'
 
 import { ProfileBox } from './ProfileBox'
@@ -27,9 +29,13 @@ function VotedProposalsBox({ address }: Props) {
       <ProfileBox title={t('page.profile.voted_proposals.title')}>
         {isLoading && <SkeletonBars amount={votes.length || 3} height={89} />}
         {!isLoading &&
-          votes.map((vote) => {
-            return <ProfileProposalItem key={vote.id} votedProposal={vote} />
-          })}
+          (votes.length > 0 ? (
+            votes.map((vote) => {
+              return <ProfileProposalItem key={vote.id} votedProposal={vote} />
+            })
+          ) : (
+            <Empty icon={<Watermelon />} description={t('page.profile.voted_proposals.empty')} />
+          ))}
         {areMoreProposals && (
           <FullWidthButton onClick={handleViewMore}>{t('page.profile.voted_proposals.button')}</FullWidthButton>
         )}
