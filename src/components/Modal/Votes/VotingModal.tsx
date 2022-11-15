35d05e7fa00dd252866db770441ec5d9b324fc0f
lemu <github@lemu.sh>
2022-11-15 13:02:39 -0300

    chore: voting modal snapshot redirect refactor

--- src/components/Modal/Votes/VotingModal.tsx
+++ src/components/Modal/Votes/VotingModal.tsx
@@ -1,25 +1,15 @@
 import React, { useState } from 'react'
 
-import Link from 'decentraland-gatsby/dist/components/Text/Link'
-import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
-import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
-import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
-import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import { Close } from 'decentraland-ui/dist/components/Close/Close'
-import { Header } from 'decentraland-ui/dist/components/Header/Header'
 import { Modal } from 'decentraland-ui/dist/components/Modal/Modal'
 
 import { ProposalAttributes } from '../../../entities/Proposal/types'
-import { snapshotProposalUrl } from '../../../entities/Proposal/utils'
 import { Survey, SurveyTopicAttributes } from '../../../entities/SurveyTopic/types'
-import { formatChoice } from '../../../modules/votes/utils'
 import { ProposalPageContext, SelectedChoice } from '../../../pages/proposal'
-import OpenExternalLink from '../../Icon/OpenExternalLink'
-import VotingDisabled from '../../Icon/VotingDisabled'
-import SentimentSurvey from '../../Proposal/SentimentSurvey/SentimentSurvey'
 import '../ProposalModal.css'
 
+import { VoteOnSnapshot } from './VoteOnSnapshot'
 import './VotingModal.css'
 import { VotingModalSurvey } from './VotingModalSurvey'
 
@@ -70,35 +60,7 @@ export function VotingModal({
           proposalContext={proposalContext}
         />
       )}
-      {showSnapshotRedirect && (
-        <Modal.Content>
-          <div className="VoteOnSnapshot__Content">
-            <VotingDisabled className="VoteOnSnapshot__Icon" />
-            <span className="VoteOnSnapshot__Header">Voting not available</span>
-            <div className="VoteOnSnapshot__Description">
-              {"It looks like we're having issues casting your vote from the Governance dApp."}
-            </div>
-            <Markdown className="VoteOnSnapshot__Suggestion">
-              {'You can try voting directly on **Snapshot** - the system we use for decision-making.'}
-            </Markdown>
-          </div>
-          <div className="VoteOnSnapshot__Actions">
-            <Button
-              fluid
-              primary
-              href={snapshotProposalUrl(proposal)}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="VoteOnSnapshot__Button"
-            >
-              <div className="VoteOnSnapshot__Hidden" />
-              {'Vote On Snapshot'}
-              <OpenExternalLink className="VoteOnSnapshot__ButtonIcon" />
-            </Button>
-            <Link className="VoteOnSnapshot__FeedbackLink">{'What about my extra feedback?'}</Link>
-          </div>
-        </Modal.Content>
-      )}
+      {showSnapshotRedirect && <VoteOnSnapshot proposal={proposal} />}
       {/*{showFeedbackReview && (*/}
 
       {/*)}*/}
