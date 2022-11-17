0fee402446608c082d24aedcc5f1619a9db63e7a
lemu <github@lemu.sh>
2022-11-17 15:02:46 -0300

    chore: use token list for icon helper css

--- src/components/Helper/IconHelper.tsx
+++ src/components/Helper/IconHelper.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import { Popup, PopupProps } from 'decentraland-ui/dist/components/Popup/Popup'
 
 type Props = Pick<PopupProps, 'position'> & {
@@ -10,6 +11,7 @@ type Props = Pick<PopupProps, 'position'> & {
 }
 
 function IconHelper({ position, text, containerClassName, icon }: Props) {
+  const useContainerClassName = containerClassName && containerClassName.length > 0
   return (
     <Popup
       content={<Markdown className="HelperText__Content">{text}</Markdown>}
@@ -17,7 +19,10 @@ function IconHelper({ position, text, containerClassName, icon }: Props) {
       trigger={<div className={'IconHelper__Container'}>{icon}</div>}
       on="hover"
       hoverable
-      className={(containerClassName && `${containerClassName}--Popup`) || 'Helper__Container--Popup'}
+      className={TokenList.join([
+        useContainerClassName && `${containerClassName}--Popup`,
+        !useContainerClassName && 'Helper__Container--Popup',
+      ])}
     />
   )
 }
