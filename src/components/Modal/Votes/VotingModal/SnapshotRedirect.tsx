b9f5510057a05819b7ee8b687df930dbc82cb6e8
lemu <github@lemu.sh>
2022-11-18 11:59:13 -0300

    chore: snapshot redirect internationalization

--- src/components/Modal/Votes/VotingModal/SnapshotRedirect.tsx
+++ src/components/Modal/Votes/VotingModal/SnapshotRedirect.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Link from 'decentraland-gatsby/dist/components/Text/Link'
 import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Modal } from 'decentraland-ui/dist/components/Modal/Modal'
 
 import { ProposalAttributes } from '../../../../entities/Proposal/types'
@@ -18,22 +19,23 @@ interface Props {
 }
 
 export function SnapshotRedirect({ proposal, onReviewFeedback }: Props) {
+  const t = useFormatMessage()
   return (
     <Modal.Content>
       <div className="SnapshotRedirect__Content">
         <VotingDisabled className="SnapshotRedirect__Icon" />
-        <span className="SnapshotRedirect__Header">Voting not available</span>
-        <div className="SnapshotRedirect__Description">
-          {"It looks like we're having issues casting your vote from the Governance dApp."}
-        </div>
+        <span className="SnapshotRedirect__Header">
+          {t('page.proposal_detail.snapshot_redirect.voting_not_available')}
+        </span>
+        <div className="SnapshotRedirect__Description">{t('page.proposal_detail.snapshot_redirect.description')}</div>
         <Markdown className="SnapshotRedirect__Suggestion">
-          {'You can try voting directly on **Snapshot** - the system we use for decision-making.'}
+          {t('page.proposal_detail.snapshot_redirect.suggestion')}
         </Markdown>
       </div>
       <div className="SnapshotRedirect__Actions">
         <SnapshotRedirectButton proposal={proposal} />
         <Link className="SnapshotRedirect__FeedbackLink" onClick={onReviewFeedback}>
-          {'What about my extra feedback?'}
+          {t('page.proposal_detail.snapshot_redirect.extra_feedback')}
         </Link>
       </div>
     </Modal.Content>
