acaad6934494708b6157c1bbf3dab10317c2b035
lemu <github@lemu.sh>
2022-11-18 13:29:29 -0300

    chore: voting section footer internationalization

--- src/components/Section/ProposalVoting/SidebarSnapshotRedirect.tsx
+++ src/components/Section/ProposalVoting/SidebarSnapshotRedirect.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
-import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Header } from 'decentraland-ui/dist/components/Header/Header'
 
 import { ProposalAttributes } from '../../../entities/Proposal/types'
-import { snapshotProposalUrl } from '../../../entities/Proposal/utils'
 import SnapshotRedirectButton from '../../Common/SnapshotRedirectButton'
-import OpenExternalLink from '../../Icon/OpenExternalLink'
 
 import './SidebarSnapshotRedirect.css'
 
@@ -16,14 +14,15 @@ interface Props {
 }
 
 const SidebarSnapshotRedirect = ({ proposal }: Props) => {
+  const t = useFormatMessage()
   return (
     <div className="SidebarSnapshotRedirect">
-      <Header sub>{'Voting not available'}</Header>
+      <Header sub>{t('page.proposal_detail.snapshot_redirect.voting_not_available')}</Header>
       <Markdown className="SidebarSnapshotRedirect__Text">
-        {"**It looks like we're having issues casting your vote from the Governance dApp.**"}
+        {t('page.proposal_detail.snapshot_redirect.description')}
       </Markdown>
       <Markdown className="SidebarSnapshotRedirect__Text">
-        {'You can try voting directly on **Snapshot** - the system we use for decision-making.'}
+        {t('page.proposal_detail.snapshot_redirect.suggestion')}
       </Markdown>
       <SnapshotRedirectButton proposal={proposal} inverted />
     </div>
