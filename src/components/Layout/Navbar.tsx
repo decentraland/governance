5f1431fa427441bdc5479f0bb1e65776698eed4e
Andy Espagnolo <andyespagnolo@gmail.com>
2022-11-22 10:01:31 -0300

    refactor: remove deprecated media query functions

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
-import UserMenu from 'decentraland-gatsby/dist/components/User/UserMenu'
-import useResponsive from 'decentraland-gatsby/dist/hooks/useResponsive'
-import Responsive from 'semantic-ui-react/dist/commonjs/addons/Responsive'
+import UserMenu, { UserMenuProps } from 'decentraland-gatsby/dist/components/User/UserMenu'
+import { Mobile, NotMobile } from 'decentraland-ui/dist/components/Media/Media'
 
 import BurgerMenu from './BurgerMenu/BurgerMenu'
 
-function Navbar({ props }: any) {
-  const responsive = useResponsive()
-  const isMobile = responsive({ maxWidth: Responsive.onlyMobile.maxWidth })
-
-  return isMobile ? <BurgerMenu /> : <UserMenu {...props} />
+function Navbar(props: UserMenuProps) {
+  return (
+    <>
+      <Mobile>
+        <BurgerMenu />
+      </Mobile>
+      <NotMobile>
+        <UserMenu {...props} />
+      </NotMobile>
+    </>
+  )
 }
 
 export default React.memo(Navbar)
