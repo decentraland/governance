3cc6c5744ff8353a8d4eb0cbf5dae17845dadaf1
lemu <github@lemu.sh>
2022-11-23 21:28:36 -0300

    chore: update migrations to remove topic labels

--- src/migrations/1667493353233_create-known-proposal-survey-topics.ts
+++ src/migrations/1667493353233_create-known-proposal-survey-topics.ts
@@ -5,12 +5,12 @@ export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   const sql = `
-    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'budgt', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
-    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'benef', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
-    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'spec', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
-    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'perso', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
-    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'roadm', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
-    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'value', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
+    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'budget', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
+    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'beneficiary', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
+    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'specification', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
+    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'personnel', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
+    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'roadmap', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
+    INSERT INTO proposal_survey_topics (id, topic_id, proposal_type, proposal_sub_types) VALUES (gen_random_uuid(), 'value_proposition', 'grant', '["Community", "Content Creator", "Platform Contributor", "Gaming"]');
   `
 
   pgm.sql(sql)
