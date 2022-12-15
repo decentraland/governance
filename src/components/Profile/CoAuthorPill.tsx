0a81f88235975b415df05011324f732370761850
Andy Espagnolo <andyespagnolo@gmail.com>
2022-12-15 14:05:35 -0300

    fix: profile issues (#629)
    * fix: coauthor label key in profile
    
    * fix: own proposals in other user profile
    
    * fix: loading proposals in profile

--- src/components/Profile/CoAuthorPill.tsx
+++ src/components/Profile/CoAuthorPill.tsx
@@ -12,7 +12,7 @@ interface Props {
 const CoAuthorPill = ({ className, hasCoauthorRequests }: Props) => {
   const t = useFormatMessage()
   const textKey = hasCoauthorRequests ? 'pending_request_label' : 'label'
-  const text = t(`page.activity.coauthoring.${textKey}`)
+  const text = t(`page.profile.activity.coauthoring.${textKey}`)
 
   return (
     <Pill className={className} size="small" color="yellow" style={hasCoauthorRequests ? 'shiny' : 'outline'}>
