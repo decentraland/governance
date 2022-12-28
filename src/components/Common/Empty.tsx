b7bd5660c02f384df5e74bc9e9dc036c5e1ae32e
lemu <github@lemu.sh>
2022-12-28 16:06:35 -0300

    feat: add asButton option to Empty component

--- src/components/Common/Empty.tsx
+++ src/components/Common/Empty.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Link from 'decentraland-gatsby/dist/components/Text/Link'
 import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import { Header } from 'decentraland-ui/dist/components/Header/Header'
 
 import Watermelon from '../Icon/Watermelon'
@@ -17,18 +18,34 @@ interface Props {
   className?: string
   onLinkClick?: () => void
   icon?: React.ReactNode
+  asButton?: boolean
 }
 
-export default function Empty({ icon, title, description, className, linkText, linkHref, onLinkClick }: Props) {
+export default function Empty({
+  icon,
+  title,
+  description,
+  className,
+  linkText,
+  linkHref,
+  onLinkClick,
+  asButton,
+}: Props) {
+  const showClickableAction = !!linkText && (onLinkClick || linkHref)
   return (
     <div className={TokenList.join(['Empty', className])}>
       {icon ? icon : <Watermelon />}
       {!!title && <Header>{title}</Header>}
       {!!description && <Markdown>{description}</Markdown>}
-      {!!linkText && (onLinkClick || linkHref) && (
-        <Link className="Empty__Link" href={linkHref} onClick={onLinkClick}>
+      {showClickableAction && !asButton && (
+        <Link className="Empty__Action" href={linkHref} onClick={onLinkClick}>
           {linkText}
         </Link>
+      )}
+      {showClickableAction && !!asButton && (
+        <Button primary className="Empty__Action" href={linkHref} onClick={onLinkClick}>
+          {linkText}
+        </Button>
       )}
     </div>
   )
