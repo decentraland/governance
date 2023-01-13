588afd4f9b6c96d02195a95a41dd849b51fffd7d
lemu <github@lemu.sh>
2023-01-13 12:00:12 -0300

    chore: add size too big validation to funding

--- src/entities/Grant/utils.ts
+++ src/entities/Grant/utils.ts
@@ -10,16 +10,6 @@ export const isValidGrantBudget = (size: number) => {
   return true
 }
 
-import { GRANT_PROPOSAL_MAX_BUDGET, GRANT_PROPOSAL_MIN_BUDGET } from './constants'
-
-export const isValidGrantBudget = (size: number) => {
-  if (size <= GRANT_PROPOSAL_MIN_BUDGET || size > GRANT_PROPOSAL_MAX_BUDGET) {
-    return false
-  }
-
-  return true
-}
-
 export function isProposalGrantCategory(value: string | null | undefined): boolean {
   return VALID_CATEGORIES.includes(value as ProposalGrantCategory)
 }
