5d7c8d238b7016393431469de150cd47f37945fa
Andy Espagnolo <andyespagnolo@gmail.com>
2023-01-16 20:30:04 -0300

    fix: type in category filter hook

--- src/hooks/useCurrentsGrantsFilteredByCategory.ts
+++ src/hooks/useCurrentsGrantsFilteredByCategory.ts
@@ -9,7 +9,7 @@ import {
 } from '../entities/Proposal/types'
 
 export function useCurrentGrantsFilteredByCategory(grants: GrantWithUpdateAttributes[]) {
-  // TODO: Add new category filters
+  // TODO: Check this
   return useMemo(
     () => ({
       [PROPOSAL_GRANT_CATEGORY_ALL]: grants,
@@ -28,6 +28,34 @@ export function useCurrentGrantsFilteredByCategory(grants: GrantWithUpdateAttrib
       [ProposalGrantCategory.ContentCreator]: filter(
         grants,
         (item) => item.configuration.category === ProposalGrantCategory.ContentCreator
+      ),
+      [ProposalGrantCategory.Accelerator]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.Accelerator
+      ),
+      [ProposalGrantCategory.CoreUnit]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.CoreUnit
+      ),
+      [ProposalGrantCategory.Documentation]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.Documentation
+      ),
+      [ProposalGrantCategory.InWorldContent]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.InWorldContent
+      ),
+      [ProposalGrantCategory.Platform]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.Platform
+      ),
+      [ProposalGrantCategory.SocialMediaContent]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.SocialMediaContent
+      ),
+      [ProposalGrantCategory.Sponsorship]: filter(
+        grants,
+        (item) => item.configuration.category === ProposalGrantCategory.Sponsorship
       ),
     }),
     [grants]
