2a7686192d303187a9c39a5a87bcd34cb0a1c993
Andy Espagnolo <andyespagnolo@gmail.com>
2023-01-19 18:22:02 -0300

    feat: add category selector texts

--- src/components/Grants/CategorySelector.tsx
+++ src/components/Grants/CategorySelector.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+
 import { ProposalGrantCategory, VALID_CATEGORIES } from '../../entities/Proposal/types'
 
 import CategoryItem from './CategoryItem'
@@ -10,11 +13,19 @@ interface Props {
 }
 
 const CategorySelector = ({ onCategoryClick }: Props) => {
+  const t = useFormatMessage()
+
   return (
-    <div className="CategorySelector">
-      {VALID_CATEGORIES.map((category) => {
-        return <CategoryItem key={category} category={category} onCategoryClick={onCategoryClick} />
-      })}
+    <div>
+      <p className="CategorySelector__Description">{t('page.submit_grant.category_selection.description')}</p>
+      <div className="CategorySelector__ItemsContainer">
+        {VALID_CATEGORIES.map((category) => {
+          return <CategoryItem key={category} category={category} onCategoryClick={onCategoryClick} />
+        })}
+      </div>
+      <Markdown className="CategorySelector__Documentation">
+        {t('page.submit_grant.category_selection.documentation')}
+      </Markdown>
     </div>
   )
 }
