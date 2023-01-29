a46a475c7930d0831b4a96506f3761529a58fcf1
lemu <github@lemu.sh>
2023-01-29 19:40:28 +0200

    feat: fetch transparency budget with schema validation

--- src/entities/Budget/jobs.ts
+++ src/entities/Budget/jobs.ts
@@ -1,7 +1,95 @@
+import logger from 'decentraland-gatsby/dist/entities/Development/logger'
+import validate from 'decentraland-gatsby/dist/entities/Route/validate'
+import schema from 'decentraland-gatsby/dist/entities/Schema'
+
 import { DclData, TransparencyBudget } from '../../clients/DclData'
 
+export const TransparencyBudgetSchema = {
+  type: 'object',
+  additionalProperties: false,
+  required: ['start_date', 'total', 'category_percentages'],
+  properties: {
+    start_date: {
+      type: 'string',
+      minLength: 6,
+      maxLength: 20,
+    },
+    total: {
+      type: 'integer',
+      minimum: 0,
+    },
+    category_percentages: {
+      type: 'object',
+      additionalProperties: false,
+      required: [
+        'accelerator',
+        'core_unit',
+        'documentation',
+        'in_world_content',
+        'platform',
+        'social_media_content',
+        'sponsorship',
+      ],
+      properties: {
+        accelerator: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+        core_unit: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+        documentation: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+        in_world_content: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+        platform: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+        social_media_content: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+        sponsorship: {
+          type: 'integer',
+          minimum: 0,
+          maximum: 100,
+        },
+      },
+    },
+  },
+}
+
+const transparencyBudgetValidator = schema.compile(TransparencyBudgetSchema)
 export async function getTransparencyBudgets() {
-  const budgets: TransparencyBudget[] = await DclData.get().getBudgets()
-  console.log('budgets', budgets)
+  let budgets: TransparencyBudget[] = []
+  try {
+    budgets = await DclData.get().getBudgets()
+    if (!budgets || budgets.length < 1) {
+      logger.error(`Received an empty list of transparency budgets`)
+      return budgets
+    }
+    try {
+      budgets.forEach((budget) => validate<TransparencyBudget>(transparencyBudgetValidator, budget))
+    } catch (e) {
+      logger.error(`Invalid transparency budgets ${budgets}. ${JSON.stringify(e)}`)
+      console.error(`Invalid transparency budgets ${budgets}`, e)
+      return []
+    }
+  } catch (e) {
+    logger.error(`Unable to fetch transparency budgets. ${JSON.stringify(e)}`)
+    console.error('Unable to fetch transparency budgets', e)
+  }
   return budgets
 }
