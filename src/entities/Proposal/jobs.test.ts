f5d52fb4c27fdc2c0f5e6220e54d24f291716f36
lemu <github@lemu.sh>
2023-02-09 16:39:46 -0300

    feat: finish proposal job test for the three current outcomes

--- src/entities/Proposal/jobs.test.ts
+++ src/entities/Proposal/jobs.test.ts
@@ -1,67 +1,112 @@
-import JobContext from 'decentraland-gatsby/dist/entities/Job/context'
-
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { DiscordService } from '../../services/DiscordService'
+import UpdateModel from '../Updates/model'
 
 import * as calculateOutcome from './calculateOutcome'
 import { ProposalOutcome } from './calculateOutcome'
 import { finishProposal } from './jobs'
 import ProposalModel from './model'
 import * as routes from './routes'
-import { createTestProposal } from './test_helpers'
+import { JOB_CONTEXT_MOCK, createTestProposal } from './test_helpers'
 import { ProposalAttributes, ProposalStatus, ProposalType } from './types'
+import { DEFAULT_CHOICES } from './utils'
 
 const REJECTED_OUTCOME = {
-  winnerChoice: 'no',
+  winnerChoice: DEFAULT_CHOICES[1],
   outcomeStatus: ProposalOutcome.REJECTED,
 }
-
-const REJECTED_PROPOSAL: ProposalAttributes<any> = createTestProposal(ProposalType.Grant, ProposalStatus.Active)
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-interface CustomJobContext extends JobContext<{}> {
-  log: jest.Mock<any, any>
-  error: jest.Mock<any, any>
+const ACCEPTED_OUTCOME = {
+  winnerChoice: DEFAULT_CHOICES[0],
+  outcomeStatus: ProposalOutcome.ACCEPTED,
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const mockJobContext: CustomJobContext = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  schedule(name: string | null, date: Date, payload: Record<string, any> | undefined): Promise<void> {
-    return Promise.resolve(undefined)
-  },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  updatePayload(payload: Record<string, any> | undefined): Promise<void> {
-    return Promise.resolve(undefined)
-  },
-  log: jest.fn(),
-  error: jest.fn(),
-  id: 'some-id',
-  handler: null,
-  payload: {},
+const FINISHED_OUTCOME = {
+  winnerChoice: DEFAULT_CHOICES[0],
+  outcomeStatus: ProposalOutcome.FINISHED,
 }
+
+const GRANT_PROPOSAL: ProposalAttributes = createTestProposal(ProposalType.Grant, ProposalStatus.Active)
 
 jest.mock('../../constants', () => ({
   DISCORD_SERVICE_ENABLED: false,
 }))
 
 describe('finishProposals', () => {
+  const updatesSpy = jest.spyOn(UpdateModel, 'createPendingUpdates')
   beforeAll(() => {
     jest.spyOn(ProposalModel, 'finishProposal')
     jest.spyOn(routes, 'commentProposalUpdateInDiscourse').mockImplementation(() => {})
     jest.spyOn(DiscordService, 'init').mockImplementation(() => {})
+    updatesSpy.mockResolvedValue([])
     jest.spyOn(DiscordService, 'finishProposal').mockImplementation(() => {})
     jest.spyOn(DiscordService, 'newProposal').mockImplementation(() => {})
     jest.spyOn(DiscordService, 'newUpdate').mockImplementation(() => {})
   })
-  describe('when there is a proposal with rejected outcome', () => {
+  beforeEach(() => {
+    updatesSpy.mockClear()
+  })
+  describe('for a grant proposal with ACTIVE status', () => {
     beforeEach(() => {
-      jest.spyOn(ProposalModel, 'getFinishedProposals').mockResolvedValue([REJECTED_PROPOSAL])
-      jest.spyOn(calculateOutcome, 'calculateOutcome').mockResolvedValue(REJECTED_OUTCOME)
+      jest.spyOn(ProposalModel, 'getFinishedProposals').mockResolvedValue([GRANT_PROPOSAL])
     })
-    it('finishes the proposal as rejected', async () => {
-      await finishProposal(mockJobContext)
-      expect(ProposalModel.finishProposal).toHaveBeenCalledWith([REJECTED_PROPOSAL.id], ProposalStatus.Rejected)
+    describe('when the outcome is REJECTED', () => {
+      beforeEach(() => {
+        jest.spyOn(calculateOutcome, 'calculateOutcome').mockResolvedValue(REJECTED_OUTCOME)
+      })
+      it('finishes the proposal as rejected', async () => {
+        await finishProposal(JOB_CONTEXT_MOCK)
+        expect(ProposalModel.finishProposal).toHaveBeenCalledWith([GRANT_PROPOSAL.id], ProposalStatus.Rejected)
+        expect(UpdateModel.createPendingUpdates).toHaveBeenCalledTimes(0)
+        expect(DiscordService.finishProposal).toHaveBeenCalledWith(
+          GRANT_PROPOSAL.id,
+          GRANT_PROPOSAL.title,
+          REJECTED_OUTCOME.outcomeStatus,
+          undefined
+        )
+      })
+      describe('when the outcome is ACCEPTED', () => {
+        beforeEach(() => {
+          jest.spyOn(calculateOutcome, 'calculateOutcome').mockResolvedValue(ACCEPTED_OUTCOME)
+        })
+        it('finishes the proposal as passed, and creates the pending updates for them', async () => {
+          await finishProposal(JOB_CONTEXT_MOCK)
+          expect(ProposalModel.finishProposal).toHaveBeenCalledWith([GRANT_PROPOSAL.id], ProposalStatus.Passed)
+          expect(UpdateModel.createPendingUpdates).toHaveBeenCalledWith(
+            GRANT_PROPOSAL.id,
+            GRANT_PROPOSAL.configuration.projectDuration
+          )
+          expect(DiscordService.finishProposal).toHaveBeenCalledWith(
+            GRANT_PROPOSAL.id,
+            GRANT_PROPOSAL.title,
+            ACCEPTED_OUTCOME.outcomeStatus,
+            undefined
+          )
+        })
+
+        // describe('when there is enough budget to fund the grant', () => {
+        //
+        // })
+        // describe('when there is not enough budget to fund the grant', () => {
+        //
+        // })
+      })
+      describe('when the outcome is FINISHED', () => {
+        beforeEach(() => {
+          jest.spyOn(calculateOutcome, 'calculateOutcome').mockResolvedValue(FINISHED_OUTCOME)
+        })
+        it('finishes the proposal as passed, and creates the pending updates for them', async () => {
+          await finishProposal(JOB_CONTEXT_MOCK)
+          expect(ProposalModel.finishProposal).toHaveBeenCalledWith([GRANT_PROPOSAL.id], ProposalStatus.Finished)
+          expect(UpdateModel.createPendingUpdates).toHaveBeenCalledTimes(0)
+          expect(DiscordService.finishProposal).toHaveBeenCalledWith(
+            GRANT_PROPOSAL.id,
+            GRANT_PROPOSAL.title,
+            FINISHED_OUTCOME.outcomeStatus,
+            FINISHED_OUTCOME.winnerChoice
+          )
+        })
+      })
     })
   })
+  // describe('for two grant proposals of the same category', () => {})
 })
