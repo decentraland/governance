b5557f903f68e265d01a3df8fa7bb057bb24c06c
Andy Espagnolo <andyespagnolo@gmail.com>
2023-02-17 11:10:34 -0300

    feat: add helper to lock icon in calculation boxes (#731)

---
+++ src/components/GrantRequest/CalculationHelper.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+
+import Helper from '../Helper/Helper'
+import Lock from '../Icon/Lock'
+
+const CalculationHelper = () => {
+  const t = useFormatMessage()
+
+  return <Helper icon={<Lock />} text={t('page.submit_grant.funding_section.calculation_helper')} />
+}
+
+export default CalculationHelper
