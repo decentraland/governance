866df6f0eb606117c4677765ecb9e74a2d087907
lemu <github@lemu.sh>
2023-02-21 16:06:48 -0300

    refactor: GATSBY_GRANT_VP_THRESHOLD env var refactor (#747)

--- src/modules/env.ts
+++ src/modules/env.ts
@@ -3,13 +3,9 @@ import { Env } from '@dcl/ui-env'
 import { config } from '../config'
 
 export const env = (variable: string, defaultValue?: string) => {
-  if (process.env.HEROKU === 'true') {
-    return process.env[variable]
-  }
-
   return config.get(variable, defaultValue)
 }
 
 export const isDevEnv = () => {
-  return config.getEnv() === Env.LOCAL || config.getEnv() === Env.DEVELOPMENT
+  return config.getEnv() === Env.LOCAL || config.getEnv() === Env.DEVELOPMENT || process.env.HEROKU === 'true'
 }
