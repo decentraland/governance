e085e7d8dee7dc0ceaf9088c2f4a6d1296f3a715
lemu <github@lemu.sh>
2023-02-22 16:19:02 -0300

    chore: revert changes to date tooltip

--- src/components/Common/DateTooltip.tsx
+++ src/components/Common/DateTooltip.tsx
@@ -2,24 +2,14 @@ import React from 'react'
 
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import Time from 'decentraland-gatsby/dist/utils/date/Time'
-import { Mobile, NotMobile } from 'decentraland-ui/dist/components/Media/Media'
 
 Time.extend(LocalizedFormat)
 
 const DateTooltip = ({ children, date }: { children: React.ReactNode; date: Date }) => {
   return (
-    <>
-      <Mobile>
-        <time dateTime={String(date)} title={Time(date).format('l')}>
-          {children}
-        </time>
-      </Mobile>
-      <NotMobile>
-        <time dateTime={String(date)} title={Time(date).format('LLL')}>
-          {children}
-        </time>
-      </NotMobile>
-    </>
+    <time dateTime={String(date)} title={Time(date).format('LLL')}>
+      {children}
+    </time>
   )
 }
 
