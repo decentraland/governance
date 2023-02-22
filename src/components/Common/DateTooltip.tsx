8a3dd13d6f5ed389b70e56b64eb0bafbed12f332
lemu <github@lemu.sh>
2023-02-22 13:33:38 -0300

    chore: adjust date lengths and avatar margins for proposal cards

--- src/components/Common/DateTooltip.tsx
+++ src/components/Common/DateTooltip.tsx
@@ -2,14 +2,24 @@ import React from 'react'
 
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import Time from 'decentraland-gatsby/dist/utils/date/Time'
+import { Mobile, NotMobile } from 'decentraland-ui/dist/components/Media/Media'
 
 Time.extend(LocalizedFormat)
 
 const DateTooltip = ({ children, date }: { children: React.ReactNode; date: Date }) => {
   return (
-    <time dateTime={String(date)} title={Time(date).format('LLL')}>
-      {children}
-    </time>
+    <>
+      <Mobile>
+        <time dateTime={String(date)} title={Time(date).format('l')}>
+          {children}
+        </time>
+      </Mobile>
+      <NotMobile>
+        <time dateTime={String(date)} title={Time(date).format('LLL')}>
+          {children}
+        </time>
+      </NotMobile>
+    </>
   )
 }
 
