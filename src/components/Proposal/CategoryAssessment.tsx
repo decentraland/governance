bfe843ed3f02607a99e74c6971cd7ecfd5b02341
ncomerci <ncomerci@itba.edu.ar>
2023-03-06 13:48:43 -0300

    small fix

--- src/components/Proposal/CategoryAssessment.tsx
+++ src/components/Proposal/CategoryAssessment.tsx
@@ -56,13 +56,11 @@ const CategoryAssessment = ({ data, category }: Props) => {
         </>
       )}
       {category === NewGrantCategory.Platform && (
-        <>
-          <CategoryAssessmentItem
-            label={t('page.proposal_detail.grant.category_assessment.platform.impact_metrics_label')}
-          >
-            {data.impactMetrics}
-          </CategoryAssessmentItem>
-        </>
+        <CategoryAssessmentItem
+          label={t('page.proposal_detail.grant.category_assessment.platform.impact_metrics_label')}
+        >
+          {data.impactMetrics}
+        </CategoryAssessmentItem>
       )}
       {category === NewGrantCategory.Documentation && (
         <>
