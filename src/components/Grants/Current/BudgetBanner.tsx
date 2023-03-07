afe3afce625bf576e1a05d6606d3ee84f9653bd2
lemu <github@lemu.sh>
2023-03-07 13:10:54 -0300

    feat: add category icons to budget banner

--- src/components/Grants/Current/BudgetBanner.tsx
+++ src/components/Grants/Current/BudgetBanner.tsx
@@ -2,10 +2,13 @@ import React, { useMemo } from 'react'
 
 import useFormatMessage, { useIntl } from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
+import snakeCase from 'lodash/snakeCase'
 
 import { GrantStatus, ProposalGrantCategory } from '../../../entities/Grant/types'
 import { PROPOSAL_GRANT_CATEGORY_ALL } from '../../../entities/Proposal/types'
+import { CategoryIconVariant } from '../../../helpers/styles'
 import useBudgetByCategory from '../../../hooks/useBudgetByCategory'
+import { getCategoryIcon } from '../../Category/CategoryOption'
 import ProgressBar from '../../Common/ProgressBar'
 import { Counter } from '../../Search/CategoryFilter'
 
@@ -44,12 +47,18 @@ function BudgetBanner({ category, status, counter }: Props) {
     [category, counter]
   )
   const showProgress = !status || status === GrantStatus.InProgress
+  console.log('category', category)
   return (
     <div className={TokenList.join(['BudgetBanner', !showProgress && 'BudgetBanner--start'])}>
-      <BudgetBannerItem
-        value={intl.formatNumber(totalBudget, currencyFormatter)}
-        label={t('page.grants.budget_banner.budget_label')}
-      />
+      <div className="BudgetBanner__LabelWithIcon">
+        {category !== PROPOSAL_GRANT_CATEGORY_ALL && (
+          <span>{getCategoryIcon(snakeCase(category), CategoryIconVariant.Circled, 48)}</span>
+        )}
+        <BudgetBannerItem
+          value={intl.formatNumber(totalBudget, currencyFormatter)}
+          label={t('page.grants.budget_banner.budget_label')}
+        />
+      </div>
       <BudgetBannerItem
         value={String(initiativesCount)}
         label={`${t('page.grants.budget_banner.progress_label')}${status ? ` ${status.toLowerCase()}` : ''}`}
@@ -61,7 +70,7 @@ function BudgetBanner({ category, status, counter }: Props) {
             percentage={percentage}
             background="linear-gradient(270deg, #A524B3 -0.33%, #FF2D55 100%)"
           />
-          <div className="BudgetBanner__ProgressLabel">
+          <div className="BudgetBannerItem__Label">
             <div>
               <span>{t('page.grants.budget_banner.spent_label')}</span>
               <span className="BudgetBanner__Amount">{intl.formatNumber(currentAmount, currencyFormatter)}</span>
