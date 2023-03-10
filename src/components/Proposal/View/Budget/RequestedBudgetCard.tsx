278f882b6e0ab54cc44b77e28d9d520549602f00
lemu <github@lemu.sh>
2023-03-10 15:23:14 -0300

    feat: over budget status for requested budget card

--- src/components/Proposal/View/Budget/RequestedBudgetCard.tsx
+++ src/components/Proposal/View/Budget/RequestedBudgetCard.tsx
@@ -1,12 +1,14 @@
 import React, { useMemo } from 'react'
 
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 import snakeCase from 'lodash/snakeCase'
 
 import { BudgetWithContestants } from '../../../../entities/Budget/types'
 import { ProposalAttributes } from '../../../../entities/Proposal/types'
 import { getFormattedPercentage } from '../../../../helpers'
 import DistributionBar, { DistributionItemProps } from '../../../Common/DistributionBar/DistributionBar'
+import Pill, { PillColor } from '../../../Common/Pill'
 import { GrantRequestSectionCard } from '../../../GrantRequest/GrantRequestSectionCard'
 
 import './RequestedBudgetCard.css'
@@ -19,42 +21,59 @@ interface Props {
 export default function RequestedBudgetCard({ proposal, budget }: Props) {
   const t = useFormatMessage()
   const grantCategory = proposal.configuration.category
-  const totalCategoryBudget = budget.categories[snakeCase(grantCategory)]?.total || 0
+  const categoryBudget = budget.categories[snakeCase(grantCategory)]
+  const totalCategoryBudget = categoryBudget?.total || 0
   const requestedBudget = proposal.configuration.size
-  const allocatedCategoryBudget = budget.categories[snakeCase(grantCategory)].allocated
-  const remainingTotalBudget = totalCategoryBudget - requestedBudget
-  const remainingTotalBudgetDisplayed = remainingTotalBudget > 0 ? remainingTotalBudget : 0
+  const allocatedCategoryBudget = categoryBudget.allocated
+  const remainingBudget = categoryBudget.available - requestedBudget
+  const remainingBudgetDisplayed = remainingBudget > 0 ? remainingBudget : 0
+  const isOverBudget = remainingBudget < 0
 
   const items: DistributionItemProps[] = useMemo(() => {
     return [
       {
         value: allocatedCategoryBudget,
-        className: 'AllocatedBudgetBar',
+        className: TokenList.join(['AllocatedBudgetBar', isOverBudget && 'AllocatedBudgetBar--overbudget']),
       },
       {
-        value: requestedBudget,
-        className: 'RequestedBudgetBar',
+        value: isOverBudget ? categoryBudget.available : requestedBudget,
+        className: TokenList.join(['RequestedBudgetBar', isOverBudget && 'RequestedBudgetBar--overbudget']),
         selected: true,
       },
       {
-        value: remainingTotalBudgetDisplayed,
-        className: 'TotalBudgetBar',
+        value: remainingBudgetDisplayed,
+        className: 'RemainingBudgetBar',
       },
     ]
-  }, [requestedBudget, remainingTotalBudgetDisplayed])
+  }, [allocatedCategoryBudget, requestedBudget, remainingBudgetDisplayed])
 
   return (
     <GrantRequestSectionCard
-      title={t('page.proposal_detail.grant.requested_budget.title')}
+      title={
+        <div className="RequestedBudgetCard__Title">
+          {t('page.proposal_detail.grant.requested_budget.title')}
+          {isOverBudget && (
+            <Pill style="outline" color={PillColor.Yellow} size={'small'}>
+              {t('page.proposal_detail.grant.requested_budget.overbudget_pill')}
+            </Pill>
+          )}
+        </div>
+      }
       content={
-        <div className="RequestedBudgetCard">
+        <div className="RequestedBudgetCard__Content">
           <>${t('general.number', { value: proposal.configuration.size })}</>
           <DistributionBar items={items} total={totalCategoryBudget} />
         </div>
       }
-      subtitle={t('page.proposal_detail.grant.requested_budget.subtitle', {
-        percentage: getFormattedPercentage(requestedBudget, totalCategoryBudget, 0),
-      })}
+      subtitle={
+        !isOverBudget
+          ? t('page.proposal_detail.grant.requested_budget.subtitle', {
+              percentage: getFormattedPercentage(requestedBudget, totalCategoryBudget, 0),
+            })
+          : t('page.proposal_detail.grant.requested_budget.overbudget_subtitle', {
+              amount: t('general.number', { value: categoryBudget.available }),
+            })
+      }
     />
   )
 }
