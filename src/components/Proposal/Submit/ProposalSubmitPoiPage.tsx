f2b4edbf9f3160bea150768beb32bc122639cfa3
Andy Espagnolo <andyespagnolo@gmail.com>
2023-03-16 18:12:33 -0300

    refactor: add poi using react-hook-form

--- src/components/Proposal/Submit/ProposalSubmitPoiPage.tsx
+++ src/components/Proposal/Submit/ProposalSubmitPoiPage.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Helmet from 'react-helmet'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 
 import Label from 'decentraland-gatsby/dist/components/Form/Label'
 import MarkdownTextarea from 'decentraland-gatsby/dist/components/Form/MarkdownTextarea'
 import Head from 'decentraland-gatsby/dist/components/Head/Head'
 import Paragraph from 'decentraland-gatsby/dist/components/Text/Paragraph'
 import useAuthContext from 'decentraland-gatsby/dist/context/Auth/useAuthContext'
-import useEditor, { assert, createValidator } from 'decentraland-gatsby/dist/hooks/useEditor'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { navigate } from 'decentraland-gatsby/dist/plugins/intl'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
@@ -15,12 +15,7 @@ import { Header } from 'decentraland-ui/dist/components/Header/Header'
 
 import { Governance } from '../../../clients/Governance'
 import { PoiType, getPoiTypeAction, newProposalPOIScheme } from '../../../entities/Proposal/types'
-import {
-  asNumber,
-  isAlreadyPointOfInterest,
-  isValidPointOfInterest,
-  userModifiedForm,
-} from '../../../entities/Proposal/utils'
+import { asNumber, isAlreadyPointOfInterest, isValidPointOfInterest } from '../../../entities/Proposal/utils'
 import loader from '../../../modules/loader'
 import locations from '../../../modules/locations'
 import ErrorMessage from '../../Error/ErrorMessage'
@@ -47,48 +42,11 @@ const initialState: POIState = {
   description: '',
 }
 
-const edit = (state: POIState, props: Partial<POIState>) => {
-  return {
-    ...state,
-    ...props,
-  }
-}
-
-function assertInMap(value: string | number) {
-  if (value === '') {
-    return undefined
-  }
-
-  const x = asNumber(value)
-  return (
-    assert(!Number.isNaN(x) && Number.isFinite(x), 'error.poi.coordinates_not_a_number') ||
-    assert(x <= schema.x.maximum && x >= schema.x.minimum, 'error.poi.coordinates_out_of_map') ||
-    undefined
-  )
-}
-
-const validate = createValidator<POIState>({
-  x: (state) => ({ x: assertInMap(state.x) }),
-  y: (state) => ({ y: assertInMap(state.y) }),
-  description: (state) => ({
-    description: assert(state.description.length <= schema.description.maxLength, 'error.poi.description_too_large'),
-  }),
-  '*': (state) => ({
-    x: assert(state.x !== '', 'error.poi.coordinates_incomplete') || assertInMap(state.x),
-    y: assert(state.y !== '', 'error.poi.coordinates_incomplete') || assertInMap(state.y),
-    description:
-      assert(state.description.length > 0, 'error.poi.description_empty') ||
-      assert(state.description.length >= schema.description.minLength, 'error.poi.description_too_short') ||
-      assert(state.description.length <= schema.description.maxLength, 'error.poi.description_too_large'),
-  }),
-})
-
 async function validateTilePointOfInterest(x: number, y: number) {
   let validPointOfInterest: boolean
   try {
     validPointOfInterest = await isValidPointOfInterest(x, y)
   } catch (err) {
-    console.log(err)
     throw new Error(`error.poi.fetching_tiles`)
   }
 
@@ -102,7 +60,6 @@ async function validateAlreadyPointOfInterest(x: number, y: number, required: bo
   try {
     alreadyPointOfInterest = await isAlreadyPointOfInterest(x, y)
   } catch (err) {
-    console.log(err)
     throw new Error(`error.poi.fetching_pois`)
   }
 
@@ -117,62 +74,89 @@ async function validateAlreadyPointOfInterest(x: number, y: number, required: bo
   }
 }
 
-export type ProposalPoiPageProps = {
+const getPoiErrorMessageKey = (errorType: string) => {
+  switch (errorType) {
+    case 'required':
+      return 'error.poi.coordinates_incomplete'
+    case 'max':
+    case 'min':
+    default:
+      return 'error.poi.coordinates_out_of_map'
+  }
+}
+
+const getDescriptionErrorMessageKey = (errorType: string) => {
+  switch (errorType) {
+    case 'maxLength':
+      return `error.poi.description_too_large`
+    case 'minLength':
+      return `error.poi.description_too_short`
+    default:
+      return `error.poi.description_empty`
+  }
+}
+
+interface Props {
   poiType: PoiType
 }
 
-export default React.memo(function ProposalSubmitPoiPage({ poiType }: ProposalPoiPageProps) {
+export default function ProposalSubmitPoiPage({ poiType }: Props) {
   const t = useFormatMessage()
   const [account, accountState] = useAuthContext()
-  const [state, editor] = useEditor(edit, validate, initialState)
   const [formDisabled, setFormDisabled] = useState(false)
   const action = getPoiTypeAction(poiType)
   const preventNavigation = useRef(false)
+  const [error, setError] = useState('')
 
-  const setCoAuthors = (addresses?: string[]) => editor.set({ coAuthors: addresses })
+  const {
+    handleSubmit,
+    formState: { isDirty, errors },
+    control,
+    setValue,
+    watch,
+  } = useForm<POIState>({ defaultValues: initialState })
+
+  const onSubmit: SubmitHandler<POIState> = async (data) => {
+    setFormDisabled(true)
+
+    try {
+      const x = asNumber(data.x)
+      const y = asNumber(data.y)
+
+      switch (poiType) {
+        case PoiType.AddPOI:
+          await validateAlreadyPointOfInterest(x, y, false)
+          await validateTilePointOfInterest(x, y)
+          break
+        case PoiType.RemovePOI:
+          await validateAlreadyPointOfInterest(x, y, true)
+          break
+        default:
+          throw new Error(`error.poi.invalid_poi_type`)
+      }
+
+      const proposal = await Governance.get().createProposalPOI({
+        ...data,
+        type: poiType,
+        x,
+        y,
+      })
+
+      loader.proposals.set(proposal.id, proposal)
+      navigate(locations.proposal(proposal.id, { new: 'true' }), {
+        replace: true,
+      })
+    } catch (error: any) {
+      setError(error.body?.error || error.message)
+      setFormDisabled(false)
+    }
+  }
+
+  const setCoAuthors = (addresses?: string[]) => setValue('coAuthors', addresses)
 
   useEffect(() => {
-    preventNavigation.current = userModifiedForm(state.value, initialState)
-
-    if (state.validated) {
-      setFormDisabled(true)
-      Promise.resolve()
-        .then(async () => {
-          const x = asNumber(state.value.x)
-          const y = asNumber(state.value.y)
-
-          switch (poiType) {
-            case PoiType.AddPOI:
-              await validateAlreadyPointOfInterest(x, y, false)
-              await validateTilePointOfInterest(x, y)
-              break
-            case PoiType.RemovePOI:
-              await validateAlreadyPointOfInterest(x, y, true)
-              break
-            default:
-              throw new Error(`error.poi.invalid_poi_type`)
-          }
-
-          return Governance.get().createProposalPOI({
-            ...state.value,
-            type: poiType,
-            x,
-            y,
-          })
-        })
-        .then((proposal) => {
-          loader.proposals.set(proposal.id, proposal)
-          navigate(locations.proposal(proposal.id, { new: 'true' }), {
-            replace: true,
-          })
-        })
-        .catch((err) => {
-          console.error(err, { ...err })
-          editor.error({ '*': err.body?.error || err.message })
-          setFormDisabled(false)
-        })
-    }
-  }, [editor, poiType, state])
+    preventNavigation.current = isDirty
+  }, [isDirty])
 
   if (accountState.loading) {
     return <LoadingView />
@@ -192,85 +176,115 @@ export default React.memo(function ProposalSubmitPoiPage({ poiType }: ProposalPo
         image="https://decentraland.org/images/decentraland.png"
       />
       <Helmet title={t(`page.submit_poi.${action}.title`) || ''} />
-      <ContentSection>
-        <Header size="huge">{t(`page.submit_poi.${action}.title`)}</Header>
-      </ContentSection>
-      <ContentSection>
-        <Paragraph small>{t('page.submit_poi.description')}</Paragraph>
-      </ContentSection>
-      <ContentSection className="CoordinatesField">
-        <Label>{t(`page.submit_poi.${action}.coordinates_label`)}</Label>
-        <Paragraph tiny secondary className="details">
-          {t('page.submit_poi.coordinates_detail')}
-        </Paragraph>
-        <div className="CoordinatesField__Inputs">
-          <Field
-            value={state.value.x}
-            type="number"
-            min={schema.x.minimum}
-            max={schema.x.maximum}
-            placeholder={t('page.submit_poi.x_placeholder')}
-            onChange={(_, { value }) => editor.set({ x: value })}
-            error={!!state.error.x}
-            disabled={formDisabled}
-          />
-          <Field
-            value={state.value.y}
-            type="number"
-            min={schema.y.minimum}
-            max={schema.y.maximum}
-            placeholder={t('page.submit_poi.y_placeholder')}
-            onChange={(_, { value }) => editor.set({ y: value })}
-            error={!!state.error.y}
-            disabled={formDisabled}
-          />
-
-          <div className="CoordinatesField__Error">
-            <Paragraph tiny primary>
-              {t(state.error.x) || t(state.error.y)}
-            </Paragraph>
-          </div>
-        </div>
-      </ContentSection>
-      <ContentSection>
-        <Label>
-          {t('page.submit_poi.description_label')}
-          <MarkdownNotice />
-        </Label>
-        <Paragraph tiny secondary className="details">
-          {t(`page.submit_poi.${action}.description_detail`)}
-        </Paragraph>
-        <MarkdownTextarea
-          minHeight={175}
-          value={state.value.description}
-          placeholder={t(`page.submit_poi.${action}.description_placeholder`)}
-          onChange={(_: unknown, { value }: { value: string }) => editor.set({ description: value })}
-          onBlur={() => editor.set({ description: state.value.description.trim() })}
-          error={!!state.error.description}
-          message={
-            t(state.error.description) +
-            ' ' +
-            t('page.submit.character_counter', {
-              current: state.value.description.length,
-              limit: schema.description.maxLength,
-            })
-          }
-          disabled={formDisabled}
-        />
-      </ContentSection>
-      <ContentSection>
-        <CoAuthors setCoAuthors={setCoAuthors} isDisabled={formDisabled} />
-      </ContentSection>
-      <ContentSection>
-        <Button primary disabled={state.validated} loading={state.validated} onClick={() => editor.validate()}>
-          {t('page.submit.button_submit')}
-        </Button>
-      </ContentSection>
-      {state.error['*'] && (
+      <form onSubmit={handleSubmit(onSubmit)}>
         <ContentSection>
-          <ErrorMessage label={t('page.submit.error_label')} errorMessage={t(state.error['*']) || state.error['*']} />
+          <Header size="huge">{t(`page.submit_poi.${action}.title`)}</Header>
         </ContentSection>
-      )}
+        <ContentSection>
+          <Paragraph small>{t('page.submit_poi.description')}</Paragraph>
+        </ContentSection>
+        <ContentSection className="CoordinatesField">
+          <Label>{t(`page.submit_poi.${action}.coordinates_label`)}</Label>
+          <Paragraph tiny secondary className="details">
+            {t('page.submit_poi.coordinates_detail')}
+          </Paragraph>
+          <div className="CoordinatesField__Inputs">
+            <Controller
+              control={control}
+              name="x"
+              rules={{
+                required: true,
+                min: schema.x.minimum,
+                max: schema.x.maximum,
+              }}
+              render={({ field: { ref, ...field } }) => (
+                <Field
+                  type="number"
+                  min={schema.x.minimum}
+                  max={schema.x.maximum}
+                  placeholder={t('page.submit_poi.x_placeholder')}
+                  error={!!errors.x}
+                  disabled={formDisabled}
+                  {...field}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="y"
+              rules={{
+                required: true,
+                min: schema.y.minimum,
+                max: schema.y.maximum,
+              }}
+              render={({ field: { ref, ...field } }) => (
+                <Field
+                  type="number"
+                  min={schema.y.minimum}
+                  max={schema.y.maximum}
+                  placeholder={t('page.submit_poi.y_placeholder')}
+                  error={!!errors.y}
+                  disabled={formDisabled}
+                  {...field}
+                />
+              )}
+            />
+
+            {errors && (
+              <div className="CoordinatesField__Error">
+                <Paragraph tiny primary>
+                  {t(getPoiErrorMessageKey(errors.x?.type || '')) || t(getPoiErrorMessageKey(errors.y?.type || ''))}
+                </Paragraph>
+              </div>
+            )}
+          </div>
+        </ContentSection>
+        <ContentSection>
+          <Label>
+            {t('page.submit_poi.description_label')}
+            <MarkdownNotice />
+          </Label>
+          <Paragraph tiny secondary className="details">
+            {t(`page.submit_poi.${action}.description_detail`)}
+          </Paragraph>
+          <Controller
+            control={control}
+            name="description"
+            rules={{ required: true, minLength: schema.description.minLength, maxLength: schema.description.maxLength }}
+            render={({ field: { ref, ...field } }) => (
+              <MarkdownTextarea
+                minHeight={175}
+                placeholder={t(`page.submit_poi.${action}.description_placeholder`)}
+                disabled={formDisabled}
+                error={!!errors.description}
+                message={
+                  t(getDescriptionErrorMessageKey(errors.description?.type || '')) +
+                  ' ' +
+                  t('page.submit.character_counter', {
+                    current: watch('description').length,
+                    limit: schema.description.maxLength,
+                  })
+                }
+                {...field}
+              />
+            )}
+          />
+        </ContentSection>
+        <ContentSection>
+          <CoAuthors setCoAuthors={setCoAuthors} isDisabled={formDisabled} />
+        </ContentSection>
+        <ContentSection>
+          <Button type="submit" primary loading={formDisabled} disabled={formDisabled}>
+            {t('page.submit.button_submit')}
+          </Button>
+        </ContentSection>
+        {error && (
+          <ContentSection>
+            <ErrorMessage label={t('page.submit.error_label')} errorMessage={t(error) || error} />
+          </ContentSection>
+        )}
+      </form>
     </ContentLayout>
   )
-})
+}
