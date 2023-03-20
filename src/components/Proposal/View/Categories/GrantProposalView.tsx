06d30aa749a4d894bccce9847be68118d5b5e88d
Andy Espagnolo <andyespagnolo@gmail.com>
2023-03-20 15:43:21 -0300

    feat: show payout strategy in grants

--- src/components/Proposal/View/Categories/GrantProposalView.tsx
+++ src/components/Proposal/View/Categories/GrantProposalView.tsx
@@ -27,6 +27,7 @@ function GrantProposalView({ config }: Props) {
     category,
     size,
     projectDuration,
+    vestingStartDate,
     abstract,
     beneficiary,
     email,
@@ -59,6 +60,12 @@ function GrantProposalView({ config }: Props) {
         <ProposalDescriptionItem
           title={t('page.proposal_view.grant.duration_title')}
           body={t('page.proposal_view.grant.breakdown_subtitle', { duration: projectDuration })}
+        />
+      )}
+      {vestingStartDate && (
+        <ProposalDescriptionItem
+          title={t('page.proposal_view.grant.payout_strategy_title')}
+          body={t('page.proposal_view.grant.payout_strategy_body', { startDate: vestingStartDate })}
         />
       )}
       <ProposalDescriptionItem title={t('page.proposal_view.grant.beneficiary_title')} body={beneficiary} />
