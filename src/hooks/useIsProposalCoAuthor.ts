856f079bb917e64ce9948d5797a78704c39fe594
lemu <github@lemu.sh>
2023-03-24 17:10:43 -0300

    chore: remove grant attributes type

--- src/hooks/useIsProposalCoAuthor.ts
+++ src/hooks/useIsProposalCoAuthor.ts
@@ -3,12 +3,12 @@ import { useMemo } from 'react'
 import useAuthContext from 'decentraland-gatsby/dist/context/Auth/useAuthContext'
 
 import { CoauthorStatus } from '../entities/Coauthor/types'
-import { GrantAttributes, ProposalAttributes } from '../entities/Proposal/types'
+import { ProposalAttributes } from '../entities/Proposal/types'
 import { isSameAddress } from '../entities/Snapshot/utils'
 
 import useCoAuthorsByProposal from './useCoAuthorsByProposal'
 
-export default function useIsProposalCoAuthor(proposal: ProposalAttributes | GrantAttributes | null) {
+export default function useIsProposalCoAuthor(proposal: ProposalAttributes | null) {
   const [account] = useAuthContext()
   const coauthorsByProposal = useCoAuthorsByProposal(proposal)
   const isCoauthor = useMemo(
