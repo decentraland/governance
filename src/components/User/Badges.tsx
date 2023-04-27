e4010980d5e2f60329cfaf69bf7f7471d5945c1b
1emu <git@lemu.sh>
2023-04-27 08:03:27 -0300

    feat: miniature badges and show more

--- src/components/User/Badges.tsx
+++ src/components/User/Badges.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
+
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
 
 import useBadges from '../../hooks/useBadges'
 import HelperText from '../Helper/HelperText'
@@ -12,14 +13,17 @@ interface Props {
 
 const NO_IMAGE = require('../../images/no-image.png').default
 
+const MAX_DISPLAYED_BADGES = 2
 export default function Badges({ address }: Props) {
   const { badges, isLoadingBadges } = useBadges(address)
 
+  const displayedBadges = badges?.slice(0, MAX_DISPLAYED_BADGES) ?? []
+  const miniatureBadges = badges?.slice(MAX_DISPLAYED_BADGES) ?? []
+
   return (
     <div className="Badges__Container">
-      {isLoadingBadges && <Skeleton className="Badge" />}
       {!isLoadingBadges &&
-        badges.map((badge) => {
+        displayedBadges.map((badge) => {
           return (
             <div className="Badge" key={`${badge.name}-id`}>
               <div className="Badge__Icon">
@@ -27,11 +31,30 @@ export default function Badges({ address }: Props) {
               </div>
               <div className="Badge__TitleContainer">
                 <HelperText labelText={badge.name} tooltipText={badge.description} position="bottom center" />
-                {badge.amount > 1 && <span className="Badge__Counter">x{badge.amount}</span>}
               </div>
             </div>
           )
         })}
+      {!!miniatureBadges && (
+        <div className="Badge__ShowMore">
+          {miniatureBadges.map((badge, index) => {
+            return (
+              <div
+                className={TokenList.join([
+                  'Badge__MiniIcon',
+                  index === 0 && 'Badge__MiniIcon__Base',
+                  index > 0 && 'Badge__MiniIcon__Overlapping',
+                ])}
+                key={`${badge.name}-id`}
+                style={{ zIndex: index }}
+              >
+                <img src={badge.image} onError={(e) => (e.currentTarget.src = NO_IMAGE)} />
+              </div>
+            )
+          })}
+          <span className="Badge__Counter">{badges.length - MAX_DISPLAYED_BADGES} MORE</span>
+        </div>
+      )}
     </div>
   )
 }
