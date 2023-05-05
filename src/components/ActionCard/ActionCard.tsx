3efc61fc7fd2943e2602c6242b74815714642ae9
ncomerci <ncomerci@itba.edu.ar>
2023-05-05 16:39:17 -0300

    started forum connect modal

--- src/components/ActionCard/ActionCard.tsx
+++ src/components/ActionCard/ActionCard.tsx
@@ -6,6 +6,8 @@ import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import { Card } from 'decentraland-ui/dist/components/Card/Card'
 import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid/Grid'
 
+import Lock from '../Icon/Lock'
+
 import './ActionCard.css'
 
 export interface ActionCardProps {
@@ -15,28 +17,47 @@ export interface ActionCardProps {
   action?: () => void
   action_title?: string
   onCardClick?: () => void
+  isDisabled?: boolean
 }
 
-function ActionCard({ icon, title, description, action, action_title, onCardClick }: ActionCardProps) {
+function ActionCard({
+  icon,
+  title,
+  description,
+  action,
+  action_title,
+  onCardClick,
+  isDisabled = false,
+}: ActionCardProps) {
   const t = useFormatMessage()
-  const isDisabled = !action && !onCardClick
+  const isImplemented = !!action || !!onCardClick
   return (
-    <Card className={TokenList.join(['ActionCard', isDisabled && 'ActionCard--disabled'])} onClick={onCardClick}>
+    <Card
+      className={TokenList.join(['ActionCard', (!isImplemented || isDisabled) && 'ActionCard--disabled'])}
+      onClick={onCardClick}
+    >
       <Grid>
         <Grid.Row>
           <Grid.Column width={3}>{icon}</Grid.Column>
           <Grid.Column width={13}>
             <div>
               <h3>
-                {title} {isDisabled && <span className="ActionCard__SoonLabel">{t('modal.identity_setup.soon')}</span>}
+                {title}
+                {!isImplemented && <span className="ActionCard__SoonLabel">{t('modal.identity_setup.soon')}</span>}
               </h3>
               <p>{description}</p>
             </div>
-            {action && action_title && (
-              <Button basic onClick={action}>
-                {action_title}
-              </Button>
-            )}
+            <div className="ActionCard__Action">
+              {action &&
+                action_title &&
+                (isDisabled ? (
+                  <Lock />
+                ) : (
+                  <Button basic onClick={action}>
+                    {action_title}
+                  </Button>
+                ))}
+            </div>
           </Grid.Column>
         </Grid.Row>
       </Grid>
--- src/components/Modal/IdentityConnectModal/AccountsConnectModal.tsx
+++ src/components/Modal/IdentityConnectModal/AccountsConnectModal.tsx
@@ -1,47 +1,113 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Close } from 'decentraland-ui/dist/components/Close/Close'
 import { Modal, ModalProps } from 'decentraland-ui/dist/components/Modal/Modal'
 
 import ActionCard, { ActionCardProps } from '../../ActionCard/ActionCard'
+import Comment from '../../Icon/Comment'
+import Copy from '../../Icon/Copy'
 import Discourse from '../../Icon/Discourse'
+import Sign from '../../Icon/Sign'
 
 import './AccountsConnectModal.css'
 
-const CARDS: ActionCardProps[] = [
-  {
-    title: 'modal.identity_setup.forum.card_title',
-    description: 'modal.identity_setup.forum.card_description',
-    icon: <Discourse />,
-    onCardClick: () => ({}),
-  },
-  {
-    title: 'modal.identity_setup.discord.card_title',
-    description: 'modal.identity_setup.discord.card_description',
-    icon: <Discourse />,
-  },
-  {
-    title: 'modal.identity_setup.twitter.card_title',
-    description: 'modal.identity_setup.twitter.card_description',
-    icon: <Discourse />,
-  },
-]
+type ModalState = {
+  title: string
+  actions: ActionCardProps[]
+  button?: string
+}
+
+enum ModalType {
+  CHOOSE_ACCOUNT,
+  FORUM,
+}
+
+const FORUM_CONNECT_STEPS_AMOUNT = 3
+
+function getAccountActionSteps(
+  account: 'forum' | 'discord' | 'twitter',
+  stepsAmount: number,
+  icons: React.ReactNode[],
+  actions: (() => void)[],
+  currentStep: number
+): ActionCardProps[] {
+  if (stepsAmount === 0 || (icons.length !== stepsAmount && actions.length !== stepsAmount)) {
+    console.error('Invalid steps amount')
+    return []
+  }
+  return Array.from({ length: stepsAmount }, (_, index) => ({
+    title: `modal.identity_setup.${account}.title_step_${index + 1}`,
+    description: `modal.identity_setup.${account}.description_step_${index + 1}`,
+    icon: icons[index],
+    action_title: `modal.identity_setup.${account}.action_step_${index + 1}`,
+    action: actions[index],
+    isDisabled: index + 1 > currentStep,
+  }))
+}
 
 function AccountsConnectModal({ open, onClose }: ModalProps) {
   const t = useFormatMessage()
+  const [currentState, setCurrentState] = useState(ModalType.CHOOSE_ACCOUNT)
+  const [currentStep, setCurrentStep] = useState(1)
+
+  const stateMap: Record<ModalType, ModalState> = useMemo(
+    () => ({
+      [ModalType.CHOOSE_ACCOUNT]: {
+        title: 'modal.identity_setup.title',
+        actions: [
+          {
+            title: 'modal.identity_setup.forum.card_title',
+            description: 'modal.identity_setup.forum.card_description',
+            icon: <Discourse />,
+            onCardClick: () => setCurrentState(ModalType.FORUM),
+          },
+          {
+            title: 'modal.identity_setup.discord.card_title',
+            description: 'modal.identity_setup.discord.card_description',
+            icon: <Discourse />,
+          },
+          {
+            title: 'modal.identity_setup.twitter.card_title',
+            description: 'modal.identity_setup.twitter.card_description',
+            icon: <Discourse />,
+          },
+        ],
+      },
+      [ModalType.FORUM]: {
+        title: 'modal.identity_setup.forum.title',
+        actions: getAccountActionSteps(
+          'forum',
+          FORUM_CONNECT_STEPS_AMOUNT,
+          [<Sign key="sign" />, <Copy key="copy" />, <Comment key="comment" />],
+          [() => ({}), () => ({}), () => ({})],
+          currentStep
+        ),
+      },
+    }),
+    [currentStep]
+  )
+
   return (
-    <Modal open={open} className="AccountsConnectModal" size="tiny" onClose={onClose} closeIcon={<Close />}>
-      <Modal.Header className="AccountsConnectModal__Header">{t('modal.identity_setup.title')}</Modal.Header>
-      <Modal.Content>
-        {CARDS.map((cardProps, index) => {
-          const { title, description } = cardProps
-          return (
-            <ActionCard key={`ActionCard--${index}`} {...cardProps} title={t(title)} description={t(description)} />
-          )
-        })}
-      </Modal.Content>
-    </Modal>
+    <>
+      <Modal open={open} className="AccountsConnectModal" size="tiny" onClose={onClose} closeIcon={<Close />}>
+        <Modal.Header className="AccountsConnectModal__Header">{t(stateMap[currentState].title)}</Modal.Header>
+        <Modal.Content>
+          {stateMap[currentState].actions.map((cardProps, index) => {
+            const { title, description, action_title } = cardProps
+            return (
+              <ActionCard
+                key={`ActionCard--${index}`}
+                {...cardProps}
+                title={t(title)}
+                description={t(description)}
+                action_title={t(action_title)}
+              />
+            )
+          })}
+        </Modal.Content>
+      </Modal>
+    </>
   )
 }
 
