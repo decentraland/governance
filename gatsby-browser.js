239fa5cd68e8c15c2a9555cd93c3187380c82061
Andy Espagnolo <andyespagnolo@gmail.com>
2023-05-19 17:09:39 -0300

    remove light theme import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,6 @@ import 'core-js/features/set-immediate'
 import 'semantic-ui-css/semantic.min.css'
 import 'balloon-css/balloon.min.css'
 import 'decentraland-ui/dist/themes/base-theme.css'
-import 'decentraland-ui/dist/themes/alternative/light-theme.css'
 import './src/theme.css'
 
 import { IntlProvider } from 'decentraland-gatsby/dist/plugins/intl'
