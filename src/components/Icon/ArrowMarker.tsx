e5da3d8fe7a6db26d5107ca7267544a0fe93fb83
lemu <github@lemu.sh>
2023-06-07 12:59:46 -0300

    Competing proposals v2 (#1005)
    * chore: remove repeated color from theme.css
    
    * feat: style changes for distribution bar popup
    
    * feat: update competing proposals distribution bar
    
    * chore: refactors and visual tweaks
    
    * chore: internationalization renames
    
    * chore: refactor competing proposals sidebar into smaller components
    
    * chore: css refactors
    
    * chore: css tweaks
    
    * chore: rename
    
    * feat: hide this initiative popup on hover, and allow hover on overbudget bar
    
    * feat: more style tweaks

---
+++ src/components/Icon/ArrowMarker.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import TokenList from 'decentraland-gatsby/dist/utils/dom/TokenList'
+
+type Props = {
+  className?: string
+}
+
+function ArrowMarker({ className }: Props) {
+  return (
+    <svg
+      className={TokenList.join(['ArrowMarker', className])}
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path opacity="0.5" d="M0 24L3 21L6 24H0Z" fill="#555555" />
+      <path opacity="0.5" d="M6 0L3 3L2.62268e-07 -5.24537e-07L6 0Z" fill="#555555" />
+    </svg>
+  )
+}
+
+export default ArrowMarker
