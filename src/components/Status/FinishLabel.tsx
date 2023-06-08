87a55ae868ae02131aac275585ac4cffcf8e784c
Andy Espagnolo <andyespagnolo@gmail.com>
2023-06-08 17:12:42 -0300

    fix: finish label start at date (#1011)
    * fix: finish label start at date
    
    * fix: has started condition

--- src/components/Status/FinishLabel.tsx
+++ src/components/Status/FinishLabel.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 
 import useCountdown, { Countdown } from 'decentraland-gatsby/dist/hooks/useCountdown'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
@@ -14,29 +14,30 @@ interface Props {
   finishAt: Date
 }
 
-function getTimeLabel(timeout: Countdown, time: Time.Dayjs, format?: 'short') {
+function getTimeLabel(timeout: Countdown, date: Date, format?: 'short') {
+  const time = Time(date)
+
   return timeout.time > 0 ? time.fromNow() : format === 'short' ? time.format('MM/DD/YY') : time.format('MMM DD, YYYY')
 }
 
 export default function FinishLabel({ startAt, finishAt }: Props) {
-  const time = useMemo(() => Time.from(finishAt), [finishAt])
   const timeout = useCountdown(finishAt)
   const t = useFormatMessage()
   const isCountdownRunning = timeout.time > 0
-  const hasStarted = Time().isBefore(startAt)
-  const tooltipDate = hasStarted ? startAt : finishAt
+  const hasStarted = Time().isAfter(startAt)
   const endLabel = isCountdownRunning
     ? `${t('page.proposal_list.finish_label.ends')} `
     : `${t('page.proposal_list.finish_label.ended')} `
-  const label = hasStarted ? `${t('page.proposal_list.finish_label.starts')} ` : endLabel
+  const label = hasStarted ? endLabel : `${t('page.proposal_list.finish_label.starts')} `
+  const time = hasStarted ? finishAt : startAt
 
   return (
     <span className="FinishLabel">
       <Mobile>
-        <DateTooltip date={tooltipDate}>{`${label} ${getTimeLabel(timeout, time, 'short')}`}</DateTooltip>
+        <DateTooltip date={time}>{`${label} ${getTimeLabel(timeout, time, 'short')}`}</DateTooltip>
       </Mobile>
       <NotMobile>
-        <DateTooltip date={tooltipDate}>{`${label} ${getTimeLabel(timeout, time)}`}</DateTooltip>
+        <DateTooltip date={time}>{`${label} ${getTimeLabel(timeout, time)}`}</DateTooltip>
       </NotMobile>
     </span>
   )
