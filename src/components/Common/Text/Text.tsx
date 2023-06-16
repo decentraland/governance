9a766da44ddba4baa0140b0f75435338d821beea
lemu <github@lemu.sh>
2023-06-16 16:08:24 -0300

    refactor: remove unused text props

--- src/components/Common/Text/Text.tsx
+++ src/components/Common/Text/Text.tsx
@@ -10,32 +10,23 @@ const DEFAULT_FONT_SIZE = 'md'
 export type FontSize = 'xs' | 'sm' | typeof DEFAULT_FONT_SIZE | 'lg' | 'xl'
 type FontWeight = 'bold' | 'semi-bold' | typeof DEFAULT_FONT_WEIGHT
 type TextColor = typeof DEFAULT_COLOR | 'primary' | 'secondary'
-type TextTransform = 'uppercase' | 'lowercase'
 
 interface Props {
   children?: React.ReactNode
   size?: FontSize
   weight?: FontWeight
-  transform?: TextTransform
   color?: TextColor
-  span?: boolean
   className?: string
 }
-export default function Text({ children, size, weight, transform, color, span, className }: Props) {
+
+export default function Text({ children, size, weight, color, className }: Props) {
   color = color || DEFAULT_COLOR
   size = size || DEFAULT_FONT_SIZE
   weight = weight || DEFAULT_FONT_WEIGHT
 
   return (
     <p
-      className={classNames(
-        'Text',
-        className,
-        `Text--size-${size}`,
-        `Text--weight-${weight}`,
-        `Text--color-${color}`,
-        transform && `Text--transform-${transform}`
-      )}
+      className={classNames('Text', className, `Text--size-${size}`, `Text--weight-${weight}`, `Text--color-${color}`)}
     >
       {children}
     </p>
