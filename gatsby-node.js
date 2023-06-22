b584de5b070fbd76090627426db1c09005698373
ncomerci <ncomerci@itba.edu.ar>
2023-06-22 16:10:25 -0300

    comments removed

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,13 +26,10 @@ exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
   // This is only an issue with regular CSS being imported.
   if (stage === 'build-javascript' || stage === 'develop') {
     const config = getConfig()
-    // Get the mini-css-extract-plugin
     const miniCssExtractPlugin = config.plugins.find((plugin) => plugin.constructor.name === 'MiniCssExtractPlugin')
-    // Set the option here to true.
     if (miniCssExtractPlugin) {
       miniCssExtractPlugin.options.ignoreOrder = true
     }
-    // Update the config.
     actions.replaceWebpackConfig(config)
   }
 }
