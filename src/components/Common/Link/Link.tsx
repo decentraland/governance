7fd1e9f9576281b85d00741955fc500d9933ceb3
lemu <github@lemu.sh>
2023-06-22 12:09:31 -0300

    refactor: update link component

--- src/components/Common/Link/Link.tsx
+++ src/components/Common/Link/Link.tsx
@@ -1,8 +1,12 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 
 import classNames from 'classnames'
 
+import { navigate } from '../../../utils/locations'
+
 import './Link.css'
+
+type Props = React.AnchorHTMLAttributes<HTMLAnchorElement>
 
 function isLocalLink(href?: string | null) {
   return (
@@ -10,19 +14,28 @@ function isLocalLink(href?: string | null) {
   )
 }
 
-export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>
-
-export default function Link({ target, rel, ...props }: LinkProps) {
-  const isLocal = isLocalLink(props.href)
-  const linkTarget = useMemo(() => (!target && !isLocal ? '_blank' : target || undefined), [isLocal, target])
-  const linkRel = useMemo(() => (!isLocal ? classNames(rel, 'noopener', 'noreferrer') : rel), [isLocal, rel])
+export default function Link({ target, rel, href, onClick, className, ...props }: Props) {
+  const isLocal = isLocalLink(href)
+  const linkTarget = !target && !isLocal ? '_blank' : target || undefined
+  const linkRel = !isLocal ? classNames(rel, 'noopener', 'noreferrer') : rel
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (onClick) {
+      onClick(e)
+    }
+    if (isLocal && href) {
+      e.preventDefault()
+      navigate(href)
+    }
+  }
 
   return (
     <a
       {...props}
-      className={classNames('Link', (props.onClick || props.href) && 'Link--pointer', props.className)}
+      className={classNames('Link', (onClick || href) && 'Link--pointer', className)}
       target={linkTarget}
       rel={linkRel}
+      href={href}
+      onClick={handleClick}
     />
   )
 }
