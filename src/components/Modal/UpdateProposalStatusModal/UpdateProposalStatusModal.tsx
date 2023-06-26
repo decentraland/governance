f5e0e51aec2211bbddbfbe91a5b3a768988950a6
Andy Espagnolo <andyespagnolo@gmail.com>
2023-06-26 11:48:02 -0300

    refactor: use react-hook-form in update proposal status modal

--- src/components/Modal/UpdateProposalStatusModal/UpdateProposalStatusModal.tsx
+++ src/components/Modal/UpdateProposalStatusModal/UpdateProposalStatusModal.tsx
@@ -1,13 +1,11 @@
-import React, { useMemo } from 'react'
+import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import classNames from 'classnames'
-import MarkdownTextarea from 'decentraland-gatsby/dist/components/Form/MarkdownTextarea'
 import Markdown from 'decentraland-gatsby/dist/components/Text/Markdown'
-import useEditor, { assert, createValidator } from 'decentraland-gatsby/dist/hooks/useEditor'
 import useFormatMessage from 'decentraland-gatsby/dist/hooks/useFormatMessage'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import { Close } from 'decentraland-ui/dist/components/Close/Close'
-import { Field } from 'decentraland-ui/dist/components/Field/Field'
 import { Header } from 'decentraland-ui/dist/components/Header/Header'
 import { Modal, ModalProps } from 'decentraland-ui/dist/components/Modal/Modal'
 import isEthereumAddress from 'validator/lib/isEthereumAddress'
@@ -15,6 +13,8 @@ import isEthereumAddress from 'validator/lib/isEthereumAddress'
 import { GrantTier } from '../../../entities/Grant/GrantTier'
 import { ProposalAttributes, ProposalStatus, ProposalType } from '../../../entities/Proposal/types'
 import { isValidTransactionHash } from '../../../entities/Proposal/utils'
+import Field from '../../Common/Form/Field'
+import MarkdownField from '../../Common/Form/MarkdownField'
 import Label from '../../Common/Label'
 import '../ProposalModal.css'
 
@@ -27,49 +27,36 @@ type UpdateProposalState = {
   description: string
 }
 
-const initialPollState: UpdateProposalState = {
+const initialUpdateProposalState: UpdateProposalState = {
   proposal: null,
   vestingAddress: '',
   enactingTx: '',
   description: '',
 }
 
-const edit = (state: UpdateProposalState, props: Partial<UpdateProposalState>) => {
-  return {
-    ...state,
-    ...props,
-  }
-}
-
-const validate = createValidator<UpdateProposalState>({
-  vestingAddress: (state) => ({
-    vestingAddress: assert(
-      !state.vestingAddress || isEthereumAddress(state.vestingAddress),
-      'error.update_status_proposal.grant_vesting_address_invalid'
-    ),
-  }),
-  enactingTx: (state) => ({
-    enactingTx: assert(
-      !state.enactingTx || isValidTransactionHash(state.enactingTx),
-      'error.update_status_proposal.grant_enacting_tx_invalid'
-    ),
-  }),
-  description: () => ({
-    description: undefined,
-  }),
-})
-
-export type UpdateProposalStatusModalProps = Omit<ModalProps, 'children'> & {
+type Props = Omit<ModalProps, 'children'> & {
   proposal?: ProposalAttributes | null
   status?: ProposalStatus | null
   loading?: boolean
-  onClickAccept?: (
-    e: React.MouseEvent<unknown>,
+  onClickAccept: (
     status: ProposalStatus,
     vestingContract: string | null,
     enactingTx: string | null,
     description: string
   ) => void
+}
+
+const getPrimaryButtonTextKey = (status?: ProposalStatus | null) => {
+  switch (status) {
+    case ProposalStatus.Enacted:
+      return 'page.proposal_detail.enact'
+    case ProposalStatus.Passed:
+      return 'page.proposal_detail.pass'
+    case ProposalStatus.Rejected:
+      return 'page.proposal_detail.reject'
+    default:
+      return 'modal.update_status_proposal.accept'
+  }
 }
 
 export function UpdateProposalStatusModal({
@@ -78,101 +65,105 @@ export function UpdateProposalStatusModal({
   status,
   loading,
   open,
+  className,
   ...props
-}: UpdateProposalStatusModalProps) {
+}: Props) {
   const t = useFormatMessage()
-  const [state, editor] = useEditor(edit, validate, initialPollState)
+  const {
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    control,
+  } = useForm<UpdateProposalState>({ defaultValues: initialUpdateProposalState, mode: 'onTouched' })
   const isOneTimePaymentProposalTier = GrantTier.isOneTimePaymentTier(proposal?.configuration.tier)
 
-  function handleAccept(e: React.MouseEvent<unknown>) {
-    editor.validate()
-    const hasErrors = Object.keys(state.error).length != 0
-    if (status && !hasErrors && onClickAccept) {
+  const onSubmit: SubmitHandler<UpdateProposalState> = async (data) => {
+    if (status) {
       onClickAccept(
-        e,
         status,
-        state.value.vestingAddress ? state.value.vestingAddress : null,
-        state.value.enactingTx ? state.value.enactingTx : null,
-        state.value.description
+        data.vestingAddress ? data.vestingAddress : null,
+        data.enactingTx ? data.enactingTx : null,
+        data.description
       )
     }
   }
-
-  const cta = useMemo(() => {
-    switch (status) {
-      case ProposalStatus.Enacted:
-        return 'page.proposal_detail.enact'
-      case ProposalStatus.Passed:
-        return 'page.proposal_detail.pass'
-      case ProposalStatus.Rejected:
-        return 'page.proposal_detail.reject'
-      default:
-        return 'modal.update_status_proposal.accept'
-    }
-  }, [status])
 
   return (
     <Modal
       {...props}
       open={open && !!status}
       size="small"
-      className={classNames('GovernanceActionModal', 'ProposalModal', 'UpdateProposalStatusModal', props.className)}
+      className={classNames('GovernanceActionModal', 'ProposalModal', 'UpdateProposalStatusModal', className)}
       closeIcon={<Close />}
     >
       <Modal.Content>
-        <div className="ProposalModal__Title">
-          <Header>{t('modal.update_status_proposal.title', { status })}</Header>
-          <Markdown>{t('modal.update_status_proposal.description', { status }) || ''}</Markdown>
-        </div>
-
-        {proposal && proposal.type === ProposalType.Grant && (
-          <div className="ProposalModal__GrantTransaction">
-            {!isOneTimePaymentProposalTier && (
-              <>
-                <Label>{t('modal.update_status_proposal.grant_vesting_address')}</Label>
-                <Field
-                  type="address"
-                  value={state.value.vestingAddress}
-                  onChange={(_, { value }) => editor.set({ vestingAddress: value }, { validate: false })}
-                  onBlur={() => editor.set({ vestingAddress: state.value.vestingAddress.trim() })}
-                  message={t(state.error.vestingAddress)}
-                  error={!!state.error.vestingAddress}
-                />
-              </>
-            )}
-            {isOneTimePaymentProposalTier && (
-              <>
-                <Label>{t('modal.update_status_proposal.grant_enacting_tx')}</Label>
-                <Field
-                  type="address"
-                  value={state.value.enactingTx}
-                  onChange={(_, { value }) => editor.set({ enactingTx: value }, { validate: false })}
-                  onBlur={() => editor.set({ enactingTx: state.value.enactingTx.trim() })}
-                  message={t(state.error.enactingTx)}
-                  error={!!state.error.enactingTx}
-                />
-              </>
-            )}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="ProposalModal__Title">
+            <Header>{t('modal.update_status_proposal.title', { status })}</Header>
+            <Markdown>{t('modal.update_status_proposal.description', { status }) || ''}</Markdown>
           </div>
-        )}
-        <div className="ProposalModal__Form">
-          <Label>{t('modal.update_status_proposal.comments')}</Label>
-          <MarkdownTextarea
-            minHeight={150}
-            value={state.value.description}
-            onChange={(_: unknown, { value }: { value: string }) =>
-              editor.set({ description: value }, { validate: false })
-            }
-          />
-        </div>
-        <div className="ProposalModal__Actions">
-          <Button primary fluid disabled={state.validated} loading={loading && state.validated} onClick={handleAccept}>
-            {t(cta)}
-          </Button>
-          <Button className="cancel" fluid onClick={props.onClose}>
-            {t('modal.update_status_proposal.reject')}
-          </Button>
-        </div>
+
+          {proposal && proposal.type === ProposalType.Grant && (
+            <div className="ProposalModal__GrantTransaction">
+              {!isOneTimePaymentProposalTier && (
+                <>
+                  <Label>{t('modal.update_status_proposal.grant_vesting_address')}</Label>
+                  <Field
+                    control={control}
+                    name="vestingAddress"
+                    type="address"
+                    message={errors.vestingAddress?.message}
+                    error={!!errors.vestingAddress}
+                    rules={{
+                      validate: (value: string) => {
+                        if (!isEthereumAddress(value)) {
+                          return t('error.update_status_proposal.grant_vesting_address_invalid')
+                        }
+                      },
+                    }}
+                  />
+                </>
+              )}
+              {isOneTimePaymentProposalTier && (
+                <>
+                  <Label>{t('modal.update_status_proposal.grant_enacting_tx')}</Label>
+                  <Field
+                    control={control}
+                    name="enactingTx"
+                    type="address"
+                    message={errors.enactingTx?.message}
+                    error={!!errors.enactingTx}
+                    rules={{
+                      validate: (value: string) => {
+                        if (!isValidTransactionHash(value)) {
+                          return t('error.update_status_proposal.grant_enacting_tx_invalid')
+                        }
+                      },
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          )}
+          <div className="ProposalModal__Form">
+            <Label>{t('modal.update_status_proposal.comments')}</Label>
+            <MarkdownField control={control} name="description" />
+          </div>
+          <div className="ProposalModal__Actions">
+            <Button type="submit" primary fluid disabled={isSubmitting} loading={loading && isSubmitting}>
+              {t(getPrimaryButtonTextKey(status))}
+            </Button>
+            <Button
+              className="cancel"
+              fluid
+              onClick={(e) => {
+                e.preventDefault()
+                props.onClose()
+              }}
+            >
+              {t('modal.update_status_proposal.reject')}
+            </Button>
+          </div>
+        </form>
       </Modal.Content>
     </Modal>
   )
