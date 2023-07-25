4ea610926c369ef1a45d500178b1de6584ce1bce
1emu <github@lemu.sh>
2023-07-25 12:34:20 -0300

    feat: remove passed grants update creation

--- src/entities/Updates/utils.ts
+++ src/entities/Updates/utils.ts
@@ -4,10 +4,8 @@ import { ProposalStatus } from '../Proposal/types'
 
 import { UpdateAttributes, UpdateStatus } from './types'
 
-const PROPOSAL_STATUS_WITH_UPDATES = new Set([ProposalStatus.Passed, ProposalStatus.Enacted])
-
 export function isProposalStatusWithUpdates(proposalStatus?: ProposalStatus) {
-  return PROPOSAL_STATUS_WITH_UPDATES.has(proposalStatus as ProposalStatus)
+  return proposalStatus === ProposalStatus.Enacted
 }
 
 export const getPublicUpdates = (updates: UpdateAttributes[]): UpdateAttributes[] => {
