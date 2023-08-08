0ce87df2af20d39132f2efd350c088565e951947
lemu <github@lemu.sh>
2023-08-08 12:50:44 -0300

    feat: legislator badge (#1157)
    * chore: hardhat tasks for airdropping
    
    * chore: methods for airdropping to governance proposal author and coauthors
    
    * feat: airdrop badge WIP
    
    * chore: airdrop badge to user from debug section
    
    * chore: pump gas when airdrop fails due to underpriced transaction
    
    * chore: remove hardhat configs
    
    * refactor: move PendingProposalsQuery to proposal/types
    
    * refactor: remove console logs and tiny thingies
    
    * refactor: address pr comments
    
    * refactor: address pr comments
    
    ---------
    
    Co-authored-by: 1emu <git@lemu.sh>

--- src/back/routes/badges.ts
+++ src/back/routes/badges.ts
@@ -1,17 +1,38 @@
+import { WithAuth, auth } from 'decentraland-gatsby/dist/entities/Auth/middleware'
+import RequestError from 'decentraland-gatsby/dist/entities/Route/error'
 import handleAPI from 'decentraland-gatsby/dist/entities/Route/handle'
 import routes from 'decentraland-gatsby/dist/entities/Route/routes'
 import { Request } from 'express'
 
 import { UserBadges } from '../../entities/Badges/types'
+import isDebugAddress from '../../entities/Debug/isDebugAddress'
 import { BadgesService } from '../../services/BadgesService'
 import { validateAddress } from '../utils/validations'
 
 export default routes((router) => {
+  const withAuth = auth()
   router.get('/badges/:address/', handleAPI(getBadges))
+  router.post('/badges/airdrop/', withAuth, handleAPI(airdropBadges))
 })
 
 async function getBadges(req: Request<{ address: string }>): Promise<UserBadges> {
   const address = req.params.address
   validateAddress(address)
   return await BadgesService.getBadges(address)
+}
+
+async function airdropBadges(req: WithAuth): Promise<string> {
+  const user = req.auth!
+  const recipients: string[] = req.body.recipients
+  const badgeSpecCId = req.body.badgeSpecCid
+
+  if (!isDebugAddress(user)) {
+    throw new RequestError('Invalid user', RequestError.Unauthorized)
+  }
+
+  recipients.map((address) => {
+    validateAddress(address)
+  })
+
+  return await BadgesService.grantBadgeToUsers(badgeSpecCId, recipients)
 }
