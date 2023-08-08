037d75c8da41743c047655fbbe25aa9418e2d5b1
1emu <github@lemu.sh>
2023-08-08 11:00:09 -0300

    feat: airdrop jobs

--- src/back/jobs/BadgeAirdrop.ts
+++ src/back/jobs/BadgeAirdrop.ts
@@ -1,27 +1,24 @@
+import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { v1 as uuid } from 'uuid'
 
+import { LEGISLATOR_BADGE_SPEC_CID } from '../../constants'
 import { ProposalAttributes, ProposalType } from '../../entities/Proposal/types'
 import { ErrorService } from '../../services/ErrorService'
 import { ErrorCategory } from '../../utils/errorCategories'
 import AirdropJob from '../models/AirdropJob'
 
-const LEGISLATOR_BADGE_SPEC_CID = process.env.LEGISLATOR_BADGE_SPEC_CID || ''
-
 export async function giveLegislatorBadges(acceptedProposals: ProposalAttributes[]) {
   const authorsAndCoauthors = acceptedProposals.flatMap((acceptedProposal) => {
     if (acceptedProposal.type === ProposalType.Governance) {
-      const {
-        user,
-        configuration: { coauthors },
-      } = acceptedProposal
-      return [user, ...coauthors]
+      return [acceptedProposal.user, ...acceptedProposal.configuration.coAuthors]
     }
     return []
   })
   await queueAirdropJob(LEGISLATOR_BADGE_SPEC_CID, authorsAndCoauthors)
 }
 
-async function queueAirdropJob(badgeSpec: string, recipients: string[]) {
+export async function queueAirdropJob(badgeSpec: string, recipients: string[]) {
+  logger.log(`Enqueueing airdrop job`, { badgeSpec, recipients })
   try {
     await AirdropJob.create({ id: uuid(), badge_spec: badgeSpec, recipients })
   } catch (error) {
