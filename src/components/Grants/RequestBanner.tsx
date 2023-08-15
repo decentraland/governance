ea37aa476200304a7dd65194f25081da447bf445
Andy Espagnolo <andyespagnolo@gmail.com>
2023-08-15 17:54:09 -0300

    feat: request a grant button is now a link

--- src/components/Grants/RequestBanner.tsx
+++ src/components/Grants/RequestBanner.tsx
@@ -4,21 +4,23 @@ import { Button } from 'decentraland-ui/dist/components/Button/Button'
 
 import { ProposalType } from '../../entities/Proposal/types'
 import useFormatMessage from '../../hooks/useFormatMessage'
-import locations, { navigate } from '../../utils/locations'
+import locations from '../../utils/locations'
+import Link from '../Common/Typography/Link'
 
 import './RequestBanner.css'
 
-function RequestBanner() {
+export default function RequestBanner() {
   const t = useFormatMessage()
+
   return (
     <div className="RequestBanner">
       <div className="RequestBanner__Title">{t('page.grants.request_banner.title')}</div>
       <div className="RequestBanner__Description">{t('page.grants.request_banner.description')}</div>
-      <Button fluid className="RequestBanner__Button" onClick={() => navigate(locations.submit(ProposalType.Grant))}>
-        {t('page.grants.request_banner.button')}
-      </Button>
+      <Link href={locations.submit(ProposalType.Grant)}>
+        <Button fluid className="RequestBanner__Button">
+          {t('page.grants.request_banner.button')}
+        </Button>
+      </Link>
     </div>
   )
 }
-
-export default RequestBanner
