05e5a085f822adc555039a2b52638b01696dd070
Andy Espagnolo <andyespagnolo@gmail.com>
2023-08-18 15:57:23 -0300

    feat: add more filters

--- src/components/Grants/Current/Stats.tsx
+++ src/components/Grants/Current/Stats.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useIntl } from 'react-intl'
 
-import { ProjectStatus, SubtypeAlternativeOptions } from '../../../entities/Grant/types'
-import { GrantWithUpdate } from '../../../entities/Proposal/types'
+import { ProjectStatus } from '../../../entities/Grant/types'
+import { GrantWithUpdate, ProposalType } from '../../../entities/Proposal/types'
 import { CURRENCY_FORMAT_OPTIONS } from '../../../helpers'
-import useBudgetByCategory from '../../../hooks/useBudgetByCategory'
 import Time from '../../../utils/date/Time'
 import MetricsCard from '../../Home/MetricsCard'
 
@@ -17,30 +16,53 @@ interface Props {
 
 export default function Stats({ projects }: Props) {
   const intl = useIntl()
-  const { total: totalBudget } = useBudgetByCategory(SubtypeAlternativeOptions.All)
+  const formatFundingValue = (value: number) => intl.formatNumber(value, CURRENCY_FORMAT_OPTIONS as any)
   const endingThisWeekProjects = projects.filter((item) => {
     const finishAt = Time(item.contract?.finish_at)
     return finishAt.isAfter(Time()) && finishAt.isBefore(Time().add(1, 'week'))
   })
+
+  // TODO: Also filter by current Q
+  const currentProjects = useMemo(
+    () =>
+      projects.filter(
+        ({ status }) =>
+          status === ProjectStatus.InProgress || status === ProjectStatus.Paused || status === ProjectStatus.Pending
+      ),
+    [projects]
+  )
+  const currentBidProjects = useMemo(
+    () => currentProjects.filter((item) => item.type === ProposalType.Bid),
+    [currentProjects]
+  )
+  const currentGrantProjects = useMemo(
+    () => currentProjects.filter((item) => item.type === ProposalType.Grant),
+    [currentProjects]
+  )
+  const totalBidFunding = useMemo(
+    () => currentBidProjects.reduce((total, obj) => total + obj.size, 0),
+    [currentBidProjects]
+  )
+  const totalGrantFunding = useMemo(
+    () => currentGrantProjects.reduce((total, obj) => total + obj.size, 0) || 0,
+    [currentGrantProjects]
+  )
+  const formattedTotalBidFunding = formatFundingValue(totalBidFunding)
+  const formattedTotalGrantFunding = formatFundingValue(totalGrantFunding)
 
   return (
     <div className="Stats">
       <MetricsCard
         variant="dark"
         category="ONGOING"
-        title={`${
-          projects.filter(
-            ({ status }) =>
-              status === ProjectStatus.InProgress || status === ProjectStatus.Paused || status === ProjectStatus.Pending
-          ).length
-        } projects`}
+        title={`${currentProjects.length} projects`}
         description={`${endingThisWeekProjects.length} ending this week`}
       />
       <MetricsCard
         variant="dark"
         category="Project funding for Q3"
-        title={`${intl.formatNumber(totalBudget, CURRENCY_FORMAT_OPTIONS as any)}`}
-        description="Grants: $1,235,678; B&T: $126,234"
+        title={`${formatFundingValue(totalBidFunding + totalGrantFunding)}`}
+        description={`Grants: ${formattedTotalGrantFunding}; B&T: ${formattedTotalBidFunding}`}
       />
       <MetricsCard
         variant="dark"
