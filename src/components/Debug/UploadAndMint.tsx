22e3c34f68890ef62f7067298ebbb2d9e6a789db
1emu <github@lemu.sh>
2023-08-25 12:40:57 -0300

    chore: add date picker for expiration date

--- src/components/Debug/UploadAndMint.tsx
+++ src/components/Debug/UploadAndMint.tsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Field as DCLField } from 'decentraland-ui/dist/components/Field/Field'
 
 import { Governance } from '../../clients/Governance'
 import useFormatMessage from '../../hooks/useFormatMessage'
+import Time from '../../utils/date/Time'
 import Field from '../Common/Form/Field'
 import Heading from '../Common/Typography/Heading'
 import Label from '../Common/Typography/Label'
 import Text from '../Common/Typography/Text'
 import ErrorMessage from '../Error/ErrorMessage'
 import { ContentSection } from '../Layout/ContentLayout'
+
+import './UploadAndMint.css'
 
 interface Props {
   className?: string
@@ -33,7 +37,7 @@ const initialState: SpecState = {
 export default function UploadAndMint({ className }: Props) {
   const t = useFormatMessage()
   const [formDisabled, setFormDisabled] = useState(false)
-  const [error, setError] = useState('')
+  const [submitError, setSubmitError] = useState('')
   const [badgeCid, setBadgeCid] = useState<string | undefined>()
   const [result, setResult] = useState<string | null>()
 
@@ -42,12 +46,15 @@ export default function UploadAndMint({ className }: Props) {
     formState: { isSubmitting, errors },
     control,
     watch,
+    setError,
+    clearErrors,
+    setValue,
   } = useForm<SpecState>({ defaultValues: initialState, mode: 'onTouched' })
 
   const onSubmit: SubmitHandler<SpecState> = async (data) => {
     setFormDisabled(true)
     setResult(null)
-    setError('')
+    setSubmitError('')
     console.log('submitting data', JSON.stringify(data))
     try {
       const result: any = await Governance.get().uploadAndMint(data)
@@ -58,7 +65,7 @@ export default function UploadAndMint({ className }: Props) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
       console.error(err, { ...err })
-      setError(err.body?.error || err.message)
+      setSubmitError(err.body?.error || err.message)
       setFormDisabled(false)
     }
   }
@@ -92,14 +99,20 @@ export default function UploadAndMint({ className }: Props) {
             }}
           />
           <Label>{'Expiration Date'}</Label>
-          <Field
+          <DCLField
+            className="UploadAndMint__ExpirationDate"
             name="expiresAt"
             control={control}
+            type="date"
             error={!!errors.expiresAt}
+            message={errors.expiresAt?.message || ''}
             disabled={formDisabled}
-            message={errors.expiresAt?.message}
-            rules={{
-              required: { value: true, message: 'Expiration date name cannot be empty' },
+            onChange={(_, { value }) => {
+              clearErrors('expiresAt')
+              if (Time(value).isBefore(Time())) {
+                setError('expiresAt', { message: 'Expiration date needs to be in the future' })
+              }
+              setValue('expiresAt', value)
             }}
           />
           <Label>{'Image Url'}</Label>
@@ -124,9 +137,9 @@ export default function UploadAndMint({ className }: Props) {
             <Text className="Debug__Result">{watch('title')}</Text>
           </>
         )}
-        {error && (
+        {submitError && (
           <ContentSection>
-            <ErrorMessage label={t('page.debug.error_label')} errorMessage={t(error) || error} />
+            <ErrorMessage label={t('page.debug.error_label')} errorMessage={t(submitError) || submitError} />
           </ContentSection>
         )}
       </form>
