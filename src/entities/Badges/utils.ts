a026097a79225add19b8b036a77fbb3122f24e20
1emu <github@lemu.sh>
2023-08-25 17:35:50 -0300

    chore: update jest config and resolver to work with nft storage

--- src/entities/Badges/utils.ts
+++ src/entities/Badges/utils.ts
@@ -3,6 +3,7 @@ import { abi as BadgesAbi } from '@otterspace-xyz/contracts/out/Badges.sol/Badge
 import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { ApiResponse } from 'decentraland-gatsby/dist/utils/api/types'
 import { ethers } from 'ethers'
+import { NFTStorage } from 'nft.storage'
 
 import { ErrorService } from '../../services/ErrorService'
 import RpcService from '../../services/RpcService'
@@ -13,6 +14,9 @@ import { GAS_MULTIPLIER, GasConfig } from './types'
 
 const RAFT_OWNER_PK = process.env.RAFT_OWNER_PK || ''
 const POLYGON_BADGES_CONTRACT_ADDRESS = process.env.POLYGON_BADGES_CONTRACT_ADDRESS || ''
+const POLYGON_RAFTS_CONTRACT_ADDRESS = process.env.POLYGON_RAFTS_CONTRACT_ADDRESS || ''
+const NFT_STORAGE_API_KEY = process.env.NFT_STORAGE_API_KEY || ''
+const TRIMMED_OTTERSPACE_RAFT_ID = trimOtterspaceId(OTTERSPACE_DAO_RAFT_ID)
 
 function checksumAddresses(addresses: string[]): string[] {
   return addresses.map((address) => ethers.utils.getAddress(address))
@@ -56,13 +60,11 @@ export async function reinstateBadge(badgeId: string) {
   const provider = RpcService.getPolygonProvider()
   const raftOwner = new ethers.Wallet(RAFT_OWNER_PK, provider)
   const contract = new ethers.Contract(POLYGON_BADGES_CONTRACT_ADDRESS, BadgesAbi, raftOwner)
-  const trimmedOtterspaceId = trimOtterspaceId(OTTERSPACE_DAO_RAFT_ID)
-
   const gasConfig = await estimateGas(contract, async () => {
-    return contract.estimateGas.reinstateBadge(trimmedOtterspaceId, badgeId)
+    return contract.estimateGas.reinstateBadge(TRIMMED_OTTERSPACE_RAFT_ID, badgeId)
   })
 
-  const txn = await contract.connect(raftOwner).reinstateBadge(trimmedOtterspaceId, badgeId, gasConfig)
+  const txn = await contract.connect(raftOwner).reinstateBadge(TRIMMED_OTTERSPACE_RAFT_ID, badgeId, gasConfig)
   await txn.wait()
   return `Reinstated badge with txn hash: ${txn.hash}`
 }
@@ -71,13 +73,12 @@ export async function revokeBadge(badgeId: string, reason: number) {
   const provider = RpcService.getPolygonProvider()
   const raftOwner = new ethers.Wallet(RAFT_OWNER_PK, provider)
   const contract = new ethers.Contract(POLYGON_BADGES_CONTRACT_ADDRESS, BadgesAbi, raftOwner)
-  const trimmedOtterspaceId = trimOtterspaceId(OTTERSPACE_DAO_RAFT_ID)
 
   const gasConfig = await estimateGas(contract, async () => {
-    return contract.estimateGas.revokeBadge(trimmedOtterspaceId, badgeId, reason)
+    return contract.estimateGas.revokeBadge(TRIMMED_OTTERSPACE_RAFT_ID, badgeId, reason)
   })
 
-  const txn = await contract.connect(raftOwner).revokeBadge(trimmedOtterspaceId, badgeId, reason, gasConfig)
+  const txn = await contract.connect(raftOwner).revokeBadge(TRIMMED_OTTERSPACE_RAFT_ID, badgeId, reason, gasConfig)
   await txn.wait()
   return `Revoked badge with txn hash: ${txn.hash}`
 }
@@ -111,4 +112,63 @@ export async function getLandOwnerAddresses(): Promise<string[]> {
     ErrorService.report("Couldn't fetch land owners", { error, category: ErrorCategory.Badges })
     return []
   }
+}
+
+const blobToFile = (theBlob: Blob, fileName: string): File => {
+  const b: any = theBlob
+  b.lastModifiedDate = new Date()
+  b.name = fileName
+  return b as File
+}
+
+function getImageInfoFromUrl(url: string): { imageName: string; extension: string } {
+  const urlParts = url.split('/')
+  const rawFileName = urlParts[urlParts.length - 1].split('?')[0]
+  const [imageName, extension] = rawFileName.split('.')
+  return { imageName, extension }
+}
+
+const fetchImageAndCreateFile = async (imgUrl: string) => {
+  console.log('imgUrl', imgUrl)
+  const { imageName, extension } = getImageInfoFromUrl(imgUrl)
+
+  try {
+    const response = await fetch(imgUrl)
+    if (!response.ok) {
+      throw new Error('Failed to fetch image')
+    }
+    const imageBlob = await response.blob()
+    const file = blobToFile(imageBlob, `${imageName}.${extension}`)
+    return file
+  } catch (error) {
+    console.error('Error fetching image:', error)
+    throw error
+  }
+}
+
+export async function storeBadgeSpec(title: string, description: string, imgUrl: string, expiresAt?: string) {
+  const client = new NFTStorage({ token: NFT_STORAGE_API_KEY })
+  const raftOwner = new ethers.Wallet(RAFT_OWNER_PK)
+  const file = await fetchImageAndCreateFile(imgUrl)
+
+  const badgeSpec = {
+    schema: 'https://api.otterspace.xyz/schemas/badge/1.0.1.json',
+    name: title,
+    description: description,
+    properties: {
+      raftTokenId: TRIMMED_OTTERSPACE_RAFT_ID,
+      raftContractAddress: POLYGON_RAFTS_CONTRACT_ADDRESS,
+      createdByAddress: raftOwner.address,
+      expiresAt: expiresAt ? expiresAt : null,
+    },
+    image: file,
+  }
+
+  const metadata = await client.store(badgeSpec)
+
+  const cid = metadata.ipnft
+
+  const metadataUrl = `https://ipfs.io/ipfs/${cid}/metadata.json`
+  const ipfsAddress = `ipfs://${cid}/metadata.json`
+  return { badgeCid: cid, metadataUrl, ipfsAddress }
 }
