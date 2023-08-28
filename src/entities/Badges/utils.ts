5b24206deba0ac81d1b7ef42b55f3dfd05cdbe1a
1emu <github@lemu.sh>
2023-08-28 14:23:15 -0300

    chore: minor refactor to estimateGas

--- src/entities/Badges/utils.ts
+++ src/entities/Badges/utils.ts
@@ -1,4 +1,3 @@
-import { Contract } from '@ethersproject/contracts'
 import { abi as BadgesAbi } from '@otterspace-xyz/contracts/out/Badges.sol/Badges.json'
 import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { ApiResponse } from 'decentraland-gatsby/dist/utils/api/types'
@@ -22,10 +21,8 @@ function checksumAddresses(addresses: string[]): string[] {
   return addresses.map((address) => ethers.utils.getAddress(address))
 }
 
-export async function estimateGas(
-  contract: Contract,
-  estimateFunction: (...args: any[]) => Promise<any>
-): Promise<GasConfig> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function estimateGas(estimateFunction: (...args: any[]) => Promise<any>): Promise<GasConfig> {
   const provider = RpcService.getPolygonProvider()
   const gasLimit = await estimateFunction()
   const gasPrice = await provider.getGasPrice()
@@ -45,9 +42,7 @@ export async function airdrop(badgeCid: string, recipients: string[], pumpGas = 
   logger.log(`Airdropping, pumping gas ${pumpGas}`)
   let txn
   if (pumpGas) {
-    const gasConfig = await estimateGas(contract, async () =>
-      contract.estimateGas.airdrop(formattedRecipients, ipfsAddress)
-    )
+    const gasConfig = await estimateGas(async () => contract.estimateGas.airdrop(formattedRecipients, ipfsAddress))
     txn = await contract.connect(raftOwner).airdrop(formattedRecipients, ipfsAddress, gasConfig)
   } else {
     txn = await contract.connect(raftOwner).airdrop(formattedRecipients, ipfsAddress)
@@ -60,7 +55,7 @@ export async function reinstateBadge(badgeId: string) {
   const provider = RpcService.getPolygonProvider()
   const raftOwner = new ethers.Wallet(RAFT_OWNER_PK, provider)
   const contract = new ethers.Contract(POLYGON_BADGES_CONTRACT_ADDRESS, BadgesAbi, raftOwner)
-  const gasConfig = await estimateGas(contract, async () => {
+  const gasConfig = await estimateGas(async () => {
     return contract.estimateGas.reinstateBadge(TRIMMED_OTTERSPACE_RAFT_ID, badgeId)
   })
 
@@ -74,7 +69,7 @@ export async function revokeBadge(badgeId: string, reason: number) {
   const raftOwner = new ethers.Wallet(RAFT_OWNER_PK, provider)
   const contract = new ethers.Contract(POLYGON_BADGES_CONTRACT_ADDRESS, BadgesAbi, raftOwner)
 
-  const gasConfig = await estimateGas(contract, async () => {
+  const gasConfig = await estimateGas(async () => {
     return contract.estimateGas.revokeBadge(TRIMMED_OTTERSPACE_RAFT_ID, badgeId, reason)
   })
 
@@ -85,7 +80,7 @@ export async function revokeBadge(badgeId: string, reason: number) {
 
 export async function checkBalance() {
   const provider = RpcService.getPolygonProvider()
-  const raftOwner = await new ethers.Wallet(RAFT_OWNER_PK, provider)
+  const raftOwner = new ethers.Wallet(RAFT_OWNER_PK, provider)
   const balance = await raftOwner.getBalance()
   const balanceInEther = ethers.utils.formatEther(balance)
   const balanceBigNumber = ethers.BigNumber.from(balance)
