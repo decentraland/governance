b94b06167592a64c4954470407597a77da9ba95c
1emu <github@lemu.sh>
2023-08-31 16:13:43 -0300

    refactor: remove unused grant category conversion

--- src/entities/Grant/utils.ts
+++ src/entities/Grant/utils.ts
@@ -1,23 +1,9 @@
 import toSnakeCase from 'lodash/snakeCase'
 
-import {
-  GRANT_PROPOSAL_MAX_BUDGET,
-  GRANT_PROPOSAL_MIN_BUDGET,
-  NewGrantCategory,
-  OldGrantCategory,
-  ProjectStatus,
-  ProposalGrantCategory,
-} from './types'
+import { GRANT_PROPOSAL_MAX_BUDGET, GRANT_PROPOSAL_MIN_BUDGET, ProjectStatus } from './types'
 
 export const isValidGrantBudget = (size: number) => {
   return !(size < GRANT_PROPOSAL_MIN_BUDGET || size > GRANT_PROPOSAL_MAX_BUDGET)
-}
-
-export function toProposalGrantCategory(category?: string | null): ProposalGrantCategory | null {
-  const categories = [...Object.values(NewGrantCategory), ...Object.values(OldGrantCategory)]
-  const index = categories.map(toSnakeCase).indexOf(toSnakeCase(category || undefined))
-
-  return index !== -1 ? categories[index] : null
 }
 
 export function toProjectStatus(status?: string | null): ProjectStatus | undefined {
