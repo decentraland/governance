6d77777e90cbf03295c2e1e556c182198f955a11
Andy Espagnolo <andyespagnolo@gmail.com>
2023-09-14 16:36:07 -0300

    chore: add stories for more common components

---
+++ src/stories/SkeletonBars.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import SkeletonBarsComponent from '../components/Common/SkeletonBars'
+
+const meta: Meta<typeof SkeletonBarsComponent> = {
+  title: 'Common/SkeletonBars',
+  component: SkeletonBarsComponent,
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof SkeletonBarsComponent>
+
+const Container = (props: any) => <div style={{ width: '250px' }} {...props} />
+
+export const SkeletonBars: Story = {
+  args: {
+    amount: 3,
+    height: 30,
+    enableAnimation: true,
+  },
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+}
