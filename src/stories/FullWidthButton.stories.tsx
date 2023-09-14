1100d2c4f01c0060eb64f58fe01288a35135e186
Andy Espagnolo <andyespagnolo@gmail.com>
2023-09-14 17:50:13 -0300

    chore: add stories for more common components (#1261)

---
+++ src/stories/FullWidthButton.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import FullWidthButtonComponent from '../components/Common/FullWidthButton'
+
+const meta: Meta<typeof FullWidthButtonComponent> = {
+  title: 'Common/FullWidthButton',
+  component: FullWidthButtonComponent,
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof FullWidthButtonComponent>
+
+const Container = (props: any) => <div style={{ width: '85vw', maxWidth: '768px' }} {...props} />
+
+export const FullWidthButton: Story = {
+  args: {
+    children: 'View all proposals',
+  },
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+}
