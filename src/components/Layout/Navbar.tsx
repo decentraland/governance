ac67425f3e69181e13f2e2e58c8046c9e63b1679
Florencia Barreto <florbarrlad@gmail.com>
2023-09-18 15:26:12 -0300

    feat: remove unused code

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 import { useLocation } from '@reach/router'
 import UserInformation, { UserInformationProps } from 'decentraland-gatsby/dist/components/User/UserInformation'
-import UserMenu from 'decentraland-gatsby/dist/components/User/UserMenu'
-import useFeatureFlagContext from 'decentraland-gatsby/dist/context/FeatureFlag/useFeatureFlagContext'
 import { Mobile, NotMobile } from 'decentraland-ui/dist/components/Media/Media'
-
-import { FeatureFlags } from '../../utils/features'
 
 import BurgerMenu from './BurgerMenu/BurgerMenu'
 
@@ -16,7 +12,6 @@ function Navbar(props: UserInformationProps) {
   const location = useLocation()
   const showBurgerMenu = BURGER_MENU_LOCATIONS.some((burgerLocation) => burgerLocation === location.pathname)
 
-  const [ff] = useFeatureFlagContext()
   return (
     <>
       {showBurgerMenu && (
