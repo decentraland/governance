ce2cbaa936b2ea899b1833a37dbcdd1b9aa2befd
Florencia Barreto <32873485+flobarreto@users.noreply.github.com>
2023-09-19 12:00:56 -0300

    Feat/add new user menu (#1266)
    * feat: added new user menu (#1256)
    
    * feat: added new user menu
    
    * replaced colors with chalk
    
    * use chalk for colors
    
    * feat: updated gatsby
    
    * feat: install chalk and isomorphic fetch
    
    * feat: added user menu
    
    * feat: removed FF
    
    * feat: remove unused code
    
    * fix: projects page navigation bar width
    
    ---------
    
    Co-authored-by: Andy Espagnolo <andyespagnolo@gmail.com>

--- src/entities/Debug/isDebugAddress.ts
+++ src/entities/Debug/isDebugAddress.ts
@@ -1,10 +1,10 @@
-import { magenta } from 'colors/safe'
+import chalk from 'chalk'
 
 import { DEBUG_ADDRESSES } from '../../constants'
 
 const debugAddresses = new Set(DEBUG_ADDRESSES)
 
-debugAddresses.forEach((address) => console.log('debug address:', magenta(address)))
+debugAddresses.forEach((address) => console.log('debug address:', chalk.magenta(address)))
 
 export default function isDebugAddress(address: string | undefined) {
   return address && address.length > 0 && debugAddresses.has(address.toLowerCase())
