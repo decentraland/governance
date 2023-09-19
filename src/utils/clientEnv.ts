28de034423da1d9f2be5846846554ed854145fa1
lemu <github@lemu.sh>
2023-09-19 16:27:11 -0300

    fix: use process.env for DEBUG_ADDRESSES, and rename env to clientEnv (#1273)

--- src/utils/clientEnv.ts
+++ src/utils/clientEnv.ts
@@ -1,5 +1,5 @@
 import { config } from '../config'
 
-export const env = (variable: string, defaultValue?: string) => {
+export const clientEnv = (variable: string, defaultValue?: string) => {
   return config.get(variable, defaultValue)
 }
