68419d5e3c05f2f06ae5658581dfd884f9e60ffc
1emu <github@lemu.sh>
2023-09-22 12:17:58 -0300

    feat: grants hero

--- src/components/Proposal/ProposalHero.tsx
+++ src/components/Proposal/ProposalHero.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { Header } from 'decentraland-ui/dist/components/Header/Header'
+import classNames from 'classnames'
 import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
 
 import { ProposalAttributes } from '../../entities/Proposal/types'
 import CategoryPill from '../Category/CategoryPill'
-import Text from '../Common/Typography/Text'
+import { PillColor } from '../Common/Pill'
 import StatusPill from '../Status/StatusPill'
 
 import './ProposalHero.css'
@@ -17,14 +17,14 @@ interface Props {
 export default function ProposalHero({ proposal }: Props) {
   return (
     <div className="ProposalHero__Container">
-      <div className="ProposalHero__Banner ProposalHero__UpperLayer"></div>
-      <div className="ProposalHero__Banner ProposalHero__BottomLayer" />
+      <div className={classNames('ProposalHero__Banner', 'ProposalHero__UpperLayer')}></div>
+      <div className={classNames('ProposalHero__Banner', 'ProposalHero__BottomLayer')} />
       <div className="ProposalHero__Text">
         <p className="ProposalHero__Title">{proposal?.title || ''}</p>
         <Loader active={!proposal} />
         <div className="ProposalDetailPage__Labels">
-          {proposal && <StatusPill isLink status={proposal.status} />}
-          {proposal && <CategoryPill isLink proposalType={proposal.type} />}
+          {proposal && <StatusPill isLink status={proposal.status} color={PillColor.White} />}
+          {proposal && <CategoryPill isLink proposalType={proposal.type} transparent />}
         </div>
       </div>
     </div>
