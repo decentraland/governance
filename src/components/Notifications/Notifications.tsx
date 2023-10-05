8c076bfe712675387a86459743ced02865ed2abe
Andy Espagnolo <andyespagnolo@gmail.com>
2023-10-05 14:29:00 -0300

    feat: add notification dot ui

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -12,16 +12,24 @@ import NotificationsFeed from './NotificationsFeed'
 export default function Notifications() {
   const t = useFormatMessage()
   const [isOpen, setOpen] = useState(false)
+  const hasNewNotifications = false // TODO: Integrate this
 
   return (
     <>
-      <button
-        className={classNames('Notifications__Button', isOpen && 'Notifications__Button--active')}
-        onClick={() => setOpen((prev) => !prev)}
-        aria-label={t('navigation.notifications.button_label')}
-      >
-        {isOpen ? <NotificationBellActive /> : <NotificationBellInactive />}
-      </button>
+      <div>
+        <button
+          className={classNames('Notifications__Button', isOpen && 'Notifications__Button--active')}
+          onClick={() => setOpen((prev) => !prev)}
+          aria-label={t('navigation.notifications.button_label')}
+        >
+          {isOpen ? <NotificationBellActive /> : <NotificationBellInactive />}
+          {hasNewNotifications && !isOpen && (
+            <div className="Notifications__DotOuterCircle">
+              <div className="Notifications__DotInnerCircle" />
+            </div>
+          )}
+        </button>
+      </div>
       <NotificationsFeed isOpen={isOpen} onClose={() => setOpen(false)} />
     </>
   )
