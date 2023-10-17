125c961eb414240457406b803c11fb6268ee76b2
Andy Espagnolo <andyespagnolo@gmail.com>
2023-10-17 14:14:05 -0300

    fix: search input mobile

--- src/components/Search/SearchInputMobile.tsx
+++ src/components/Search/SearchInputMobile.tsx
@@ -1,11 +1,10 @@
-import { useLayoutEffect, useRef, useState } from 'react'
+import { useCallback, useLayoutEffect, useRef, useState } from 'react'
 
 import { useLocation } from '@reach/router'
 import classNames from 'classnames'
 
 import useFormatMessage from '../../hooks/useFormatMessage'
 import { useProposalsSearchParams } from '../../hooks/useProposalsSearchParams'
-import useSnapshotStatus from '../../hooks/useSnapshotStatus'
 import locations, { navigate } from '../../utils/locations'
 import Cross from '../Icon/Cross'
 
@@ -18,7 +17,6 @@ export default function SearchInputMobile() {
   const searchInput = useRef<HTMLInputElement>(null)
   const [open, setOpen] = useState(false)
   const [searchText, setSearchText] = useState(search || '')
-  const showSnapshotStatus = useSnapshotStatus()
 
   const handleSearch = (textSearch: string, location: Location) => {
     const newParams = new URLSearchParams(location.search)
@@ -31,7 +29,7 @@ export default function SearchInputMobile() {
       newParams.delete('page')
     }
 
-    setOpen(false)
+    handleClose()
     searchInput.current?.blur()
     navigate(locations.proposals(newParams))
   }
@@ -42,7 +40,7 @@ export default function SearchInputMobile() {
 
   const handleClear = () => {
     if (searchText === '') {
-      setOpen(false)
+      handleClose()
     } else {
       setSearchText('')
       searchInput.current?.focus()
@@ -62,45 +60,51 @@ export default function SearchInputMobile() {
     }
   }
 
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = useCallback(() => {
+    setOpen(false)
+  }, [])
+
   useLayoutEffect(() => {
     if (typeof window !== 'undefined') {
       const onScroll = function () {
         searchInput.current?.blur()
-        setOpen(false)
+        handleClose()
       }
 
       window.addEventListener('scroll', onScroll)
 
       return () => window.removeEventListener('scroll', onScroll)
     }
-  }, [])
+  }, [handleClose])
 
   return (
-    <div className={classNames('SearchInputMobile', open && 'SearchInputMobile--open')}>
-      <div className="SearchInputMobile__Container">
-        {!open && <div className="SearchInputMobile__Gradient" />}
-        <input
-          ref={searchInput}
-          className={classNames('SearchInputMobile__Input', open && 'SearchInputMobile__Input--open')}
-          value={searchText}
-          placeholder={t('navigation.search.placeholder') || ''}
-          onChange={handleChange}
-          onKeyPress={handleKeyPress}
-          onKeyUp={handleKeyUp}
-          onFocus={() => setOpen(true)}
-        />
-        {open && (
-          <Cross className="SearchInputMobile__CloseIcon" size="14" color="var(--black-800)" onClick={handleClear} />
-        )}
-      </div>
+    <>
       <div
-        onClick={() => setOpen(false)}
-        className={classNames(
-          'SearchInputMobile__Overlay',
-          open && 'SearchInputMobile__Overlay--open',
-          showSnapshotStatus && 'SearchInputMobile__Overlay--with-snapshot-status'
-        )}
+        className={classNames('SearchInputMobile__Overlay', open && 'SearchInputMobile__Overlay--open')}
+        onClick={handleClose}
       />
-    </div>
+      <div className={classNames('SearchInputMobile', open && 'SearchInputMobile--open')}>
+        <div className="SearchInputMobile__Container">
+          {!open && <div className="SearchInputMobile__Gradient" />}
+          <input
+            ref={searchInput}
+            className={classNames('SearchInputMobile__Input', open && 'SearchInputMobile__Input--open')}
+            value={searchText}
+            placeholder={t('navigation.search.placeholder') || ''}
+            onChange={handleChange}
+            onKeyPress={handleKeyPress}
+            onKeyUp={handleKeyUp}
+            onFocus={handleOpen}
+          />
+          {open && (
+            <Cross className="SearchInputMobile__CloseIcon" size="14" color="var(--black-800)" onClick={handleClear} />
+          )}
+        </div>
+      </div>
+    </>
   )
 }
