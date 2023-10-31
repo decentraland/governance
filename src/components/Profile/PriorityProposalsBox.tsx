fa6f3b2bdf710cd956ad253cfd1255f6bc97aeb8
1emu <github@lemu.sh>
2023-10-31 16:06:11 -0300

    feat: upcoming opportunities section WIP

--- src/components/Profile/PriorityProposalsBox.tsx
+++ src/components/Profile/PriorityProposalsBox.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 
 import { ProposalAttributes } from '../../entities/Proposal/types'
+import useFormatMessage from '../../hooks/useFormatMessage'
 import usePriorityProposals from '../../hooks/usePriorityProposals'
+import locations from '../../utils/locations'
 import FullWidthButton from '../Common/FullWidthButton'
 import ProposalPreviewCard from '../Common/ProposalPreviewCard/ProposalPreviewCard'
 
@@ -9,16 +11,28 @@ import { ActionBox } from './ActionBox'
 import './PriorityProposalsBox.css'
 
 interface Props {
-  title: string
-  info?: string
   address: string
   collapsible?: boolean
 }
 
 const PROPOSALS_PER_PAGE = 5
 
-//TODO: extract common behavior with voted proposals box
-function PriorityProposalsBox({ title, info, address, collapsible }: Props) {
+function renderPriorityProposals(
+  priorityProposals: any[] | Partial<ProposalAttributes>[] | undefined,
+  displayedProposals: number
+) {
+  return (
+    <>
+      {priorityProposals &&
+        priorityProposals.slice(0, displayedProposals).map((proposal) => {
+          return <ProposalPreviewCard key={proposal.id} proposal={proposal as ProposalAttributes} variant="slim" />
+        })}
+    </>
+  )
+}
+
+function PriorityProposalsBox({ address, collapsible = false }: Props) {
+  const t = useFormatMessage()
   const { priorityProposals, isLoading } = usePriorityProposals(address)
   const [displayedProposals, setDisplayedProposals] = useState(PROPOSALS_PER_PAGE)
   const hasMoreProposals = priorityProposals && priorityProposals.length > PROPOSALS_PER_PAGE
@@ -33,17 +47,28 @@ function PriorityProposalsBox({ title, info, address, collapsible }: Props) {
     if (priorityProposals) setDisplayedProposals(PROPOSALS_PER_PAGE)
   }
 
+  // TODO: internationalization
   return isLoading || (!isLoading && priorityProposals && priorityProposals.length === 0) ? null : (
-    <ActionBox title={title} info={info} collapsible={collapsible}>
-      {priorityProposals &&
-        priorityProposals.slice(0, displayedProposals).map((proposal) => {
-          return <ProposalPreviewCard key={proposal.id} proposal={proposal as ProposalAttributes} variant="slim" />
-        })}
-      {showViewMoreButton && (
-        <FullWidthButton onClick={handleViewMore}>{`Show all ${priorityProposals?.length} proposals`}</FullWidthButton>
+    <>
+      {collapsible ? (
+        <ActionBox title={'Time Sensitive'} info={'Proposals that need your attention reit neu'} collapsible>
+          {renderPriorityProposals(priorityProposals, displayedProposals)}
+          {showViewMoreButton && (
+            <FullWidthButton
+              onClick={handleViewMore}
+            >{`Show all ${priorityProposals?.length} proposals`}</FullWidthButton>
+          )}
+          {showViewLessButton && <FullWidthButton onClick={handleViewLess}>{`Show less`}</FullWidthButton>}
+        </ActionBox>
+      ) : (
+        <>
+          {renderPriorityProposals(priorityProposals, displayedProposals)}
+          <FullWidthButton link={locations.proposals()}>
+            {t('page.home.open_proposals.view_all_proposals')}
+          </FullWidthButton>
+        </>
       )}
-      {showViewLessButton && <FullWidthButton onClick={handleViewLess}>{`Show less`}</FullWidthButton>}
-    </ActionBox>
+    </>
   )
 }
 
