d82d31509cf806128968996b6dd420b88b7109d9
ncomerci <ncomerci@itba.edu.ar>
2023-11-14 16:18:39 -0300

    feat: admin & debug tabs

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react'
+
 import useAuthContext from 'decentraland-gatsby/dist/context/Auth/useAuthContext'
 
+import BoxTabs from '../components/Common/BoxTabs'
 import Heading from '../components/Common/Typography/Heading'
 import Text from '../components/Common/Typography/Text'
 import WiderContainer from '../components/Common/WiderContainer'
@@ -17,9 +20,17 @@ import useIsDebugAddress from '../hooks/useIsDebugAddress'
 
 import './debug.css'
 
+enum Panel {
+  Admin = 'Admin',
+  Debug = 'Debug',
+}
+
 export default function DebugPage() {
   const [account] = useAuthContext()
   const { isDebugAddress } = useIsDebugAddress(account)
+  const [activePanel, setActivePanel] = useState(Panel.Admin)
+  const isAdminActive = activePanel === Panel.Admin
+  const isDebugActive = activePanel === Panel.Debug
 
   if (!account || !isDebugAddress) {
     return <LogIn title={'test'} description={'test'} />
@@ -31,14 +42,34 @@ export default function DebugPage() {
       <WiderContainer className="DebugPage">
         <Heading size="sm">{'Version'}</Heading>
         <Text>{process.env.GATSBY_VERSION_NUMBER}</Text>
-        <BudgetsUpdate className="DebugPage__Section" />
-        <HttpStatus className="DebugPage__Section" />
-        <BadgesAdmin className="DebugPage__Section" />
-        <Snapshot className="DebugPage__Section" />
-        <EnvStatus className="DebugPage__Section" />
-        <TriggerFunction className="DebugPage__Section" />
-        <ErrorReporting className="DebugPage__Section" />
-        <Notifications className="DebugPage__Section" />
+        <div className="DebugTabs">
+          <BoxTabs>
+            <BoxTabs.Left>
+              <BoxTabs.Tab active={isAdminActive} onClick={() => setActivePanel(Panel.Admin)}>
+                {Panel.Admin}
+              </BoxTabs.Tab>
+              <BoxTabs.Tab active={isDebugActive} onClick={() => setActivePanel(Panel.Debug)}>
+                {Panel.Debug}
+              </BoxTabs.Tab>
+            </BoxTabs.Left>
+          </BoxTabs>
+        </div>
+        {isAdminActive && (
+          <>
+            <BudgetsUpdate className="DebugPage__Section" />
+            <BadgesAdmin className="DebugPage__Section" />
+            <TriggerFunction className="DebugPage__Section" />
+            <ErrorReporting className="DebugPage__Section" />
+            <Notifications className="DebugPage__Section" />
+          </>
+        )}
+        {isDebugActive && (
+          <>
+            <HttpStatus className="DebugPage__Section" />
+            <Snapshot className="DebugPage__Section" />
+            <EnvStatus className="DebugPage__Section" />
+          </>
+        )}
       </WiderContainer>
     </>
   )
