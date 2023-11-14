44bbe32b1c722fb9a0155316fa7111d564cc3690
Andy Espagnolo <andyespagnolo@gmail.com>
2023-11-14 20:49:45 -0300

    feat: improve date in mobile

--- src/components/Proposal/View/ProjectCard.tsx
+++ src/components/Proposal/View/ProjectCard.tsx
@@ -1,5 +1,7 @@
 import { useIntl } from 'react-intl'
 
+import { useMobileMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
+
 import { ProposalAttributes, VestingContractData } from '../../../entities/Proposal/types'
 import { getBudget } from '../../../entities/Proposal/utils'
 import { CURRENCY_FORMAT_OPTIONS } from '../../../helpers'
@@ -14,14 +16,23 @@ interface Props {
   proposal: ProposalAttributes & { contract?: VestingContractData }
 }
 
+function formatDate(date: Time.Dayjs, isMobile: boolean) {
+  return isMobile ? date.format('MM/DD/YY') : date.fromNow()
+}
+
 export default function ProjectCard({ proposal }: Props) {
   const t = useFormatMessage()
   const { id, title, contract, configuration } = proposal
+  const isMobile = useMobileMediaQuery()
   const finishAt = Time.unix(contract?.finish_at || 0)
   const startAt = Time.unix(contract?.start_at || 0)
   const dateText = finishAt.isBefore(Time())
-    ? t('page.proposal_detail.author_details.sidebar.project_ended', { date: finishAt.fromNow() })
-    : t('page.proposal_detail.author_details.sidebar.project_began', { date: startAt.fromNow() })
+    ? t('page.proposal_detail.author_details.sidebar.project_ended', {
+        date: formatDate(finishAt, isMobile),
+      })
+    : t('page.proposal_detail.author_details.sidebar.project_began', {
+        date: formatDate(startAt, isMobile),
+      })
   const { formatNumber } = useIntl()
   const budget = getBudget(proposal)
 
@@ -29,11 +40,11 @@ export default function ProjectCard({ proposal }: Props) {
     <ProposalCardContainer id={id} title={title}>
       <div className="ProjectCard__Details">
         <ProjectPill type={configuration.category} style="light" />
-        <span className="ProjectCard__BudgetAndDate">
+        <div className="ProjectCard__BudgetAndDate">
           <span>{formatNumber(budget || 0, CURRENCY_FORMAT_OPTIONS)}</span>
           <span>{' · '}</span>
           <span>{dateText}</span>
-        </span>
+        </div>
       </div>
     </ProposalCardContainer>
   )
