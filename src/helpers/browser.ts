9f99e518901ff6d65f4885b87018c80197e01ba4
1emu <github@lemu.sh>
2023-11-21 17:36:51 -0300

    feat: scroll to bidding and tendering section on proposals with submissions

---
+++ src/helpers/browser.ts
@@ -0,0 +1,12 @@
+export function scrollToAnchor(anchor: string, pixelsOverAnchor = 0) {
+  if (typeof window === 'undefined') return
+  const element = document.getElementById(anchor)
+  if (element) {
+    const bounding = element.getBoundingClientRect()
+    const position = bounding.top + window.scrollY - pixelsOverAnchor
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth',
+    })
+  }
+}
