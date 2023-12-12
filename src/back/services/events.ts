3ac9b838b218246f98de58a49212ed22082a83f8
1emu <github@lemu.sh>
2023-12-12 13:15:04 -0300

    chore: report event creation errors

--- src/back/services/events.ts
+++ src/back/services/events.ts
@@ -1,36 +1,33 @@
 import crypto from 'crypto'
 
-import { getNameAndAvatar } from '../../utils/Catalyst'
-import EventModel, { EventType, ProposalCreatedEvent, UpdateCreatedEvent } from '../models/Event'
+import { ErrorService } from '../../services/ErrorService'
+import { ErrorCategory } from '../../utils/errorCategories'
+import EventModel, { EventType, ProposalCreatedEvent, UpdateCreatedEvent, VotedEvent } from '../models/Event'
 
 export class EventsService {
   static async getLatest() {
     return await EventModel.getLatest()
   }
 
-  //TODO: error handling
   static async proposalCreated(proposal_id: string, proposal_title: string, address: string) {
-    const { displayableUser } = await getNameAndAvatar(address)
-
-    const proposalCreatedEvent: ProposalCreatedEvent = {
-      id: crypto.randomUUID(),
-      address,
-      username: displayableUser !== address ? displayableUser : undefined,
-      event_type: EventType.ProposalCreated,
-      event_data: { proposal_id, proposal_title },
-      created_at: new Date(),
+    try {
+      const proposalCreatedEvent: ProposalCreatedEvent = {
+        id: crypto.randomUUID(),
+        address,
+        event_type: EventType.ProposalCreated,
+        event_data: { proposal_id, proposal_title },
+        created_at: new Date(),
+      }
+      await EventModel.create(proposalCreatedEvent)
+    } catch (error) {
+      this.reportEventError(error as Error, EventType.ProposalCreated, { address, proposal_id, proposal_title })
     }
-    await EventModel.create(proposalCreatedEvent)
   }
 
-  //TODO: error handling
   static async updateCreated(update_id: string, proposal_id: string, proposal_title: string, address: string) {
     try {
-      const { displayableUser } = await getNameAndAvatar(address)
-
       const updateCreatedEvent: UpdateCreatedEvent = {
         id: crypto.randomUUID(),
-        username: displayableUser !== address ? displayableUser : undefined,
         address,
         event_type: EventType.UpdateCreated,
         event_data: { update_id, proposal_id, proposal_title },
@@ -38,7 +35,36 @@ export class EventsService {
       }
       await EventModel.create(updateCreatedEvent)
     } catch (error) {
-      console.log('error creating event', error)
+      this.reportEventError(error as Error, EventType.UpdateCreated, {
+        address,
+        update_id,
+        proposal_id,
+        proposal_title,
+      })
     }
+  }
+
+  static async voted(proposal_id: string, proposal_title: string, choice: string, address: string) {
+    try {
+      const votedEvent: VotedEvent = {
+        id: crypto.randomUUID(),
+        address,
+        event_type: EventType.Voted,
+        event_data: { proposal_id, proposal_title, choice },
+        created_at: new Date(),
+      }
+      await EventModel.create(votedEvent)
+    } catch (error) {
+      this.reportEventError(error as Error, EventType.Voted, { address, proposal_id, proposal_title, choice })
+    }
+  }
+
+  private static reportEventError(error: Error, eventType: EventType, args: Record<string, unknown>) {
+    ErrorService.report('Error creating event', {
+      error,
+      event_type: eventType,
+      ...args,
+      category: ErrorCategory.Events,
+    })
   }
 }
