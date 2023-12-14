b40ff71e1a58721718f3a2d1ddabd12f0c3f945b
1emu <github@lemu.sh>
2023-12-14 13:54:24 -0300

    refactor: remove blockie

--- src/components/Common/Username.tsx
+++ src/components/Common/Username.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 import { Address } from 'decentraland-ui/dist/components/Address/Address'
-import { Blockie } from 'decentraland-ui/dist/components/Blockie/Blockie'
 
 import { getChecksumAddress } from '../../entities/Snapshot/utils'
 import useDclProfile from '../../hooks/useDclProfile'
@@ -25,35 +24,8 @@ type Props = {
   strong?: boolean
 }
 
-function getBlockieScale(size?: string) {
-  const DEFAULT_BLOCKIE_SCALE = 3.35
-  switch (size) {
-    case AvatarSize.Mini:
-      return 3
-    case AvatarSize.Tiny:
-      return DEFAULT_BLOCKIE_SCALE
-    case AvatarSize.Small:
-      return 4.9
-    case AvatarSize.Medium:
-      return 7
-    case AvatarSize.Large:
-      return 8.4
-    case AvatarSize.Big:
-      return 10.5
-    case AvatarSize.Huge:
-      return 14.5
-    case AvatarSize.Massive:
-      return 20
-    case AvatarSize.Full:
-      return 42.5
-    default:
-      return DEFAULT_BLOCKIE_SCALE
-  }
-}
-
 const Username = ({ address, size, linked, variant = UsernameVariant.Full, strong = false, className }: Props) => {
-  const { username, avatar, hasCustomAvatar, isLoadingDclProfile } = useDclProfile(address)
-  const blockieScale = getBlockieScale(size)
+  const { username, avatar, isLoadingDclProfile } = useDclProfile(address)
   const isAddressVariant = variant === UsernameVariant.Address
   const isFullVariant = variant === UsernameVariant.Full
   const checksumAddress = address ? getChecksumAddress(address) : ''
@@ -67,20 +39,9 @@ const Username = ({ address, size, linked, variant = UsernameVariant.Full, stron
 
       {!isAddressVariant && (
         <>
-          {hasCustomAvatar && (
-            <>
-              <Avatar size={size} address={address} avatar={avatar} isLoadingDclProfile={isLoadingDclProfile} />
-              {username && isFullVariant && <span className="Username__Name">{username}</span>}
-              {!username && isFullVariant && <Address value={checksumAddress} strong={strong} />}
-            </>
-          )}
-
-          {!hasCustomAvatar && (
-            <>
-              <Blockie scale={blockieScale} seed={checksumAddress} />
-              {isFullVariant && <Address value={checksumAddress} strong={strong} />}
-            </>
-          )}
+          <Avatar size={size} address={address} avatar={avatar} isLoadingDclProfile={isLoadingDclProfile} />
+          {username && isFullVariant && <span className="Username__Name">{username}</span>}
+          {!username && isFullVariant && <Address value={checksumAddress} strong={strong} />}
         </>
       )}
     </Component>
