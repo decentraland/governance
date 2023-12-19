c7a176365ea199870773b18b2f91bdb74245758b
Andy Espagnolo <andyespagnolo@gmail.com>
2023-12-19 16:10:31 -0300

    feat: add link to activity ticker item

--- src/components/Home/ActivityTicker.tsx
+++ src/components/Home/ActivityTicker.tsx
@@ -4,14 +4,31 @@ import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
 import { Governance } from '../../clients/Governance'
 import { ONE_MINUTE_MS } from '../../hooks/constants'
 import useFormatMessage from '../../hooks/useFormatMessage'
+import { EventType, EventWithAuthor } from '../../shared/types/events'
 import Time from '../../utils/date/Time'
+import locations from '../../utils/locations'
 import Avatar from '../Common/Avatar'
 import Empty from '../Common/Empty'
 import Heading from '../Common/Typography/Heading'
+import Link from '../Common/Typography/Link'
 import Markdown from '../Common/Typography/Markdown'
 import Text from '../Common/Typography/Text'
 
 import './ActivityTicker.css'
+
+const getLink = (event: EventWithAuthor) => {
+  if (event.event_type === EventType.ProposalCreated) {
+    return locations.proposal(event.event_data.proposal_id)
+  }
+
+  if (event.event_type === EventType.UpdateCreated) {
+    return locations.update(event.event_data.update_id)
+  }
+
+  if (event.event_type === EventType.Voted) {
+    return locations.proposal(event.event_data.proposal_id)
+  }
+}
 
 export default function ActivityTicker() {
   const t = useFormatMessage()
@@ -46,20 +63,24 @@ export default function ActivityTicker() {
             <div className="ActivityTicker__List">
               {events.map((item) => (
                 <div key={item.id} className="ActivityTicker__ListItem">
-                  <Avatar size="xs" avatar={item.avatar} address={item.address} />
+                  <Link href={locations.profile({ address: item.address })}>
+                    <Avatar size="xs" avatar={item.avatar} address={item.address} />
+                  </Link>
                   <div>
-                    <Markdown
-                      className="ActivityTicker__ListItemMarkdown"
-                      componentsClassNames={{ strong: 'ActivityTicker__ListItemMarkdownTitle' }}
-                    >
-                      {t(`page.home.activity_ticker.${item.event_type}`, {
-                        author: item.author,
-                        title: item.event_data.proposal_title,
-                      })}
-                    </Markdown>
-                    <Text className="ActivityTicker__ListItemDate" size="xs">
-                      {Time(item.created_at).fromNow()}
-                    </Text>
+                    <Link href={getLink(item)}>
+                      <Markdown
+                        className="ActivityTicker__ListItemMarkdown"
+                        componentsClassNames={{ strong: 'ActivityTicker__ListItemMarkdownTitle' }}
+                      >
+                        {t(`page.home.activity_ticker.${item.event_type}`, {
+                          author: item.author,
+                          title: item.event_data.proposal_title,
+                        })}
+                      </Markdown>
+                      <Text className="ActivityTicker__ListItemDate" size="xs">
+                        {Time(item.created_at).fromNow()}
+                      </Text>
+                    </Link>
                   </div>
                 </div>
               ))}
