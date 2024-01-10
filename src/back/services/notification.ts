f5a8af088ec589a75e0d73c97f49343f50a8a973
1emu <github@lemu.sh>
2024-01-10 12:37:59 -0300

    fix: don't send notifications when pk is not present

--- src/back/services/notification.ts
+++ src/back/services/notification.ts
@@ -1,6 +1,7 @@
 import { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
 import logger from 'decentraland-gatsby/dist/entities/Development/logger'
 import { ethers } from 'ethers'
+import isEthereumAddress from 'validator/lib/isEthereumAddress'
 
 import { NOTIFICATIONS_SERVICE_ENABLED, PUSH_CHANNEL_ID } from '../../constants'
 import { ProposalAttributes } from '../../entities/Proposal/types'
@@ -29,8 +30,20 @@ import PushAPI = require('@pushprotocol/restapi')
 const chainId = isProdEnv() ? ChainId.ETHEREUM_MAINNET : ChainId.ETHEREUM_SEPOLIA
 const PUSH_CHANNEL_OWNER_PK = process.env.PUSH_CHANNEL_OWNER_PK
 const PUSH_API_URL = process.env.PUSH_API_URL
-const pkAddress = `0x${PUSH_CHANNEL_OWNER_PK}`
-const signer = NOTIFICATIONS_SERVICE_ENABLED ? new ethers.Wallet(pkAddress) : undefined
+
+function getSigner() {
+  if (NOTIFICATIONS_SERVICE_ENABLED) {
+    if (!PUSH_CHANNEL_OWNER_PK || !isEthereumAddress(PUSH_CHANNEL_OWNER_PK)) {
+      logger.error(
+        'Push channel owner PK is missing. You can either add one or set NOTIFICATIONS_SERVICE_ENABLED=false'
+      )
+      return undefined
+    }
+    return new ethers.Wallet(`0x${PUSH_CHANNEL_OWNER_PK}`)
+  }
+  return undefined
+}
+
 const NotificationIdentityType = {
   DIRECT_PAYLOAD: 2,
 }
@@ -38,13 +51,15 @@ const ADDITIONAL_META_CUSTOM_TYPE = 0
 const ADDITIONAL_META_CUSTOM_TYPE_VERSION = 1
 
 export class NotificationService {
+  static signer = getSigner()
+
   static async sendNotification({ type, title, body, recipient, url, customType }: Notification) {
-    if (!NOTIFICATIONS_SERVICE_ENABLED) {
+    if (!NOTIFICATIONS_SERVICE_ENABLED || !this.signer) {
       return
     }
 
     const response = await PushAPI.payloads.sendNotification({
-      signer,
+      signer: this.signer,
       type: this.getType(type, recipient),
       identityType: NotificationIdentityType.DIRECT_PAYLOAD,
       notification: {
