422b609483c34dfccd983f979830f8973f46244c
1emu <github@lemu.sh>
2024-01-16 16:54:27 -0300

    chore: don't filter comment events

--- src/back/models/Event.ts
+++ src/back/models/Event.ts
@@ -1,20 +1,18 @@
 import { Model } from 'decentraland-gatsby/dist/entities/Database/model'
 import { SQL, table } from 'decentraland-gatsby/dist/entities/Database/utils'
 
-import { Event, EventType } from '../../shared/types/events'
+import { Event } from '../../shared/types/events'
 
 export default class EventModel extends Model<Event> {
   static tableName = 'events'
   static withTimestamps = false
   static primaryKey = 'id'
 
-  // TODO: do we want to show comment events on the activity ticker?
   static async getLatest(): Promise<Event[]> {
     const query = SQL`
       SELECT *
       FROM ${table(EventModel)}
       WHERE created_at >= NOW() - INTERVAL '7 day'
-      AND event_type != ${EventType.Commented}
       ORDER BY created_at DESC
     `
     const result = await this.namedQuery<Event>('get_latest_events', query)
