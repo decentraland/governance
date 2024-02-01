2cc73c28f6d83c35de5f0a6aa6fa28611403dfad
Nicolás Comerci <45410089+ncomerci@users.noreply.github.com>
2024-02-01 10:42:18 -0300

    chore: not found component replaced (#1622)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,11 @@
-import NotFound from 'decentraland-gatsby/dist/components/Layout/NotFound'
-
 import Navigation from '../components/Layout/Navigation'
-import useFormatMessage from '../hooks/useFormatMessage'
+import NotFound from '../components/Layout/NotFound'
 
 export default function NotFoundPage() {
-  const t = useFormatMessage()
-  const title = t('page.404.title')!
-  const description = t('page.404.description')!
-
   return (
     <>
       <Navigation />
-      <NotFound title={title} description={description} />
+      <NotFound />
     </>
   )
 }
