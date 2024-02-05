20939c4f688c842a3e19af505107f7ca8b548124
lemu <github@lemu.sh>
2024-02-05 14:28:23 -0300

    chore: remove old discourse comments endpoint (#1636)

--- src/back/routes/events.ts
+++ src/back/routes/events.ts
@@ -5,14 +5,11 @@ import routes from 'decentraland-gatsby/dist/entities/Route/routes'
 import { EventsService } from '../services/events'
 import { validateDebugAddress, validateProposalId, validateRequiredString } from '../utils/validations'
 
-import { discourseComment } from './webhooks'
-
 export default routes((route) => {
   const withAuth = auth()
   route.get('/events', handleAPI(getLatestEvents))
   route.get('/events/all', withAuth, handleAPI(getAllEvents))
   route.post('/events/voted', withAuth, handleAPI(voted))
-  route.post('/events/discourse/new', handleAPI(discourseComment)) //TODO: deprecate
 })
 
 async function getLatestEvents() {
