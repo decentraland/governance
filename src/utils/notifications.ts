d285d69b5a885ba1947ea619297abcc8b295eb2c
Andy Espagnolo <andyespagnolo@gmail.com>
2024-02-05 14:04:48 -0300

    chore: update notification body text

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -33,5 +33,5 @@ export enum NotificationBody {
   ProposalVotedFinished = 'Discover the results of the proposal you participated in as a voter. Your input matters!',
   ProposalAuthoredFinished = 'The votes are in! Find out the outcome of the voting on your proposal now',
   CoAuthorRequestReceived = "You've been invited to collaborate as a co-author on a published proposal. Accept it or reject it here",
-  ProjectEnacted = 'Congratulations! Your Project has been successfully enacted and a funding Vesting Contract was added',
+  ProjectEnacted = 'Congratulations! Your Project has been successfully enacted and a funding Vesting Contract was created',
 }
