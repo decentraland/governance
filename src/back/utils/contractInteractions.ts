b3f63f24fb39a989cc106ad8a7a7a57611eaad8b
lemu <github@lemu.sh>
2024-02-14 12:40:40 -0300

    chore: airdrops general improvements (#1642)
    * chore: lazy initialization for rollbar client on error service
    
    * chore: improve gas estimations and gas increase mechanism
    
    * chore: airdrop legislator badges only to users without it
    
    * fix: gas config big number multiplication
    
    * chore: add BLOCKNATIVE_API_KEY to example and readme
    
    * chore: improve transactions error identification, avoid resubmitting tx if a successful replacement tx is found
    
    * refactor: address pr comments
    
    * refactor: getIncreasedGasConfig rename

--- src/back/utils/contractInteractions.ts
+++ src/back/utils/contractInteractions.ts
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Contract } from '@ethersproject/contracts'
 import { abi as BadgesAbi } from '@otterspace-xyz/contracts/out/Badges.sol/Badges.json'
-import { ethers } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
 
 import { POLYGON_BADGES_CONTRACT_ADDRESS, RAFT_OWNER_PK, TRIMMED_OTTERSPACE_RAFT_ID } from '../../constants'
-import { ActionStatus, BadgeCreationResult, GAS_MULTIPLIER, GasConfig } from '../../entities/Badges/types'
+import { ActionStatus, BadgeCreationResult, GasConfig } from '../../entities/Badges/types'
 import RpcService from '../../services/RpcService'
 import logger from '../../utils/logger'
+import { BlockNative } from '../clients/BlockNative'
 import { AirdropJobStatus, AirdropOutcome } from '../types/AirdropJob'
 
-const TRANSACTION_UNDERPRICED_ERROR_CODE = -32000
+const TRANSACTION_UNDERPRICED_ERROR_CODE = '-32000'
+const TRANSACTION_REPLACED_CODE = 'TRANSACTION_REPLACED'
 
 function checksumAddresses(addresses: string[]): string[] {
   return addresses.map((address) => ethers.utils.getAddress(address))
@@ -20,33 +24,50 @@ function getBadgesSignerAndContract() {
   return { signer, contract }
 }
 
+const GAS_LIMIT_PERCENTAGE_OVER_ESTIMATED = 20
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function estimateGas(estimateFunction: (...args: any[]) => Promise<any>): Promise<GasConfig> {
-  const provider = RpcService.getPolygonProvider()
-  const gasLimit = await estimateFunction()
-  const gasPrice = await provider.getGasPrice()
-  const adjustedGasPrice = gasPrice.mul(GAS_MULTIPLIER)
+  const gasData = await BlockNative.getPolygonGasData()
+  const estimatedGasLimit = await estimateFunction()
+  const gasLimit = estimatedGasLimit.add(estimatedGasLimit.mul(GAS_LIMIT_PERCENTAGE_OVER_ESTIMATED).div(100))
+
   return {
-    gasPrice: adjustedGasPrice,
+    ...gasData,
     gasLimit,
   }
 }
 
-export async function airdrop(badgeCid: string, recipients: string[], shouldPumpGas = false) {
+export async function airdrop(badgeCid: string, recipients: string[], gasIncrement = 0) {
   const { signer, contract } = getBadgesSignerAndContract()
   const ipfsAddress = `ipfs://${badgeCid}/metadata.json`
   const formattedRecipients = checksumAddresses(recipients)
-  logger.log(`Airdropping, pumping gas ${shouldPumpGas}`)
-  let txn
-  if (shouldPumpGas) {
-    const gasConfig = await estimateGas(async () => contract.estimateGas.airdrop(formattedRecipients, ipfsAddress))
-    txn = await contract.connect(signer).airdrop(formattedRecipients, ipfsAddress, gasConfig)
-  } else {
-    txn = await contract.connect(signer).airdrop(formattedRecipients, ipfsAddress)
-  }
+  logger.log(`Preparing airdrop with gas increment: ${gasIncrement}`)
+
+  const gasConfig = await getGasConfig(contract, formattedRecipients, ipfsAddress, gasIncrement)
+  const txn = await contract.connect(signer).airdrop(formattedRecipients, ipfsAddress, gasConfig)
   await txn.wait()
   logger.log('Airdropped badge with txn hash:', txn.hash)
   return txn.hash
+}
+
+async function getGasConfig(
+  contract: Contract,
+  formattedRecipients: string[],
+  ipfsAddress: string,
+  gasIncrement: number
+) {
+  const defaultGasConfig = await estimateGas(async () => contract.estimateGas.airdrop(formattedRecipients, ipfsAddress))
+  return gasIncrement > 0 ? getIncreasedGasConfig(defaultGasConfig, gasIncrement) : defaultGasConfig
+}
+
+function getIncreasedGasConfig(gasConfig: GasConfig, gasIncrement: number) {
+  const incrementFactor = 100 + gasIncrement * 10
+
+  return {
+    gasLimit: BigNumber.from(gasConfig.gasLimit).mul(incrementFactor).div(100),
+    maxFeePerGas: BigNumber.from(gasConfig.maxFeePerGas).mul(incrementFactor).div(100),
+    maxPriorityFeePerGas: BigNumber.from(gasConfig.maxPriorityFeePerGas).mul(incrementFactor).div(100),
+  }
 }
 
 export async function reinstateBadge(badgeId: string) {
@@ -82,14 +103,6 @@ export async function checkBalance() {
   console.log(`Balance of ${signer.address}: ${balanceInEther} ETH = ${balanceBigNumber}`)
 }
 
-export function trimOtterspaceId(rawId: string) {
-  const parts = rawId.split(':')
-  if (parts.length === 2) {
-    return parts[1]
-  }
-  return ''
-}
-
 export function getIpfsAddress(badgeCid: string) {
   return `ipfs://${badgeCid}/metadata.json`
 }
@@ -112,17 +125,25 @@ export async function airdropWithRetry(
   badgeCid: string,
   recipients: string[],
   retries = 3,
-  shouldPumpGas = false
+  gasIncrement = 0
 ): Promise<AirdropOutcome> {
   try {
-    await airdrop(badgeCid, recipients, shouldPumpGas)
+    await airdrop(badgeCid, recipients, gasIncrement)
     return { status: AirdropJobStatus.FINISHED, error: '' }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     if (retries > 0) {
+      if (isTransactionReplacedError(error)) {
+        logger.log(`Found replacement transaction for airdrop`, error)
+        if (error.receipt.status === 1) {
+          return { status: AirdropJobStatus.FINISHED, error: JSON.stringify(error) }
+        }
+      }
       logger.log(`Retrying airdrop... Attempts left: ${retries}`, error)
-      const shouldPumpGas = isTransactionUnderpricedError(error)
-      return await airdropWithRetry(badgeCid, recipients, retries - 1, shouldPumpGas)
+      let newGasIncrement = gasIncrement
+      if (isTransactionUnderpricedError(error)) {
+        newGasIncrement += 1
+      }
+      return await airdropWithRetry(badgeCid, recipients, retries - 1, newGasIncrement)
     } else {
       logger.error('Airdrop failed after maximum retries', error)
       return { status: AirdropJobStatus.FAILED, error: JSON.stringify(error) }
@@ -134,7 +155,6 @@ export async function createSpecWithRetry(badgeCid: string, retries = 3): Promis
   try {
     await createSpec(badgeCid)
     return { status: ActionStatus.Success, badgeCid }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     if (retries > 0) {
       logger.log(`Retrying create spec... Attempts left: ${retries}`, error)
@@ -146,12 +166,40 @@ export async function createSpecWithRetry(badgeCid: string, retries = 3): Promis
   }
 }
 
-export function isTransactionUnderpricedError(error: { body: string }) {
-  try {
-    const errorParsed = JSON.parse(error.body)
-    const errorCode = errorParsed?.error?.code
-    return errorCode === TRANSACTION_UNDERPRICED_ERROR_CODE
-  } catch (e) {
+export function isTransactionUnderpricedError(error: any): boolean {
+  const checkError = (err: any): boolean => {
+    if (!err) return false
+    if (
+      err.code === 'REPLACEMENT_UNDERPRICED' ||
+      err.code === TRANSACTION_UNDERPRICED_ERROR_CODE ||
+      String(err.error?.error?.code) === TRANSACTION_UNDERPRICED_ERROR_CODE ||
+      err.reason === 'replacement fee too low'
+    ) {
+      return true
+    }
+    if (err.error) {
+      return checkError(err.error)
+    }
     return false
   }
+
+  return checkError(error)
+}
+
+export function isTransactionReplacedError(error: any): boolean {
+  const checkError = (err: any): boolean => {
+    if (!err) return false
+
+    if (err.code === TRANSACTION_REPLACED_CODE || err.reason === 'replaced') {
+      return true
+    }
+
+    if (err.error) {
+      return checkError(err.error)
+    }
+
+    return false
+  }
+
+  return checkError(error)
 }
--- src/services/ErrorService.ts
+++ src/services/ErrorService.ts
@@ -1,34 +1,40 @@
 import Rollbar from 'rollbar'
 
-import { DAO_ROLLBAR_TOKEN } from '../constants'
 import { isProdEnv } from '../utils/governanceEnvs'
 import logger from '../utils/logger'
 
 const FILTERED_ERRORS = ['FETCH_ERROR', 'ACTION_REJECTED']
 
 export class ErrorService {
-  static client = new Rollbar({
-    accessToken: DAO_ROLLBAR_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    environment: 'production',
-    itemsPerMinute: 10,
-    maxItems: 50,
-    captureIp: 'anonymize',
-    ignoreDuplicateErrors: true,
-    checkIgnore: (isUncaught: boolean, args: Rollbar.LogArgument[], item: Rollbar.Dictionary) => {
-      const body = item.body as { message?: string }
-      if (body && typeof body.message === 'string') {
-        return FILTERED_ERRORS.some((errorString) => body.message?.includes(errorString))
-      }
-      return false
-    },
-  })
+  private static client: Rollbar | null
+
+  private static getClient() {
+    if (!this.client) {
+      this.client = new Rollbar({
+        accessToken: require('../constants').DAO_ROLLBAR_TOKEN,
+        captureUncaught: true,
+        captureUnhandledRejections: true,
+        environment: 'production',
+        itemsPerMinute: 10,
+        maxItems: 50,
+        captureIp: 'anonymize',
+        ignoreDuplicateErrors: true,
+        checkIgnore: (isUncaught: boolean, args: Rollbar.LogArgument[], item: Rollbar.Dictionary) => {
+          const body = item.body as { message?: string }
+          if (body && typeof body.message === 'string') {
+            return FILTERED_ERRORS.some((errorString) => body.message?.includes(errorString))
+          }
+          return false
+        },
+      })
+    }
+    return this.client
+  }
 
   public static report(errorMsg: string, extraInfo?: Record<string, unknown>) {
     if (isProdEnv()) {
-      if (DAO_ROLLBAR_TOKEN) {
-        this.client.error(errorMsg, { extraInfo })
+      if (require('../constants').DAO_ROLLBAR_TOKEN) {
+        this.getClient().error(errorMsg, { extraInfo })
       } else {
         logger.error('Rollbar server access token not found')
       }
