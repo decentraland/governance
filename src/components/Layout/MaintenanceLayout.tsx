91088a387da33edc991eba6fa49b94c493f80008
Andy Espagnolo <andyespagnolo@gmail.com>
2024-02-20 14:54:39 -0300

    refactor: move maintenance page to its own file

--- src/components/Layout/MaintenanceLayout.tsx
+++ src/components/Layout/MaintenanceLayout.tsx
@@ -1,37 +1,12 @@
-import useFormatMessage from '../../hooks/useFormatMessage'
-import Heading from '../Common/Typography/Heading'
-import Text from '../Common/Typography/Text'
-import WiderContainer from '../Common/WiderContainer'
-
 import ContentLayout from './ContentLayout'
 import Head from './Head'
+import MaintenancePage from './MaintenancePage'
 import Navigation, { NavigationTab } from './Navigation'
 
 interface Props {
   title: string
   description: string
   activeTab?: NavigationTab
-}
-
-const MaintenancePage = () => {
-  const t = useFormatMessage()
-
-  return (
-    <WiderContainer>
-      <div
-        style={{
-          minHeight: '70vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Heading>{t('maintenance.title')}</Heading>
-        <Text size="lg">{t('maintenance.description')}</Text>
-      </div>
-    </WiderContainer>
-  )
 }
 
 const MaintenanceLayout = ({ title, description, activeTab }: Props) => {
