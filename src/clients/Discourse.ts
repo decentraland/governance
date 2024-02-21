a85b1397c2dc9fe91fcf5c8dc73c5b3f1a591b93
1emu <github@lemu.sh>
2024-02-21 11:15:38 -0300

    chore: replace API client in Discourse client

--- src/clients/Discourse.ts
+++ src/clients/Discourse.ts
@@ -1,7 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import API from 'decentraland-gatsby/dist/utils/api/API'
-import Options from 'decentraland-gatsby/dist/utils/api/Options'
-
 import { requiredEnv } from '../config'
 import { DISCOURSE_API, DISCOURSE_USER } from '../entities/User/utils'
 import {
@@ -15,13 +12,13 @@ import {
   DiscourseUser,
 } from '../shared/types/discourse'
 
+import API from './API'
 import { trimLastForwardSlash } from './utils'
 
 export class Discourse extends API {
   static Url = DISCOURSE_API || 'https://meta.discourse.org/'
 
   static Cache = new Map<string, Discourse>()
-  private readonly auth: DiscourseAuth
 
   static from(baseUrl: string) {
     baseUrl = trimLastForwardSlash(baseUrl)
@@ -38,7 +35,9 @@ export class Discourse extends API {
 
   constructor(baseUrl: string) {
     super(baseUrl)
-    this.auth = Discourse.getCredentials()
+    const { apiKey, apiUsername } = Discourse.getCredentials()
+    this.setDefaultHeader('Api-Key', apiKey)
+    this.setDefaultHeader('Api-Username', apiUsername)
   }
 
   private static getCredentials() {
@@ -53,53 +52,42 @@ export class Discourse extends API {
     return DISCOURSE_AUTH
   }
 
-  private withAuth(options: Options) {
-    return options.header('Api-Key', this.auth.apiKey).header('Api-Username', this.auth.apiUsername)
-  }
-
   async getPost(id: number) {
-    let options: Options = this.options()
-    options = this.withAuth(options)
-    return this.fetch<DiscoursePost>(`/posts/${id}.json`, options)
+    return this.fetch<DiscoursePost>(`/posts/${id}.json`)
   }
 
   async getLatestPosts() {
-    let options = this.options()
-    options = this.withAuth(options)
-
-    return this.fetch<{ latest_posts: DiscoursePost[] }>(`/posts.json`, options)
+    return this.fetch<{ latest_posts: DiscoursePost[] }>(`/posts.json`)
   }
 
   async createPost(post: DiscourseNewPost) {
-    return this.fetch<DiscoursePost>(`/posts.json`, this.withAuth(this.options().method('POST').json(post)))
+    return this.fetch<DiscoursePost>(`/posts.json`, { method: 'POST', json: post })
   }
 
   async commentOnPost(post: DiscourseComment) {
-    return this.fetch<DiscoursePost>(`/posts.json`, this.withAuth(this.options().method('POST').json(post)))
+    return this.fetch<DiscoursePost>(`/posts.json`, { method: 'POST', json: post })
   }
 
   async updatePost({ id, ...update }: DiscourseUpdatePost) {
-    return this.fetch<DiscoursePost>(`/posts/${id}.json`, this.withAuth(this.options().method('PUT').json(update)))
+    return this.fetch<DiscoursePost>(`/posts/${id}.json`, { method: 'PUT', json: update })
   }
 
   async closeTopic({ id }: DiscourseCloseTopic) {
-    return this.fetch<Record<string, unknown>>(
-      `/t/${id}/status.json`,
-      this.withAuth(
-        this.options().method('PUT').json({
-          status: 'closed',
-          enabled: 'true',
-        })
-      )
-    )
+    return this.fetch<Record<string, unknown>>(`/t/${id}/status.json`, {
+      method: 'POST',
+      json: {
+        status: 'closed',
+        enabled: 'true',
+      },
+    })
   }
 
   async removeTopic(id: number) {
-    return this.fetch<Record<string, unknown>>(`/t/${id}.json`, this.withAuth(this.options().method('DELETE')))
+    return this.fetch<Record<string, unknown>>(`/t/${id}.json`, { method: 'DELETE' })
   }
 
   async getTopic(topic_id: number) {
-    return this.fetch<DiscourseTopic>(`/t/${topic_id}.json`, this.withAuth(this.options().method('GET')))
+    return this.fetch<DiscourseTopic>(`/t/${topic_id}.json`)
   }
 
   async getPosts(topic_id: number, postIds: number[]) {
@@ -108,13 +96,10 @@ export class Discourse extends API {
       if (index > 0) query = query.concat('&')
       query = query.concat('post_ids[]=' + id)
     })
-    return this.fetch<Pick<DiscourseTopic, 'post_stream' | 'id'>>(
-      `/t/${topic_id}/posts.json` + query,
-      this.withAuth(this.options().method('GET'))
-    )
+    return this.fetch<Pick<DiscourseTopic, 'post_stream' | 'id'>>(`/t/${topic_id}/posts.json` + query)
   }
 
   async getUserById(id: number) {
-    return this.fetch<DiscourseUser>(`/admin/users/${id}.json`, this.withAuth(this.options().method('GET')))
+    return this.fetch<DiscourseUser>(`/admin/users/${id}.json`)
   }
 }
