896b456bcd645d810575d879ce1357baca219a3d
1emu <github@lemu.sh>
2024-02-27 18:10:53 -0300

    chore: custom markdown preview

--- src/components/Common/Form/MarkdownField.tsx
+++ src/components/Common/Form/MarkdownField.tsx
@@ -1,7 +1,11 @@
+import React, { useContext } from 'react'
 import { Control, Controller, FieldValues, Path, PathValue } from 'react-hook-form'
 
-import MDEditor, { MDEditorProps } from '@uiw/react-md-editor'
+import MDEditor, { EditorContext, MDEditorProps, commands } from '@uiw/react-md-editor'
 import classNames from 'classnames'
+import rehypeSanitize from 'rehype-sanitize'
+
+import Markdown from '../Typography/Markdown'
 
 import './MarkdownTextArea.css'
 
@@ -26,6 +30,27 @@ export default function MarkdownField<T extends FieldValues>({
   message,
   ...markdownProps
 }: MarkdownFieldProps<T>) {
+  const Button = () => {
+    const { preview, dispatch } = useContext(EditorContext)
+    const click = () => {
+      !!dispatch &&
+        dispatch({
+          preview: preview === 'edit' ? 'preview' : 'edit',
+        })
+    }
+    if (preview === 'edit') {
+      return <div onClick={click}>{'Preview'}</div>
+    }
+    return <div onClick={click}>{'Edit'}</div>
+  }
+
+  const codePreview = {
+    name: 'preview',
+    keyCommand: 'preview',
+    value: 'preview',
+    icon: <Button />,
+  }
+
   return (
     <>
       <Controller
@@ -35,13 +60,21 @@ export default function MarkdownField<T extends FieldValues>({
         rules={rules}
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         render={({ field: { ref, ...field } }) => (
-          <MDEditor
-            contentEditable={!disabled}
-            minHeight={175}
-            {...field}
-            {...markdownProps}
-            className={classNames(error && 'MarkdownEditor--error', 'MarkdownEditor')}
-          />
+          <>
+            <MDEditor
+              contentEditable={!disabled}
+              minHeight={175}
+              {...field}
+              {...markdownProps}
+              preview={'edit'}
+              previewOptions={{
+                rehypePlugins: [[rehypeSanitize]],
+              }}
+              components={{ preview: (source: string) => <Markdown>{source}</Markdown> }}
+              className={classNames(error && 'MarkdownEditor--error', 'MarkdownEditor')}
+              extraCommands={[codePreview, commands.fullscreen]}
+            />
+          </>
         )}
       />
       {error && (
