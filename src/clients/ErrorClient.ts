9397185ab92ee717c9e0e5a67dfa10eec7e6b761
Andy Espagnolo <andyespagnolo@gmail.com>
2024-03-26 12:57:36 -0300

    fix: backend crashing when dao rollbar token is missing

--- src/clients/ErrorClient.ts
+++ src/clients/ErrorClient.ts
@@ -1,13 +1,10 @@
-import { DAO_ROLLBAR_TOKEN } from '../constants'
-
 import { Governance } from './Governance'
 
 export class ErrorClient {
-  static isServerSide = DAO_ROLLBAR_TOKEN && DAO_ROLLBAR_TOKEN.length > 0
-
   public static report(errorMsg: string, extraInfo?: Record<string, unknown>) {
     try {
-      if (!this.isServerSide) {
+      const isClientSide = typeof window !== 'undefined'
+      if (isClientSide) {
         Governance.get().reportErrorToServer(errorMsg, extraInfo)
       } else {
         const ErrorService = require('../services/ErrorService.ts')
