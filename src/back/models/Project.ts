869c4a210d1056a1074a09603e890a2c99c7ac7c
ncomerci <ncomerci@itba.edu.ar>
2024-05-15 14:12:21 -0300

    feat: get projects query

--- src/back/models/Project.ts
+++ src/back/models/Project.ts
@@ -1,4 +1,5 @@
 import { Model } from 'decentraland-gatsby/dist/entities/Database/model'
+import { SQL } from 'decentraland-gatsby/dist/entities/Database/utils/sql'
 
 import { ProjectStatus } from '../../entities/Grant/types'
 
@@ -15,8 +16,27 @@ export type ProjectAttributes = {
   created_at: Date
 }
 
+// TODO: add here all data from other tables (updates, personnel, milestones, etc)
+export type ProjectAugmented = ProjectAttributes & {
+  author: string
+  coauthors: string[] | null
+}
+
 export default class ProjectModel extends Model<ProjectAttributes> {
   static tableName = 'projects'
   static withTimestamps = false
   static primaryKey = 'id'
+
+  static async getProject(id: string) {
+    const query = SQL`
+    SELECT pr.id, pr.proposal_id, p.user as author, array_agg(co.address) FILTER (WHERE co.status = 'APPROVED') as coauthors, pr.title, pr.status, pr.links, pr.about, pr.about_updated_by, pr.about_updated_at, pr.updated_at, pr.created_at
+    FROM projects pr
+    JOIN proposals p on pr.proposal_id = p.id
+    LEFT JOIN coauthors co on pr.proposal_id = co.proposal_id
+    WHERE pr.id = ${id}
+    GROUP BY pr.id, pr.proposal_id, p.user, pr.title, pr.status, pr.links, pr.about, pr.about_updated_by, pr.about_updated_at, pr.updated_at, pr.created_at;
+    `
+    const result = await this.namedQuery<ProjectAugmented>(`get_project_${id}`, query)
+    return result[0]
+  }
 }
