60172f9c198e80bd5ea27541176aabfd6cc7be5d
lemu <github@lemu.sh>
2024-05-29 10:32:47 -0300

    chore: create project links (#1818)

---
+++ src/migrations/1716925443361_remove-project-link-column.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate"
+import Model from "../back/models/Project"
+
+export const shorthands: ColumnDefinitions | undefined = undefined
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropColumns(Model.tableName, [ 'links' ])
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumns(Model.tableName, {
+    links: {
+      type: 'TEXT[]',
+      notNull: true,
+      default: '{}'
+    },
+  })
+}
