a0ca2f95e9ea481ed1f640c921de5dad5e29d24a
1emu <github@lemu.sh>
2024-05-30 11:40:13 -0300

    fix: add/delete link endpoints

--- src/back/routes/project.ts
+++ src/back/routes/project.ts
@@ -4,11 +4,12 @@ import handleAPI, { handleJSON } from 'decentraland-gatsby/dist/entities/Route/h
 import routes from 'decentraland-gatsby/dist/entities/Route/routes'
 import { Request } from 'express'
 
-import { PersonnelInCreationSchema } from '../../entities/Project/types'
+import { PersonnelInCreationSchema, ProjectLinkInCreationSchema } from '../../entities/Project/types'
 import CacheService, { TTL_1_HS } from '../../services/CacheService'
 import { ProjectService } from '../../services/ProjectService'
 import { isProjectAuthorOrCoauthor } from '../../utils/projects'
 import PersonnelModel, { PersonnelAttributes } from '../models/Personnel'
+import ProjectLinkModel, { ProjectLink } from '../models/ProjectLink'
 import { isValidDate, validateId } from '../utils/validations'
 
 export default routes((route) => {
@@ -16,6 +17,8 @@ export default routes((route) => {
   route.get('/projects', handleJSON(getProjects))
   route.post('/projects/personnel/', withAuth, handleAPI(addPersonnel))
   route.delete('/projects/personnel/:personnel_id', withAuth, handleAPI(deletePersonnel))
+  route.post('/projects/links/', withAuth, handleAPI(addLink))
+  route.delete('/projects/links/:link_id', withAuth, handleAPI(deleteLink))
   route.get('/projects/:project', handleAPI(getProject))
   route.get('/projects/pitches-total', handleJSON(getOpenPitchesTotal))
   route.get('/projects/tenders-total', handleJSON(getOpenTendersTotal))
@@ -102,4 +105,42 @@ async function deletePersonnel(req: WithAuth<Request<{ personnel_id: string }>>)
     throw new RequestError("Only the project's authors and coauthors can delete personnel", RequestError.Unauthorized)
   }
   return await ProjectService.deletePersonnel(personnel_id, user)
+}
+
+async function addLink(req: WithAuth): Promise<ProjectLink> {
+  const user = req.auth!
+  const { project_link } = req.body
+  validateId(project_link.project_id)
+  const project = await ProjectService.getProject(project_link.project_id)
+  if (!project) {
+    throw new RequestError(`Project "${project_link.project_id}" not found`, RequestError.NotFound)
+  }
+  if (!isProjectAuthorOrCoauthor(user, project)) {
+    throw new RequestError("Only the project's authors and coauthors can create links", RequestError.Unauthorized)
+  }
+  const parsedLink = ProjectLinkInCreationSchema.safeParse(project_link)
+  if (!parsedLink.success) {
+    throw new RequestError(`Invalid link: ${parsedLink.error.message}`, RequestError.BadRequest)
+  }
+  return await ProjectService.addLink(parsedLink.data, user)
+}
+
+//TODO: refactor repeated validations
+async function deleteLink(req: WithAuth<Request<{ link_id: string }>>): Promise<string | null> {
+  const user = req.auth!
+  const link_id = req.params.link_id
+  validateId(link_id)
+  const projectLink = await ProjectLinkModel.findOne<PersonnelAttributes>(link_id)
+  if (!projectLink) {
+    throw new RequestError(`Link "${link_id}" not found`, RequestError.NotFound)
+  }
+  //TODO: use a different query to get project authors/coauthors
+  const project = await ProjectService.getProject(projectLink.project_id)
+  if (!project) {
+    throw new RequestError(`Project "${projectLink.project_id}" not found`, RequestError.NotFound)
+  }
+  if (!isProjectAuthorOrCoauthor(user, project)) {
+    throw new RequestError("Only the project's authors and coauthors can delete links", RequestError.Unauthorized)
+  }
+  return await ProjectService.deleteLink(link_id)
 }
--- src/entities/Project/types.ts
+++ src/entities/Project/types.ts
@@ -2,6 +2,7 @@ import isEthereumAddress from 'validator/lib/isEthereumAddress'
 import { ZodSchema, z } from 'zod'
 
 import { PersonnelAttributes } from '../../back/models/Personnel'
+import { ProjectLink } from '../../back/models/ProjectLink'
 
 const addressCheck = (data: string) => !data || (!!data && isEthereumAddress(data))
 
@@ -15,5 +16,12 @@ export const PersonnelInCreationSchema: ZodSchema<PersonnelInCreation> = z.objec
   role: z.string().min(1).max(80),
   about: z.string().min(1).max(750),
   relevantLink: z.string().min(0).max(200).url().optional().or(z.literal('')),
+  project_id: z.string().min(0),
+})
+
+export type ProjectLinkInCreation = Pick<ProjectLink, 'label' | 'url' | 'project_id'>
+export const ProjectLinkInCreationSchema: ZodSchema<ProjectLinkInCreation> = z.object({
+  label: z.string().min(1).max(80),
+  url: z.string().min(0).max(200).url(),
   project_id: z.string().min(0),
 })
