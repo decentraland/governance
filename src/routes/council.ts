d29eef7602398f27bf9b5065f5d75c22be14cb77
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2025-12-10 11:28:51 +0100

    fix: change import path

--- src/routes/council.ts
+++ src/routes/council.ts
@@ -1,7 +1,7 @@
 import handleAPI from 'decentraland-gatsby/dist/entities/Route/handle'
 import routes from 'decentraland-gatsby/dist/entities/Route/routes'
 
-import { DAO_COUNCIL_ADDRESSES } from '../entities/Council/IsDAOCouncil'
+import { DAO_COUNCIL_ADDRESSES } from '../constants'
 
 export default routes((router) => {
   return router.get(
